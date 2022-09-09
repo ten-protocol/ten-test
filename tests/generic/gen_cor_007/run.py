d7d607c117da6f7d7a9fcfcb403614938956f25d
Moray Grieve <moray.grieve@me.com>
2022-09-09 16:14:26 +0100

    Correct titles

--- tests/generic/gen_cor_007/run.py
+++ tests/generic/gen_cor_007/run.py
@@ -15,7 +15,7 @@ class PySysTest(EthereumTest):
 
         self.log.info('Getting the contract bytecode ...')
         try:
-            byte_code = web3.eth.getCode(error.contract_address).hex()[2:]
+            byte_code = web3.eth.get_code(error.contract_address).hex()[2:]
             self.assertTrue(byte_code in error.bytecode)
         except Exception as e:
             self.log.error('Error getting bytecode', e)
