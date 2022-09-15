24c593bac35311683e173f1e8c5997ccde09bec7
Moray Grieve <moray.grieve@r3.com>
2022-09-15 12:00:32 +0100

    More refactoring  (#44)
    * Add some refactoring
    
    * Refactoring

--- src/python/ethsys/networks/ropsten.py
+++ src/python/ethsys/networks/ropsten.py
@@ -5,17 +5,19 @@ from ethsys.networks.default import Default
 
 class Ropsten(Default):
     """A Ropsten node giving access to the underlying network."""
-    HOST = 'ropsten.infura.io'
+    HOST = 'https://ropsten.infura.io/v3'
+    WS_HOST = 'wss://ropsten.infura.io/v3'
 
     @classmethod
     def chain_id(cls): return 3
 
     @classmethod
-    def http_connection(cls, private_key, host, port):
-        web3 = Web3(Web3.HTTPProvider('https://%s/v3/%s' % (host, Properties().infuraProjectID())))
+    def connection(cls, private_key, web_socket):
+        provider = Web3.HTTPProvider if not web_socket else Web3.WebsocketProvider
+        host = '%s/%s' % (cls.HOST if not web_socket else cls.WS_HOST, Properties().infuraProjectID())
+
+        web3 = Web3(provider(host))
         account = web3.eth.account.privateKeyToAccount(private_key)
         return web3, account
-
-
 
 
