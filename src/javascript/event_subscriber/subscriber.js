d2413ac2f0718a24070f5d9179b41853a7306b2e
Moray Grieve <moray.grieve@me.com>
2022-09-20 17:07:57 +0100

    Updated

--- src/javascript/event_subscriber/subscriber.js
+++ src/javascript/event_subscriber/subscriber.js
@@ -1,21 +1,35 @@
 const Web3 = require('web3');
-const url = 'wss://ws-nd-123-456-789.p2pify.com/3c6e0b8a9c15224a8228b9a98ca1531';
-const web3 = new Web3(url);
+const commander = require('commander');
 
-var options = {
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('-u, --url <url>', 'The node WSS endpoint.')
+  .option('-a, --address <value>', 'the address of the contract whose events you subscribe to.', 'Default')
+  .option('-k, --event <value>', 'The Keccak-256 value of the event.', 'Default')
+  .parse(process.argv);
+
+const options = commander.opts();
+console.log('URL:', `${options.url}`);
+console.log('Address:', `${options.address}`);
+console.log('Event:', `${options.event}`);
+
+const web3 = new Web3(`${options.url}`);
+
+var subscription_options = {
 reconnect: {
          auto: true,
-         delay: 5000, // ms
+         delay: 5000,
          maxAttempts: 5,
          onTimeout: false
 },
-address: '0x514910771af9ca656af840dff83e8264ecf986ca',
+address: `${options.address}`,
 topics: [
- '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+ `${options.event}`
 ]
 };
 
-var subscription = web3.eth.subscribe('logs', options, function(error, result){
+var subscription = web3.eth.subscribe('logs', subscription_options, function(error, result){
     if (!error) console.log('got result');
     else console.log(error);
 }).on("data", function(log){
--- utils/docker/image.Dockerfile
+++ utils/docker/image.Dockerfile
@@ -10,6 +10,7 @@ RUN apt install -y solc
 RUN apt install -y nodejs npm
 RUN npm install -g ganache-cli
 RUN npm install -g web3
+RUN npm install commander
 
 RUN apt install -y vim
 RUN apt install -y python3-pip
