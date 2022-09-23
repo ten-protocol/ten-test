6d8212607d5eea71a85b447370691229673c5348
Moray Grieve <moray.grieve@me.com>
2022-09-23 07:58:39 +0100

    Addition

---
+++ tests/generic/gen_cor_062/run.py
@@ -0,0 +1,56 @@
+from web3 import Web3
+from ethsys.basetest import EthereumTest
+from ethsys.contracts.erc20.obx import OBXCoin
+from ethsys.networks.factory import NetworkFactory
+from ethsys.utils.properties import Properties
+
+class PySysTest(EthereumTest):
+    WEBSOCKET = False
+
+    def execute(self):
+        # deployment of contract
+        network = NetworkFactory.get_network(self.env)
+        account2_pk = Properties().account2pk()
+        Web3.
+        web3_1, account1 = network.connect_account1(self, web_socket=self.WEBSOCKET)
+
+        erc20 = OBXCoin(self, web3_1)
+        erc20.deploy(network, account1)
+
+        # check initial allocations
+        self.log.info('Balances at initial allocation')
+        self.log.info('  Account1 balance = %d ' % erc20.contract.functions.balanceOf(account1.address).call())
+
+        # transfer from account1 into account2
+        network.transact(self, web3_1, erc20.contract.functions.transfer(account2.address, 200), account1, erc20.GAS)
+        self.log.info('Balances after transfer from account 1 to account 2')
+        self.log.info('  Account1 balance = %d ' % contract_1.functions.balanceOf(account1.address).call())
+        self.log.info('  Account2 balance = %d ' % contract_2.functions.balanceOf(account2.address).call())
+        self.log.info('  Account3 balance = %d ' % contract_3.functions.balanceOf(account3.address).call())
+        self.assertTrue(contract_1.functions.balanceOf(account1.address).call() == 999800)
+        self.assertTrue(contract_2.functions.balanceOf(account2.address).call() == 200)
+        self.assertTrue(contract_3.functions.balanceOf(account3.address).call() == 0)
+
+        # account1 approves account2 to withdraw 1000
+        network.transact(self, web3_1, contract_1.functions.approve(account2.address, 1000), account1, erc20.GAS)
+
+        # account2 withdraws from account1 into account3
+        network.transact(self, web3_2, contract_2.functions.transferFrom(account1.address, account3.address, 100),
+                         account2, erc20.GAS)
+        self.log.info('Balances after approval and transfer;')
+        self.log.info('  Account1 balance = %d ' % contract_1.functions.balanceOf(account1.address).call())
+        self.log.info('  Account2 balance = %d ' % contract_2.functions.balanceOf(account2.address).call())
+        self.log.info('  Account3 balance = %d ' % contract_3.functions.balanceOf(account3.address).call())
+        self.assertTrue(contract_1.functions.balanceOf(account1.address).call() == 999700)
+        self.assertTrue(contract_2.functions.balanceOf(account2.address).call() == 200)
+        self.assertTrue(contract_3.functions.balanceOf(account3.address).call() == 100)
+
+        # account3 sends back to account1
+        network.transact(self, web3_3, contract_3.functions.transfer(account1.address, 100), account3, erc20.GAS)
+        self.log.info('Balances after transfer from account 3 to account 1')
+        self.log.info('  Account1 balance = %d ' % contract_1.functions.balanceOf(account1.address).call())
+        self.log.info('  Account2 balance = %d ' % contract_2.functions.balanceOf(account2.address).call())
+        self.log.info('  Account3 balance = %d ' % contract_3.functions.balanceOf(account3.address).call())
+        self.assertTrue(contract_1.functions.balanceOf(account1.address).call() == 999800)
+        self.assertTrue(contract_2.functions.balanceOf(account2.address).call() == 200)
+        self.assertTrue(contract_3.functions.balanceOf(account3.address).call() == 0)
