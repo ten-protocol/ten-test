a0900d62ddb96d4ecb09ee8e56306d36d85cc385
Moray Grieve <moray.grieve@me.com>
2022-09-29 11:16:00 +0100

    Add in some filters

--- src/solidity/storage/KeyStorage.sol
+++ src/solidity/storage/KeyStorage.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.0;
 
 contract Store {
-    event ItemSet(string key, string value, address setter);
+    event ItemSet1(string key, uint256 value, address indexed setter);
+    event ItemSet2(string key, uint256 indexed value, address indexed setter);
+    event ItemSet3(string indexed key, uint256 indexed value, address indexed setter);
 
     address public owner;
-    mapping (string => string) public items;
+    mapping (string => uint256) public items;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function setItem(string calldata key, string calldata value) external {
+    function setItem(string calldata key, uint256 value) external {
         items[key] = value;
-        emit ItemSet(key, value, msg.sender);
+        emit ItemSet1(key, value, msg.sender);
+        emit ItemSet2(key, value, msg.sender);
+        emit ItemSet3(key, value, msg.sender);
     }
 
     function destroy() public {
