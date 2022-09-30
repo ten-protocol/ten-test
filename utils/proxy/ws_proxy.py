43e4fedcca6a614c8fd115f45beca6b9a8e6a8a7
Moray Grieve <moray.grieve@r3.com>
2022-09-30 17:38:42 +0100

    Support for running through a websocket proxy (#50)
    * Add in a proxy for websockets
    
    * Updated to add in proxy

---
+++ utils/proxy/ws_proxy.py
@@ -0,0 +1,44 @@
+import argparse, asyncio, websockets, sys
+
+
+async def hello(websocket, path):
+    """Called whenever a new connection is made to the server."""
+
+    url = REMOTE_URL + path
+    async with websockets.connect(url) as ws:
+        taskA = asyncio.create_task(clientToServer(ws, websocket))
+        taskB = asyncio.create_task(serverToClient(ws, websocket))
+
+        await taskA
+        await taskB
+
+
+async def clientToServer(ws, websocket):
+    async for message in ws:
+        FP.write('%s\n' % message)
+        FP.flush()
+        await websocket.send(message)
+
+
+async def serverToClient(ws, websocket):
+    async for message in websocket:
+        FP.write('%s\n' % message)
+        FP.flush()
+        await ws.send(message)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='websocket proxy.')
+    parser.add_argument('--host', help='Host to bind to')
+    parser.add_argument('--port', help='Port to bind to')
+    parser.add_argument('--remote_url', help='Remote websocket url')
+    parser.add_argument('--filename', help='Log messages to this filename')
+    args = parser.parse_args()
+
+    REMOTE_URL = args.remote_url
+    FP = open(args.filename, 'w')
+
+    start_server = websockets.serve(hello, args.host, args.port)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
