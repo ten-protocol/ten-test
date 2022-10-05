f2e0556e36ef6cb52f02894bb7f77b7a56c734e9
Moray Grieve <moray.grieve@me.com>
2022-10-05 13:40:03 +0100

    Complete unsubscription test

--- tests/generic/gen_cor_083/Input/subscriber.js
+++ tests/generic/gen_cor_083/Input/subscriber.js
@@ -2,30 +2,30 @@ const Web3 = require('web3');
 const commander = require('commander');
 const http = require('http')
 
-function doSubscribe() {
-  console.log('Subscribing for event logs')
+function subscribe() {
   topic = web3.utils.sha3('Stored(uint256)');
-  subscription = web3.eth.subscribe('logs',
-    {
-      topics: [topic],
-      address: options.address
-    },
-    function(error, result) {
-      if (error) {
-        console.log('Error returned is ', error)
-      } else {
-        console.log('Full result is ', result);
-        console.log('Stored value =', Web3.utils.hexToNumber(result.data));
-      }
-    }
-  )
+  subscription = web3.eth.subscribe('logs', {
+    topics: [topic],
+    address: options.contract_address
+  }, function(error, result) {
+       if (error)
+         console.log('Error returned is ', error)
+  })
+  .on("connected", function(subscriptionId){
+      console.log('Subscribed for event logs')
+      console.log('Subscription id is', subscription.id);
+      console.log('Subscription arguments are', subscription.arguments)
+  })
+  .on("data", function(log){
+      console.log('Full log is ', log);
+      console.log('Stored value =', Web3.utils.hexToNumber(log.data));
+  })
 }
 
-function doUnsubscribe() {
-  console.log('Unsubscribing from event logs')
+function unsubscribe() {
   subscription.unsubscribe(function(error, success){
     if (success)
-        console.log('Successfully unsubscribed!');
+        console.log('Unsubscribed for event logs');
   });
 }
 
@@ -39,9 +39,9 @@ function createServer(){
         })
         request.on('end', function() {
           if (body == 'SUBSCRIBE'){
-            doSubscribe()
+            subscribe()
           } else if (body == 'UNSUBSCRIBE'){
-            doUnsubscribe()
+            unsubscribe()
           }
           response.writeHead(200, {'Content-Type': 'text/plain'})
           response.end('post received')
@@ -90,7 +90,7 @@ commander
   .option('-p, --script_server_port <value>', 'This script listening port for HTTP posts')
   .option('-u, --network_http <value>', 'Http connection URL to the network')
   .option('-w, --network_ws <value>', 'Web socket connection URL to the network')
-  .option('-a, --contract_address <value>', 'Address of the contract')
+  .option('-a, --contract_address <value>', 'Address of the contract to subscribe to')
   .option('-p, --account_private_key <value>', 'Private key for the account')
   .option('-o, --is_obscuro', 'True if running against obscuro', false)
   .parse(process.argv);
--- tests/generic/gen_cor_083/run.py
+++ tests/generic/gen_cor_083/run.py
@@ -13,7 +13,7 @@ class PySysTest(EthereumTest):
         web3, account = network.connect_account1(self)
 
         # deploy the storage contracts
-        storage = Storage(self, web3, 100)
+        storage = Storage(self, web3, 0)
         storage.deploy(network, account)
 
         # run a background script
@@ -28,9 +28,34 @@ class PySysTest(EthereumTest):
         args.extend(['--contract_address', '%s' % storage.contract_address])
         args.extend(['--account_private_key', '%s' % Properties().account3pk()])
         if self.is_obscuro(): args.append('--is_obscuro')
-
         self.run_javascript(script, stdout, stderr, args)
         self.waitForGrep(file=stdout, expr='Subscriber listening for instructions', timeout=10)
 
+        # perform some transactions on the key storage contract
+        network.transact(self, web3, storage.contract.functions.store(100), account, storage.GAS)
+        network.transact(self, web3, storage.contract.functions.store(101), account, storage.GAS)
+
+        # subscribe
+        self.subscribe(port, stdout)
+        network.transact(self, web3, storage.contract.functions.store(102), account, storage.GAS)
+        network.transact(self, web3, storage.contract.functions.store(103), account, storage.GAS)
+
+        # unsubscribe
+        self.unsubscribe(port, stdout)
+        network.transact(self, web3, storage.contract.functions.store(104), account, storage.GAS)
+        network.transact(self, web3, storage.contract.functions.store(105), account, storage.GAS)
+
+        # wait and validate
+        exprList=[]
+        exprList.append('Stored value = 102')
+        exprList.append('Stored value = 103')
+        self.waitForGrep(file=stdout, expr='Stored value', condition='== 2', timeout=20)
+        self.assertOrderedGrep(file=stdout, exprList=exprList)
+
+    def subscribe(self, port, stdout):
         requests.post('http://127.0.0.1:%d' % port, data='SUBSCRIBE', headers={'Content-Type': 'text/plain'})
+        self.waitForGrep(file=stdout, expr='Subscribed for event logs', timeout=10)
+
+    def unsubscribe(self, port, stdout):
         requests.post('http://127.0.0.1:%d' % port, data='UNSUBSCRIBE', headers={'Content-Type': 'text/plain'})
+        self.waitForGrep(file=stdout, expr='Unsubscribed for event logs', timeout=10)
