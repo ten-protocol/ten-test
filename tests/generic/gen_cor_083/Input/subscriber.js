7d72ea301a9cd4466d3f2b6211cfdbf10bdeba33
Moray Grieve <moray.grieve@r3.com>
2022-10-05 12:44:56 +0100

    Initial unsubscription (#53)
    * Sub and unsub via HTTP post
    
    * Updated
    
    * Updated

---
+++ tests/generic/gen_cor_083/Input/subscriber.js
@@ -0,0 +1,118 @@
+const Web3 = require('web3');
+const commander = require('commander');
+const http = require('http')
+
+function doSubscribe() {
+  console.log('Subscribing for event logs')
+  topic = web3.utils.sha3('Stored(uint256)');
+  subscription = web3.eth.subscribe('logs',
+    {
+      topics: [topic],
+      address: options.address
+    },
+    function(error, result) {
+      if (error) {
+        console.log('Error returned is ', error)
+      } else {
+        console.log('Full result is ', result);
+        console.log('Stored value =', Web3.utils.hexToNumber(result.data));
+      }
+    }
+  )
+}
+
+function doUnsubscribe() {
+  console.log('Unsubscribing from event logs')
+  subscription.unsubscribe(function(error, success){
+    if (success)
+        console.log('Successfully unsubscribed!');
+  });
+}
+
+function createServer(){
+  var server = http.createServer(
+    function(request, response) {
+      if (request.method == 'POST') {
+        var body = ''
+        request.on('data', function(data) {
+          body += data
+        })
+        request.on('end', function() {
+          if (body == 'SUBSCRIBE'){
+            doSubscribe()
+          } else if (body == 'UNSUBSCRIBE'){
+            doUnsubscribe()
+          }
+          response.writeHead(200, {'Content-Type': 'text/plain'})
+          response.end('post received')
+        })
+      }
+    }
+  )
+  return server
+}
+
+function generate_viewing_key() {
+  console.log('Generating viewing key for', options.account_private_key)
+  console.log(options.network_http + '/generateviewingkey/')
+
+  fetch(options.network_http +'/generateviewingkey/', {
+    method: 'POST',
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    body: JSON.stringify({address: account.address})
+  })
+  .then(response => response.text())
+  .then((response) => {
+         sign_viewing_key()
+   })
+}
+
+function sign_viewing_key() {
+  console.log('Signing viewing key for', options.account_private_key)
+  console.log('Result was', response)
+  signed_msg = web3.eth.accounts.sign('vk' + response, '0x' + options.account_private_key)
+
+  fetch(options.network_http + '/submitviewingkey/', {
+    method: 'POST',
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    body: JSON.stringify( {signature: signed_msg.signature, address: account.address})
+  })
+  .then(response => response.text())
+  .then((response) => {
+    console.log('Starting task ...')
+    task()
+   })
+}
+
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('-p, --script_server_port <value>', 'This script listening port for HTTP posts')
+  .option('-u, --network_http <value>', 'Http connection URL to the network')
+  .option('-w, --network_ws <value>', 'Web socket connection URL to the network')
+  .option('-a, --contract_address <value>', 'Address of the contract')
+  .option('-p, --account_private_key <value>', 'Private key for the account')
+  .option('-o, --is_obscuro', 'True if running against obscuro', false)
+  .parse(process.argv);
+
+// options
+const options = commander.opts();
+
+// subscription is initially null
+var subscription = null
+
+// create the server to listen for subscription instructions
+const server = createServer()
+
+const web3 = new Web3(`${options.network_ws}`);
+const account = web3.eth.accounts.privateKeyToAccount(`${options.account_private_key}`)
+
+if (options.is_obscuro == true) {
+  generate_viewing_key()
+  server.listen(options.script_server_port, '127.0.0.1')
+  console.log('Subscriber listening for instructions')
+}
+else {
+  server.listen(options.script_server_port, '127.0.0.1')
+  console.log('Subscriber listening for instructions')
+}
--- tests/generic/gen_cor_083/run.py
+++ tests/generic/gen_cor_083/run.py
@@ -1,22 +1,36 @@
-import os, requests, json
+import os, requests
 from ethsys.basetest import EthereumTest
+from ethsys.contracts.storage.storage import Storage
+from ethsys.networks.factory import NetworkFactory
+from ethsys.utils.properties import Properties
 
 
 class PySysTest(EthereumTest):
 
     def execute(self):
-        host = '127.0.0.1'
-        port = self.getNextAvailableTCPPort()
+        # connect to network
+        network = NetworkFactory.get_network(self.env)
+        web3, account = network.connect_account1(self)
+
+        # deploy the storage contracts
+        storage = Storage(self, web3, 100)
+        storage.deploy(network, account)
 
         # run a background script
-        stdout = os.path.join(self.output, 'listener.out')
-        stderr = os.path.join(self.output, 'listener.err')
-        script = os.path.join(self.input, 'server.js')
+        port = self.getNextAvailableTCPPort()
+        stdout = os.path.join(self.output, 'subscriber.out')
+        stderr = os.path.join(self.output, 'subscriber.err')
+        script = os.path.join(self.input, 'subscriber.js')
         args = []
-        args.extend(['--port', '%d' % port])
-        self.run_javascript(script, stdout, stderr, args)
-        self.waitForGrep(file=stdout, expr='Listening at http', timeout=10)
+        args.extend(['--script_server_port', '%d' % port])
+        args.extend(['--network_http', '%s' % network.connection_url(web_socket=False)])
+        args.extend(['--network_ws', '%s' % network.connection_url(web_socket=True)])
+        args.extend(['--contract_address', '%s' % storage.contract_address])
+        args.extend(['--account_private_key', '%s' % Properties().account3pk()])
+        if self.is_obscuro(): args.append('--is_obscuro')
 
-        data = {"command": 'subscribe'}
-        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-        response = requests.post('http://%s:%d' % (host, port), data=json.dumps(data), headers=headers)
+        self.run_javascript(script, stdout, stderr, args)
+        self.waitForGrep(file=stdout, expr='Subscriber listening for instructions', timeout=10)
+
+        requests.post('http://127.0.0.1:%d' % port, data='SUBSCRIBE', headers={'Content-Type': 'text/plain'})
+        requests.post('http://127.0.0.1:%d' % port, data='UNSUBSCRIBE', headers={'Content-Type': 'text/plain'})
