ed60f3d62d37cd01aa2f5cbf977c078dbb1066a5
Moray Grieve <moray.grieve@me.com>
2022-10-06 16:30:57 +0100

    Use common subscriber

--- tests/generic/gen_cor_079/run.py
+++ tests/generic/gen_cor_079/run.py
@@ -1,9 +1,8 @@
-import os
 from obscuro.test.obscuro_test import ObscuroTest
 from obscuro.test.contracts.storage.storage import Storage
 from obscuro.test.contracts.storage.key_storage import KeyStorage
 from obscuro.test.networks.factory import NetworkFactory
-from obscuro.test.utils.properties import Properties
+from obscuro.test.helpers.log_subscriber import EventLogSubscriber
 
 
 class PySysTest(ObscuroTest):
@@ -20,16 +19,13 @@ class PySysTest(ObscuroTest):
         key_storage = KeyStorage(self, web3)
         key_storage.deploy(network, account)
 
-        # run a background script to filter and collect events
-        stdout = os.path.join(self.output, 'subscriber.out')
-        stderr = os.path.join(self.output, 'subscriber.err')
-        script = os.path.join(self.input, 'subscriber.js')
-        args = []
-        args.extend(['--network_http', '%s' % network.connection_url(web_socket=False)])
-        args.extend(['--network_ws', '%s' % network.connection_url(web_socket=True)])
-        if self.is_obscuro(): args.extend(['--pk_to_register', '%s' % Properties().account3pk()])
-        self.run_javascript(script, stdout, stderr, args)
-        self.waitForGrep(file=stdout, expr='Starting task ...', timeout=10)
+        # run the javascript event log subscriber in the background
+        subscriber = EventLogSubscriber(self, network)
+        subscriber.run(
+            filter_topics=[web3.keccak(text='Stored(uint256)').hex()],
+            proxy=self.PROXY
+        )
+        subscriber.subscribe()
 
         # perform some transactions on the storage contract
         network.transact(self, web3, storage.contract.functions.store(0), account, storage.GAS)
@@ -43,7 +39,7 @@ class PySysTest(ObscuroTest):
         network.transact(self, web3, key_storage.contract.functions.setItem('k1', 101), account, storage.GAS)
 
         # wait and validate
-        self.waitForGrep(file=stdout, expr='Stored value = [0-9]{1,3}$', condition='== 6', timeout=20)
+        self.waitForGrep(file=subscriber.stdout, expr='Stored value = [0-9]{1,3}$', condition='== 6', timeout=20)
 
         expr_list = []
         expr_list.append('Stored value = 0')
@@ -52,5 +48,5 @@ class PySysTest(ObscuroTest):
         expr_list.append('Stored value = 3')
         expr_list.append('Stored value = 4')
         expr_list.append('Stored value = 100')
-        self.assertOrderedGrep(file=stdout, exprList=expr_list)
+        self.assertOrderedGrep(file=subscriber.stdout, exprList=expr_list)
 
