30847907b0397123562a95c538c7a4b14c8ef898
Moray Grieve <moray.grieve@me.com>
2022-10-11 13:19:51 +0100

    Add in the ethers test

---
+++ tests/generic/gen_cor_084/Input/poller.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const ethers = require('ethers')
+const commander = require('commander')
+const vk = require('viewing_key.js')
+
+require('console-stamp')(console, 'HH:MM:ss')
+
+function task() {
+  console.log('Starting task ...')
+  task1(0)
+}
+
+function task1(from) {
+  setTimeout(async function() {
+    const events = await contract.queryFilter('Stored', from, 'latest')
+    if (events.length) {
+      for (var i = 0, len = events.length; i < len; i+=1) {
+        log = interface.decodeEventLog('Stored', events[i].data, events[i].topics)
+        console.log('Stored value =', log.value.toNumber())
+        from = events[i].blockNumber+1
+      }
+    }
+    task1(from)
+  }, 2000)
+}
+
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('--network_http <value>', 'Http connection URL to the network')
+  .option('--network_ws <value>', 'Web socket connection URL to the network')
+  .option('--contract_address <value>', 'Web socket connection URL to the network')
+  .option('--contract_abi <value>', 'Web socket connection URL to the network')
+  .option('--pk_to_register <value>', 'Private key used to register for a viewing key (obscuro only)')
+  .parse(process.argv)
+
+const options = commander.opts()
+const provider = new ethers.providers.WebSocketProvider(options.network_ws)
+
+var json = fs.readFileSync(options.contract_abi)
+var abi = JSON.parse(json)
+const contract = new ethers.Contract(options.contract_address, abi, provider)
+const interface = new ethers.utils.Interface(abi)
+
+if (options.pk_to_register == true) {
+  wallet = ethers.Wallet(options.pk_to_register)
+  address = wallet.getAddress()
+  vk.generate_viewing_key(web3, options.network_http, address, options.pk_to_register, task)
+}
+else {
+  task()
+}
+
--- tests/generic/gen_cor_084/run.py
+++ tests/generic/gen_cor_084/run.py
@@ -1,11 +1,44 @@
-import pysys
-from pysys.constants import *
-from pysys.basetest import BaseTest
+import os, time
+from obscuro.test.obscuro_test import ObscuroTest
+from obscuro.test.contracts.storage.storage import Storage
+from obscuro.test.networks.factory import NetworkFactory
+from obscuro.test.utils.properties import Properties
+from obscuro.test.helpers.ws_proxy import WebServerProxy
 
-class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
-	def validate(self):
-		pass
-	
+class PySysTest(ObscuroTest):
+
+    def execute(self):
+        # connect to network
+        network = NetworkFactory.get_network(self.env)
+        web3, account = network.connect_account1(self)
+
+        # deploy the contract and dump out the abi
+        storage = Storage(self, web3, 100)
+        storage.deploy(network, account)
+
+        # go through a proxy to log websocket communications if needed
+        ws_url = network.connection_url(web_socket=True)
+        if self.PROXY: ws_url = WebServerProxy.create(self).run(ws_url, 'proxy.logs')
+
+        # run a background script to filter and collect events
+        stdout = os.path.join(self.output, 'poller.out')
+        stderr = os.path.join(self.output, 'poller.err')
+        script = os.path.join(self.input, 'poller.js')
+        args = []
+        args.extend(['--network_http', '%s' % network.connection_url(web_socket=False)])
+        args.extend(['--network_ws', ws_url])
+        args.extend(['--contract_address', '%s' % storage.contract_address])
+        args.extend(['--contract_abi', '%s' % storage.abi_path])
+        if self.is_obscuro(): args.extend(['--pk_to_register', '%s' % Properties().account3pk()])
+        self.run_javascript(script, stdout, stderr, args)
+        self.waitForGrep(file=stdout, expr='Starting task ...', timeout=10)
+
+        # perform some transactions with a sleep in between
+        for i in range(0, 5):
+           network.transact(self, web3, storage.contract.functions.store(i), account, storage.GAS)
+           time.sleep(1.0)
+
+        # wait and validate
+        self.waitForGrep(file=stdout, expr='Stored value = [0-9]$', condition='== 5', timeout=20)
+        self.assertOrderedGrep(file=stdout, exprList=['Stored value = %d' % x for x in range(0, 5)])
