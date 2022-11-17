f450f87bdeb2f2eed4aae4154a3e2538c96cce6f
Moray Grieve <moray.grieve@me.com>
2022-11-17 10:44:19 +0000

    Updated

--- tests/obscuro/obs_cor_011/run.py
+++ tests/obscuro/obs_cor_011/run.py
@@ -4,7 +4,6 @@ from obscuro.test.basetest import ObscuroNetworkTest
 class PySysTest(ObscuroNetworkTest):
 
     def execute(self):
-        sum_transactions = 0
         txs = self.get_latest_transactions(1)
         self.log.info('Last transactions are %s' % txs)
         if len(txs) >= 1:
@@ -12,22 +11,26 @@ class PySysTest(ObscuroNetworkTest):
             parent_hash = rollup['Header']['ParentHash']
             number = rollup['Header']['Number']
             num_tx = len(rollup['TxHashes'])
-            sum_transactions += num_tx
             self.log.info('Parent hash: %s, Number: %d, Ntx: %d' % (parent_hash, number, num_tx))
 
-            while number >= 1:
-                self.log.info('Calling to get rollup for number %d' % number)
+            count = 100
+            while count > 0:
+                self.log.info('Calling to get rollup for parent %s' % parent_hash)
                 rollup = self.get_rollup(parent_hash)
-                parent_hash = rollup['Header']['ParentHash']
-                number = rollup['Header']['Number']
-                num_tx = len(rollup['TxHashes'])
-                sum_transactions += num_tx
-                self.log.info('Parent hash: %s, Number: %d, Ntx: %d' % (parent_hash, number, num_tx))
+                if number == 0:
+                    self.log.info('Reached the genesis block so exiting')
+                    self.log.info(rollup)
+                    break
+                else:
+                    parent_hash = rollup['Header']['ParentHash']
+                    number = rollup['Header']['Number']
+                    num_tx = len(rollup['TxHashes'])
+                    self.log.info('Parent hash: %s, Number: %d, Ntx: %d' % (parent_hash, number, num_tx))
 
-            self.log.info('Calling to get rollup for number %d (the genesis block)' % number)
-            self.get_rollup(parent_hash) # not throwing an exception is a pass
+                count = count - 1
 
-        num_transactions = self.get_total_transactions()
-        self.log.info('Number of transactions is %d' % num_transactions)
-        self.log.info('Summed transactions is %d' % sum_transactions)
-        self.assertTrue(num_transactions == sum_transactions)
+            # in case we didn't reach the genesis block give is a go
+            parent_hash = '0x0000000000000000000000000000000000000000000000000000000000000000'
+            self.log.info('Calling to get rollup for parent %s' % parent_hash)
+            rollup = self.get_rollup(parent_hash)
+            self.log.info('Genesis rollup is %s' % rollup)
