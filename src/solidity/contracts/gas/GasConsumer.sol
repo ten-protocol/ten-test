777badf7ba5c8867ece6658ca9c7fbd21048f13d
Moray Grieve <moray.grieve@me.com>
2022-12-07 10:36:38 +0000

    Add in times methods

--- src/solidity/contracts/gas/GasConsumer.sol
+++ src/solidity/contracts/gas/GasConsumer.sol
@@ -11,19 +11,31 @@ contract GasConsumer {
     }
 
     function add_once() public {
-        1 + 1;
+        1 + 2;
     }
 
     function add_twice() public {
-        1 + 1 + 1;
+        1 + 2 + 3;
     }
 
     function add_thrice() public {
-        1 + 1 + 1 + 1;
+        1 + 2 + 3 + 4;
     }
 
     function add_three_times_with_a_long_name() public {
-        1 + 1 + 1 + 1;
+        1 + 2 + 3 + 4;
+    }
+
+    function times_once() public {
+        2 * 2;
+    }
+
+    function times_twice() public {
+        2 * 2 * 2;
+    }
+
+    function times_thrice() public {
+        2 * 2 * 2 * 2;
     }
 
     function destroy() public {
