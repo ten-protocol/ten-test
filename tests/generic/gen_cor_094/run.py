ec7769115da4ca5a1c8bf045941b5ed921858d6f
Moray Grieve <moray.grieve@me.com>
2022-12-08 16:37:44 +0000

    Add in destroy test

--- tests/generic/gen_cor_094/run.py
+++ tests/generic/gen_cor_094/run.py
@@ -1,9 +1,11 @@
+from pysys.constants import PASSED, FAILED
 from obscuro.test.basetest import GenericNetworkTest
 from obscuro.test.contracts.storage.storage import Storage
 from obscuro.test.networks.factory import NetworkFactory
 
 
 class PySysTest(GenericNetworkTest):
+    REFERENCE = 24000
 
     def execute(self):
         # connect to the network
@@ -11,10 +13,11 @@ class PySysTest(GenericNetworkTest):
         web3, account = network.connect_account1(self, web_socket=self.WEBSOCKET)
         self.log.info('Using account with address %s' % account.address)
 
-        # create the storage contract but don't deploy yet
+        # create the storage contract
+        b1 = web3.eth.get_balance(account.address)
         storage = Storage(self, web3, 100)
 
-        # estimate the deployment cost
+        # estimate and perform deployment
         build_tx = storage.contract.buildTransaction(
             {
                 'nonce': web3.eth.get_transaction_count(account.address),
@@ -26,5 +29,25 @@ class PySysTest(GenericNetworkTest):
         deploy_gas = web3.eth.estimate_gas(build_tx)
         self.log.info('Deployment gas estimate is %d' % deploy_gas)
 
-        # deploy the storage contract
-        storage.deploy(network, account)
+        tx = storage.deploy(network, account)
+        b2 = web3.eth.get_balance(account.address)
+        self.log.info('TX cost of deployment:        %d' % int(tx["gasUsed"]))
+        self.log.info('Actual cost of deployment:    %d' % (b1-b2))  # gas units * gas price (1000 by default)
+
+        # estimate and perform destruction
+        est_1 = storage.contract.functions.destroy().estimate_gas()
+        self.log.info("Estimate destruction:         %d" % est_1)
+
+        tx = network.transact(self, web3, storage.contract.functions.destroy(), account, storage.GAS_LIMIT)
+        b3 = web3.eth.get_balance(account.address)
+        self.log.info('TX cost of destruction:       %d' % int(tx["gasUsed"]))
+        self.log.info('Actual cost of destruction:   %d' % (b3-b2))  # gas units * gas price (1000 by default)
+        self.percentile_difference('destroy', int(tx["gasUsed"]), self.REFERENCE, 20)
+
+    def percentile_difference(self, text, result, reference, tolerance):
+        percentile = abs(((reference - result) / reference) * 100)
+        if percentile >= tolerance:
+            self.log.error('Percentile difference for %s is %d (%d compared to %d)' % (text, percentile, result, reference))
+            self.addOutcome(FAILED)
+        else:
+            self.addOutcome(PASSED)
