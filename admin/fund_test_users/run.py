4bf837e2b688ac0062930f6bec8929849b6be148
Moray Grieve <moray.grieve@me.com>
2023-01-23 17:02:36 +0000

    Removal of HOC and POC

--- admin/fund_test_users/run.py
+++ admin/fund_test_users/run.py
@@ -11,13 +11,9 @@ class PySysTest(ObscuroNetworkTest):
         Properties().account1_3pk(), Properties().account2_3pk(), Properties().account3_3pk(), Properties().account4_3pk()
     ]
     OBX = Web3().toWei(100, 'ether')
-    TOKENS = Web3().toWei(20, 'ether')
 
     def execute(self):
         network = NetworkFactory.get_network(self)
-        web3_distro, account_distro = network.connect(self, Properties().distro_account_pk(self.env))
-        hoc_address = Properties().l2_hoc_token_address(self.env)
-        poc_address = Properties().l2_poc_token_address(self.env)
 
         for user in self.USERS:
             web3_user, account_user = network.connect(self, user)
@@ -28,10 +24,3 @@ class PySysTest(ObscuroNetworkTest):
             self.log.info('OBX User balance before = %d ' % user_obx)
             self.fund_obx(network, account_user, self.OBX, web3_user)
             self.log.info('OBX User balance after = %d ' % user_obx)
-
-            if not self.is_obscuro_sim():
-                self.log.info('Funding HOC and POC to the test user account')
-                self.transfer_token(network, 'HOC', hoc_address, web3_distro, account_distro, account_user.address, self.TOKENS)
-                self.transfer_token(network, 'POC', poc_address, web3_distro, account_distro, account_user.address, self.TOKENS)
-
-
