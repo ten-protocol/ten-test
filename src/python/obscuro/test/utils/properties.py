dfec61c48f4bdeffeb0cbdf70ab253e262b77ee7
Moray Grieve <moray.grieve@me.com>
2023-01-26 10:19:03 +0000

    Some initial changes to support the new bridge + discord notifications  (#90)
    * Remove hoc and poc
    
    * Add in discord notifications

--- src/python/obscuro/test/utils/properties.py
+++ src/python/obscuro/test/utils/properties.py
@@ -84,29 +84,14 @@ class Properties:
     def faucet_url(self, key):
         return self.get('env.'+key, 'FaucetURL')
 
-    def management_bridge_address(self, key):
-        return self.get('env.'+key, 'ManagementBridgeAddress')
-
     def distro_account_pk(self, key):
         return self.get('env.'+key, 'DistroAccountPK')
 
     def l1_funded_account_pk(self, key):
         return self.get('env.'+key, 'L1FundedAccountPK')
 
-    def l1_hoc_token_address(self, key):
-        return self.get('env.'+key, 'L1TokenHOCContractAddress')
-
-    def l1_poc_token_address(self, key):
-        return self.get('env.'+key, 'L1TokenPOCContractAddress')
-
     def l2_funded_account_pk(self, key):
         return self.get('env.'+key, 'L2FundedAccountPK')
-
-    def l2_hoc_token_address(self, key):
-        return self.get('env.'+key, 'L2TokenHOCContractAddress')
-
-    def l2_poc_token_address(self, key):
-        return self.get('env.'+key, 'L2TokenPOCContractAddress')
 
     # infura related
     def infuraProjectID(self):
