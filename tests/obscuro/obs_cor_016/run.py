3eb52c678f646c00bfd271477b907c1ff8f527d9
Moray Grieve <moray.grieve@me.com>
2023-02-06 21:40:01 +0000

    Variable naming

--- tests/obscuro/obs_cor_016/run.py
+++ tests/obscuro/obs_cor_016/run.py
@@ -11,18 +11,18 @@ class PySysTest(ObscuroNetworkTest):
 
         # create the L1 and L2 connections, and users for the test
         funded = BridgeUser(self, props.l1_funded_account_pk(self.env), props.l2_funded_account_pk(self.env))
-        account1 = BridgeUser(self, props.account1pk(), props.account1pk())
+        accnt1 = BridgeUser(self, props.account1pk(), props.account1pk())
 
         # deploy the ERC20 token and set the L1 contract instance
         token = MintedERC20Token(self, funded.l1.web3, 'SmartyCoin', 'SCN', 10000)
         token.deploy(funded.l1.network, funded.l1.account, persist_nonce=False)
 
         funded.l1.set_token_from_address(token.contract_address)
-        account1.l1.set_token_from_address(token.contract_address)
+        accnt1.l1.set_token_from_address(token.contract_address)
 
         # transfer tokens from the funded account to account1, and account1 approves the bridge to transfer
-        funded.l1.transfer_token(account1.l1.account.address, 200)
-        account1.l1.approve_token(account1.l1.bridge.contract_address, 100)
+        funded.l1.transfer_token(accnt1.l1.account.address, 200)
+        accnt1.l1.approve_token(accnt1.l1.bridge.contract_address, 100)
 
         # whitelist the token, wait for it to be verified as finalised on L2
         _, xchain_msg = funded.l1.white_list_token()
@@ -32,16 +32,16 @@ class PySysTest(ObscuroNetworkTest):
         _, l2_token_address = funded.l2.relay_whitelist_message(xchain_msg)
         
         funded.l2.set_token_from_address(l2_token_address)
-        account1.l2.set_token_from_address(l2_token_address)
+        accnt1.l2.set_token_from_address(l2_token_address)
 
         # send tokens across the bridge, and wait for it to be verified as finalised on L2
-        _, xchain_msg = account1.l1.token_send_erc20(account1.l2.account.address, 10)
+        _, xchain_msg = accnt1.l1.token_send_erc20(accnt1.l2.account.address, 10)
         funded.l2.wait_for_message(xchain_msg)
 
         # print out the balances
-        balance = account1.l1.token.functions.balanceOf(account1.l1.acount.address).call()
+        balance = accnt1.l1.token.functions.balanceOf(accnt1.l1.acount.address).call()
         self.log.info('  Account1 ERC20 balance L1 = %d ' % balance)
-        balance = account1.l2.token.functions.balanceOf(account1.l2.account.address).call({"from":account1.l2.account.address})
+        balance = accnt1.l2.token.functions.balanceOf(accnt1.l2.account.address).call({"from":accnt1.l2.account.address})
         self.log.info('  Account1 ERC20 balance L2 = %d ' % balance)
 
 
