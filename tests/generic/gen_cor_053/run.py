e135cebf60c7b3a6d030b948b7b015471a6e8668
Moray Grieve <moray.grieve@me.com>
2023-03-21 17:26:48 +0000

    Add test to ensure only the deployer can see the contract balance (#108)
    * Add in new test
    
    * Run all three transfers
    
    * Added test
    
    * Updated titles

---
+++ tests/generic/gen_cor_053/run.py
@@ -0,0 +1,53 @@
+from obscuro.test.basetest import ObscuroNetworkTest
+from obscuro.test.networks.factory import NetworkFactory
+from obscuro.test.contracts.payable import ReceiveEther, SendEther
+from obscuro.test.utils.properties import Properties
+from obscuro.test.helpers.log_subscriber import AllEventsLogSubscriber
+
+
+class PySysTest(ObscuroNetworkTest):
+
+    def execute(self):
+        network = NetworkFactory.get_network(self)
+        web3, account = network.connect_account1(self)
+        balance = web3.eth.get_balance(account.address)
+        self.log.info('Balance account %.3f' % web3.fromWei(balance, 'ether'))
+
+        # deploy the contract and send eth to it
+        recv_contract = ReceiveEther(self, web3)
+        recv_contract.deploy(network, account)
+
+        send_contract = SendEther(self, web3)
+        send_contract.deploy(network, account)
+
+        # collect events
+        recv_sub = AllEventsLogSubscriber(self, network, recv_contract, stdout='recv_sub.out', stderr='recv_sub.err')
+        recv_sub.run(Properties().account4pk(), network.connection_url(), network.connection_url(web_socket=True))
+
+        send_sub = AllEventsLogSubscriber(self, network, send_contract, stdout='send_sub.out', stderr='send_sub.err')
+        send_sub.run(Properties().account4pk(), network.connection_url(), network.connection_url(web_socket=True))
+
+        # perform transfers
+        last_balance = 0
+        for fn in ['sendViaTransfer', 'sendViaSend', 'sendViaCall']:
+            self.log.info('Encoding and transferring using %s' % fn)
+
+            data = send_contract.contract.encodeABI(fn_name=fn, args=[recv_contract.address])
+            self.send(network, web3, send_contract, account, data, 0.5)
+
+            balance = web3.eth.get_balance(recv_contract.address)
+            self.log.info('Last balance: %s', web3.fromWei(last_balance, 'ether'))
+            self.log.info('New balance : %s', web3.fromWei(balance, 'ether'))
+
+            self.assertTrue(balance == last_balance + web3.toWei(0.5, 'ether'))
+            last_balance = balance
+
+    def send(self, network, web3, contract, account, data, amount):
+        tx = {
+            'value': web3.toWei(amount, 'ether'),
+            'gas': contract.GAS_LIMIT,
+            'data': data,
+            'gasPrice': 1000,
+            'to': contract.address
+        }
+        return network.tx(self, web3, tx, account)
---
+++ tests/obscuro/obs_cor_001/run.py
@@ -0,0 +1,48 @@
+from obscuro.test.basetest import ObscuroNetworkTest
+from obscuro.test.networks.factory import NetworkFactory
+from obscuro.test.contracts.payable import ReceiveEther
+from obscuro.test.networks.obscuro import Obscuro
+from obscuro.test.helpers.wallet_extension import WalletExtension
+
+
+class PySysTest(ObscuroNetworkTest):
+
+    def execute(self):
+        # the deployer goes through the test wallet extension
+        network_deploy = NetworkFactory.get_network(self)
+        web3_deploy, account_deploy = network_deploy.connect_account1(self)
+
+        # the user goes through their own instance of the wallet extension
+        network_user = Obscuro()
+        extension = WalletExtension.start(self, name='user')
+        network_user.PORT = extension.port
+        network_user.WS_PORT = extension.ws_port
+        web3_user, account_user = network_user.connect_account2(self)
+
+        # deploy the contract and send eth to it
+        contract = ReceiveEther(self, web3_deploy)
+        contract.deploy(network_deploy, account_deploy)
+        self.send(network_user, web3_user, account_user, contract, 0.5)
+
+        # the user should not be able to read the balance of the contract
+        raised = False
+        try:
+            balance = web3_user.eth.get_balance(contract.address)
+            self.log.info('Contract balance is %.3f' % web3_user.fromWei(balance, 'ether'))
+        except Exception as e:
+            raised = True
+        self.assertTrue(raised)
+
+        # the deployer should still be able to read their balance
+        balance = web3_deploy.eth.get_balance(contract.address)
+        self.log.info('Contract balance is %.3f' % web3_deploy.fromWei(balance, 'ether'))
+        self.assertTrue(balance == web3_deploy.toWei(0.5, 'ether'))
+
+    def send(self, network, web3, account, contract, amount):
+        tx = {
+            'to': contract.address,
+            'value': web3.toWei(amount, 'ether'),
+            'gas': contract.GAS_LIMIT,
+            'gasPrice': 1000
+        }
+        return network.tx(self, web3, tx, account)
