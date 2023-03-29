1785f89d038a769194dcabffef1012478c82114a
Moray Grieve <moray.grieve@me.com>
2023-03-29 16:36:36 +0100

    Interim update to add support for withdrawals

--- tests/generic/gen_cor_083/run.py
+++ tests/generic/gen_cor_083/run.py
@@ -27,19 +27,19 @@ class PySysTest(GenericNetworkTest):
         # perform some transactions on the storage contract
         network.transact(self, web3, storage.contract.functions.store(100), account, storage.GAS_LIMIT)
         network.transact(self, web3, storage.contract.functions.store(101), account, storage.GAS_LIMIT)
-        self.wait(float(self.block_time) * 1.1)
+        self.wait(float(self.block_time) * 2.0)
 
         # subscribe and transact
         subscriber.subscribe()
         network.transact(self, web3, storage.contract.functions.store(102), account, storage.GAS_LIMIT)
         network.transact(self, web3, storage.contract.functions.store(103), account, storage.GAS_LIMIT)
-        self.wait(float(self.block_time) * 1.1)
+        self.wait(float(self.block_time) * 2.0)
 
         # unsubscribe and transact
         subscriber.unsubscribe()
         network.transact(self, web3, storage.contract.functions.store(104), account, storage.GAS_LIMIT)
         network.transact(self, web3, storage.contract.functions.store(105), account, storage.GAS_LIMIT)
-        self.wait(float(self.block_time) * 1.1)
+        self.wait(float(self.block_time) * 2.0)
 
         # wait and validate
         self.waitForGrep(file=subscriber.stdout, expr='Stored value = [0-9]{3}$', condition='== 2', timeout=20)
