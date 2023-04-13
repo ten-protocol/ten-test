a01f18f7bafbce82560337727089d5605f8b8deb
Moray Grieve <moray.grieve@me.com>
2023-04-13 12:11:44 +0100

    Updates to relevancy check tests  (#116)
    * Updates for filtering
    
    * Completed test
    
    * Repurpose

--- tests/obscuro/obs_cor_003/run.py
+++ tests/obscuro/obs_cor_003/run.py
@@ -2,7 +2,7 @@ from pysys.constants import FAILED
 from obscuro.test.basetest import ObscuroNetworkTest
 from obscuro.test.networks.factory import NetworkFactory
 from obscuro.test.contracts.relevancy import Relevancy
-from obscuro.test.helpers.log_subscriber import FilterLogSubscriber
+from obscuro.test.helpers.log_subscriber import AllEventsLogSubscriber
 
 
 class PySysTest(ObscuroNetworkTest):
@@ -13,36 +13,31 @@ class PySysTest(ObscuroNetworkTest):
 
         # connect via the primary wallet extension used by the test in the order of
         # account1, account2, account3, account4
-        web3, account = network.connect_account4(self)
-        network.connect_account1(self)
-        network.connect_account2(self)
-        network.connect_account3(self)
+        web3_4, account4 = network.connect_account4(self)
+        web3_1, account1 = network.connect_account1(self)
+        web3_2, account2 = network.connect_account2(self)
+        web3_3, account3 = network.connect_account3(self)
 
         # deploy the storage contract as account 4
-        contract = Relevancy(self, web3)
-        contract.deploy(network, account)
+        contract = Relevancy(self, web3_4)
+        contract.deploy(network, account4)
 
         # run a background script to filter and collect events
-        subscriber = FilterLogSubscriber(self, network)
-        subscriber.run(
-            filter_address=contract.address,
-            filter_topics=[
-                web3.keccak(text='CallerIndexedAddress(address)').hex(),
-                web3.utils.sha3(account.address)
-            ]
-        )
+        subscriber = AllEventsLogSubscriber(self, network, contract,
+                                            stdout='subscriber.out',
+                                            stderr='subscriber.err')
+        subscriber.run()
 
-        # perform some transactions as account4, resulting in an event with the account 4 address included
+        # perform some transactions
         self.log.info('Performing transactions ... ')
-        network.transact(self, web3, contract.contract.functions.callerIndexedAddress(), account, contract.GAS_LIMIT)
+        network.transact(self, web3_4, contract.contract.functions.callerIndexedAddress(), account4, contract.GAS_LIMIT)
+        network.transact(self, web3_1, contract.contract.functions.callerIndexedAddress(), account1, contract.GAS_LIMIT)
+        network.transact(self, web3_2, contract.contract.functions.callerIndexedAddress(), account2, contract.GAS_LIMIT)
+        network.transact(self, web3_3, contract.contract.functions.callerIndexedAddress(), account3, contract.GAS_LIMIT)
         self.wait(float(self.block_time)*1.1)
 
-        # we would expect that given account4 vk is registered it can be decrypted
-        try:
-            self.waitForGrep(file='subscriber.out', expr='Received event: CallerIndexedAddress', timeout=10)
-        except:
-            self.log.error('Timed out waiting for CallerIndexedAddress event log in subscriber')
-            self.addOutcome(FAILED)
-        else:
-            self.assertGrep(file='subscriber.out', expr='Received event: CallerIndexedAddress')
+        # wait and assert that account4 does see this event
+        self.waitForGrep(file='subscriber.out', expr='Received event: CallerIndexedAddress')
+        self.assertLineCount(file='subscriber.out', expr='Received event: CallerIndexedAddress', condition='==1')
+
 
