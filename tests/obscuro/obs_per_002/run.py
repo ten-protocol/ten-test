f9ee6784b6b5795a4d4b6915d063e09e9eb68bb6
Moray Grieve <moray.grieve@me.com>
2023-04-26 13:29:48 +0100

    Interim update

--- tests/obscuro/obs_per_002/run.py
+++ tests/obscuro/obs_per_002/run.py
@@ -1,5 +1,6 @@
 import os, shutil, secrets
 from datetime import datetime
+from collections import OrderedDict
 from obscuro.test.contracts.error import Error
 from obscuro.test.basetest import GenericNetworkTest
 from obscuro.test.networks.factory import NetworkFactory
@@ -8,12 +9,11 @@ from obscuro.test.helpers.wallet_extension import WalletExtension
 
 
 class PySysTest(GenericNetworkTest):
-    ITERATIONS = 2500
-    CLIENTS = 1
+    ITERATIONS = 5000
     ACCOUNTS = 20
 
     def execute(self):
-        self.execute_run()
+        self.execute_graph()
 
     def execute_run(self):
         # connect to the network
@@ -64,12 +64,46 @@ class PySysTest(GenericNetworkTest):
         self.waitForGrep(file=stdout, expr='Starting client %s' % name, timeout=10)
 
     def execute_graph(self):
-        """Test method to develop graph creation. """
+        shutil.copy(os.path.join(self.input, 'data_one.bin'), os.path.join(self.output, 'data_one.bin'))
+        shutil.copy(os.path.join(self.input, 'data_two.bin'), os.path.join(self.output, 'data_two.bin'))
+
+        data1 = self.load_data('data_one.bin')
+        data2 = self.load_data('data_two.bin')
+        first = int(data1[0][1])
+        last = int(data2[-1][1])
+
+        data1_binned = self.bin_data(first, last, data1, OrderedDict())
+        with open(os.path.join(self.output, 'data_one.bins'), 'w') as fp:
+            for key, value in data1_binned.items(): fp.write('%d %d\n' % (key, value))
+
+        data2_binned = self.bin_data(first, last, data2, OrderedDict())
+        with open(os.path.join(self.output, 'data_two.bins'), 'w') as fp:
+            for key, value in data2_binned.items(): fp.write('%d %d\n' % (key, value))
+
+        with open(os.path.join(self.output, 'data.bin'), 'w') as fp:
+            for t in range(0, last+1-first): fp.write('%d %d\n' % (t, data1_binned[t]+data2_binned[t]))
+
         branch = GnuplotHelper.buildInfo().branch
-        duration = 93
-        average = 53.1234
+        duration = last - first
+        average = float(2*self.ITERATIONS) / float(duration) if duration != 0 else 0
         date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        shutil.copy(os.path.join(self.input, 'data_one.bin'), os.path.join(self.output, 'data.bin'))
         GnuplotHelper.graph(self, os.path.join(self.input, 'gnuplot.in'),
                             branch, date,
-                            str(self.mode), str(self.ITERATIONS), str(duration), '%.3f' % average)
+                            str(self.mode), str(2*self.ITERATIONS), str(duration), '%.3f' % average)
+
+    def load_data(self, file):
+        data = []
+        with open(os.path.join(self.output, file), 'r') as fp:
+            for line in fp.readlines():
+                nonce, timestamp = line.split()
+                data.append((nonce, int(timestamp)))
+        return data
+
+    def bin_data(self, first, last, data, binned_data):
+        b = OrderedDict()
+        for _, t in data: b[t] = 1 if t not in b else b[t] + 1
+        for t in range(first, last+1): binned_data[t-first] = 0 if t not in b else b[t]
+        return binned_data
+
+
+
