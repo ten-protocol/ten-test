bb6cb07d472ac02feddbd74fe869f9e6d51b5e14
Moray Grieve <moray.grieve@me.com>
2023-05-06 09:20:03 +0100

    Print balances

--- admin/print_balances/run.py
+++ admin/print_balances/run.py
@@ -1,3 +1,5 @@
+from pysys.constants import LOG_TRACEBACK
+from pysys.utils.logutils import BaseLogFormatter
 from obscuro.test.basetest import ObscuroNetworkTest
 from obscuro.test.utils.properties import Properties
 from obscuro.test.networks.factory import NetworkFactory
@@ -6,16 +8,9 @@ from obscuro.test.networks.factory import NetworkFactory
 class PySysTest(ObscuroNetworkTest):
 
     def execute(self):
-        PKS = [
-            Properties().pre_funded_pk(),
-            Properties().account1_1pk(), Properties().account2_1pk(),
-            Properties().account3_1pk(), Properties().account4_1pk(),
-            Properties().account1_2pk(), Properties().account2_2pk(),
-            Properties().account3_2pk(), Properties().account4_2pk(),
-            Properties().account1_3pk(), Properties().account2_3pk(),
-            Properties().account3_3pk(), Properties().account4_3pk(),
-            Properties().gg_appdev_pk(), Properties().gg_endusr_pk()
-        ]
-
         network = NetworkFactory.get_network(self)
-        for pk in PKS: network.connect(self, pk, check_funds=False)
+        for fn in Properties().accounts():
+            self.log.info("")
+            self.log.info("Checking funds for %s:", fn.__name__, extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
+            network.connect(self, fn(), check_funds=False)
+
