2ad8294651a79436d5778e730c233213350d8409
Moray Grieve <moray.grieve@me.com>
2023-05-09 10:48:18 +0100

    Remove

--- src/python/scripts/address_pk.py
+++ src/python/scripts/address_pk.py
@@ -2,7 +2,7 @@
 #
 from web3 import Web3
 
-pk = "69b32fb4662704820404866d4a4c9b2fcc8b4a796121939117b9cbb3ededb701"
+pk = ""
 account = Web3().eth.account.privateKeyToAccount(pk)
 print('Private key: %s' % pk)
 print('Address is: %s' % account.address)
