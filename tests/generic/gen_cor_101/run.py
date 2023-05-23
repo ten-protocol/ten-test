fcd8ced16e4ff0ab84d551a5036655b7db5b39f1
Moray Grieve <moray.grieve@me.com>
2023-05-23 12:10:00 +0100

    Add test for key storage persistence

---
+++ tests/generic/gen_cor_101/run.py
@@ -0,0 +1,43 @@
+import json
+from obscuro.test.basetest import GenericNetworkTest
+from obscuro.test.contracts.storage import KeyStorage
+from obscuro.test.networks.factory import NetworkFactory
+
+
+class PySysTest(GenericNetworkTest):
+
+    def execute(self):
+        # connect to network
+        network = NetworkFactory.get_network(self)
+        web3, account = network.connect_account1(self)
+
+        # get contract address, or deploy
+        address, abi = self.contract_db.get_contract(KeyStorage.CONTRACT, self.env)
+        if address is not None:
+            self.log.info('Using pre-deployed contract at address %s' % address)
+            if web3.eth.getCode(address) == b'':
+                self.log.warn('Contract address does not appear to be a deployed contract')
+                contract = self.deploy(network, web3, account)
+            else:
+                contract = web3.eth.contract(address=address, abi=abi)
+        else:
+            self.log.warn('Contract does not appear to be deployed')
+            contract = self.deploy(network, web3, account)
+
+        # retrieve the current value
+        value = contract.functions.getItem('key').call()
+        self.log.info('Call shows value %d' % value)
+
+        # set the value via a transaction and retrieve the new value
+        network.transact(self, web3, contract.functions.setItem('key', value+1), account, KeyStorage.GAS_LIMIT)
+        value_after = contract.functions.getItem('key').call()
+        self.log.info('Call shows value %d' % value_after)
+        self.assertTrue(value_after == value + 1)
+
+    def deploy(self, network, web3, account):
+        storage = KeyStorage(self, web3)
+        storage.deploy(network, account)
+        self.log.warn('Deployed %s contract to address %s' % (storage.CONTRACT, storage.address))
+        network.transact(self, web3, storage.contract.functions.setItem('key', 1), account, KeyStorage.GAS_LIMIT)
+        self.contract_db.insert(storage.CONTRACT, self.env, storage.address, json.dumps(storage.abi))
+        return storage.contract
