15c1c27591f02d9fa9c1dc9f65ddc6d97961305f
Moray Grieve <moray.grieve@me.com>
2023-08-17 20:11:33 +0100

    More refactoring

--- admin/print_balances/run.py
+++ admin/print_balances/run.py
@@ -2,13 +2,12 @@ from pysys.constants import LOG_TRACEBACK
 from pysys.utils.logutils import BaseLogFormatter
 from obscuro.test.basetest import ObscuroNetworkTest
 from obscuro.test.utils.properties import Properties
-from obscuro.test.networks.factory import NetworkFactory
 
 
 class PySysTest(ObscuroNetworkTest):
 
     def execute(self):
-        network = NetworkFactory.get_network(self)
+        network = self.get_network_connection()
         for fn in Properties().accounts():
             self.log.info("")
             self.log.info("Checking funds for %s:", fn.__name__, extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
