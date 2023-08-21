9898f95c2f0462b8ddeff48388fd2f33fd93eda5
Moray Grieve <moray.grieve@me.com>
2023-08-21 14:36:29 +0100

    More updates

--- src/javascript/modules/register.js
+++ src/javascript/modules/register.js
@@ -3,19 +3,17 @@ const http = require('http')
 
 module.exports = { register }
 
-function register(sign, host, port, user_id, address, callback) {
+async function register(sign, host, port, user_id, address, callback) {
     console.log('Registering', address)
     text_to_sign = "Register " + user_id + " for " + address
     signed_msg = sign(text_to_sign)
 
-    fetch(host + ':' + port + '/authenticate/?u=' + user_id, {
+    response = await fetch(host + ':' + port + '/authenticate/?u=' + user_id, {
       method: 'POST',
       headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
       body: JSON.stringify( {signature: signed_msg.signature, message: text_to_sign})
     })
-      .then(response => response.text())
-      .then((response) => {
-         console.log(response)
-         callback()
-    })
+    text = await response.text()
+    console.log(text)
+    callback()
 }
--- tests/obscuro/obs_cor_002/run.py
+++ tests/obscuro/obs_cor_002/run.py
@@ -19,24 +19,23 @@ class PySysTest(ObscuroNetworkTest):
         # run the javascript event log subscriber in the background for the other accounts
         self.subscribe(network, None, 'account4', contract)
         self.subscribe(network, Properties().account1pk(), 'account1', contract, new_wallet=True)
+        self.subscribe(network, Properties().account2pk(), 'account2', contract, new_wallet=True)
+        self.subscribe(network, Properties().account3pk(), 'account3', contract, new_wallet=True)
+        self.wait(float(self.block_time)*1.1)
 
-        # self.subscribe(network, Properties().account2pk(), 'account2', contract, new_wallet=True)
-        # self.subscribe(network, Properties().account3pk(), 'account3', contract, new_wallet=True)
-        # self.wait(float(self.block_time)*1.1)
-        #
-        # # perform some transactions
-        # self.log.info('Performing transactions ... ')
-        # network.transact(self, web3, contract.contract.functions.callerIndexedAddress(), account, contract.GAS_LIMIT)
-        # self.wait(float(self.block_time)*1.1)
-        #
-        # # wait and assert that account4 does see this event
-        # self.waitForGrep(file='subscriber_account4.out', expr='Received event: CallerIndexedAddress', timeout=90)
-        # self.assertGrep(file='subscriber_account4.out', expr='Received event: CallerIndexedAddress')
-        #
-        # # ensure that the other users don't see it
-        # self.assertGrep(file='subscriber_account1.out', expr='Received event: CallerIndexedAddress', contains=False)
-        # self.assertGrep(file='subscriber_account2.out', expr='Received event: CallerIndexedAddress', contains=False)
-        # self.assertGrep(file='subscriber_account3.out', expr='Received event: CallerIndexedAddress', contains=False)
+        # perform some transactions
+        self.log.info('Performing transactions ... ')
+        network.transact(self, web3, contract.contract.functions.callerIndexedAddress(), account, contract.GAS_LIMIT)
+        self.wait(float(self.block_time)*1.1)
+
+        # wait and assert that account4 does see this event
+        self.waitForGrep(file='subscriber_account4.out', expr='Received event: CallerIndexedAddress', timeout=90)
+        self.assertGrep(file='subscriber_account4.out', expr='Received event: CallerIndexedAddress')
+
+        # ensure that the other users don't see it
+        self.assertGrep(file='subscriber_account1.out', expr='Received event: CallerIndexedAddress', contains=False)
+        self.assertGrep(file='subscriber_account2.out', expr='Received event: CallerIndexedAddress', contains=False)
+        self.assertGrep(file='subscriber_account3.out', expr='Received event: CallerIndexedAddress', contains=False)
 
     def subscribe(self, network, pk_to_register, name, contract, new_wallet=False):
         if new_wallet:
@@ -45,6 +44,6 @@ class PySysTest(ObscuroNetworkTest):
         subscriber = AllEventsLogSubscriber(self, network, contract,
                                             stdout='subscriber_%s.out' % name,
                                             stderr='subscriber_%s.err' % name)
-        subscriber.run(pk_to_register, network.connection_url(web_socket=False), network.connection_url(web_socket=True))
+        subscriber.run(pk_to_register)
 
 
