1ca31d767c47ff1c4420d6d33cb763f74591aa35
Moray Grieve <moray.grieve@me.com>
2023-08-24 13:05:29 +0100

    Add separate connection

--- tests/generic/gen_cor_045/run.py
+++ tests/generic/gen_cor_045/run.py
@@ -9,28 +9,26 @@ class PySysTest(GenericNetworkTest):
 
     def execute(self):
         # deployment of contract
-        network = self.get_network_connection()
+        network_dev = self.get_network_connection(name='dev_connection')
+        web3_dev, account_dev = network_dev.connect_account2(self)
+        token = Token(self, web3_dev)
+        token.deploy(network_dev, account_dev)
+        game = Game(self, web3_dev, 9, token.address)
+        game.deploy(network_dev, account_dev)
 
         # we need to register the end user first for event relevancy rules to take effect
-        web3_usr, account_usr = network.connect_account1(self)
-
-        # deploy as the app developer
-        web3_dev, account_dev = network.connect_account2(self)
-        token = Token(self, web3_dev)
-        token.deploy(network, account_dev)
-
-        game = Game(self, web3_dev, 9, token.address)
-        game.deploy(network, account_dev)
+        network_usr = self.get_network_connection(name='usr_connection')
+        web3_usr, account_usr = network_usr.connect_account1(self)
 
         # run a background script to filter and collect events (register out-side of the script)
-        network.connect(self, private_key=Properties().gg_endusr_pk())
+        network_usr.connect(self, private_key=Properties().gg_endusr_pk())
 
         stdout = os.path.join(self.output, 'subscriber.out')
         stderr = os.path.join(self.output, 'subscriber.err')
         script = os.path.join(self.input, 'listener.js')
         args = []
-        args.extend(['--network_http', '%s' % network.connection_url(web_socket=False)])
-        args.extend(['--network_ws', '%s' % network.connection_url(web_socket=True)])
+        args.extend(['--network_http', '%s' % network_usr.connection_url(web_socket=False)])
+        args.extend(['--network_ws', '%s' % network_usr.connection_url(web_socket=True)])
         args.extend(['--erc_address', '%s' % token.address])
         args.extend(['--erc_abi', '%s' % token.abi_path])
         args.extend(['--game_address', '%s' % game.address])
@@ -40,7 +38,7 @@ class PySysTest(GenericNetworkTest):
 
         # approve the game to spend tokens on behalf of the user
         token_player = web3_usr.eth.contract(address=token.address, abi=token.abi)
-        network.transact(self, web3_usr, token_player.functions.approve(game.address, Web3().toWei(10, 'ether')), account_usr, game.GAS_LIMIT)
+        network_usr.transact(self, web3_usr, token_player.functions.approve(game.address, Web3().toWei(10, 'ether')), account_usr, game.GAS_LIMIT)
 
         # play the game
         game_player = web3_usr.eth.contract(address=game.address, abi=game.abi)
@@ -50,10 +48,12 @@ class PySysTest(GenericNetworkTest):
             balance = token_player.functions.balanceOf(account_usr.address).call({"from":account_usr.address})
             self.log.info('Allowance is %.3f', Web3().fromWei(allowance, 'ether'))
             self.log.info('Balance is %.3f', Web3().fromWei(balance, 'ether'))
-            network.transact(self, web3_usr, game_player.functions.attempt(i), account_usr, game.GAS_LIMIT)
+            network_usr.transact(self, web3_usr, game_player.functions.attempt(i), account_usr, game.GAS_LIMIT)
             self.waitForSignal(file='subscriber.out', filedir=self.output, expr='Your guess of %d' % i, timeout=20)
 
         # we should have won at some point, and all are guesses should have been logged
         self.waitForSignal(file='subscriber.out', filedir=self.output,
                            expr='Congratulations.*Your guess of.*has won you the prize of.*OGG', timeout=20)
+
+        self.assertLineCount(file='subscriber.out', filedir=self.output, expr='Your guess of', condition='==10')
 
