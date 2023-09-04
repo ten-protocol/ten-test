73086c2533f766bdce96c805bf3fb97f6ccab7c9
Moray Grieve <moray.grieve@me.com>
2023-09-04 13:43:49 +0100

    Add in logging

--- src/python/obscuro/test/baserunner.py
+++ src/python/obscuro/test/baserunner.py
@@ -75,7 +75,7 @@ class ObscuroRunnerPlugin():
 
                     runner.log.info('Registering account %s with the network', account.address)
                     response = self.__register(account, '%s/v1/authenticate/?u=%s' % (gateway_url, user_id), user_id)
-                    runner.log.info(response)
+                    runner.log.info(response.ok)
                     web3 = Web3(Web3.HTTPProvider('%s/v1/?u=%s' % (gateway_url, user_id)))
                     runner.addCleanupFunction(lambda: self.__print_cost(runner, port, web3, user_id))
 
