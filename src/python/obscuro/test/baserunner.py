93642247d65f81ae9b970f2e41cbd7a5cf55897f
Moray Grieve <moray.grieve@me.com>
2023-09-04 14:12:37 +0100

    More logging

--- src/python/obscuro/test/baserunner.py
+++ src/python/obscuro/test/baserunner.py
@@ -62,8 +62,12 @@ class ObscuroRunnerPlugin():
                 if self.is_obscuro():
                     hprocess, port = self.run_wallet(runner)
                     gateway_url = 'http://127.0.0.1:%d/' % port
+                    runner.log.info('Joining network using url %s', '%s/v1/join/' % gateway_url)
                     user_id = self.__join('%s/v1/join/' % gateway_url)
-                    self.__register(account, '%s/v1/authenticate/?u=%s' % (gateway_url, user_id), user_id)
+
+                    runner.log.info('Registering account %s with the network', account.address)
+                    response = self.__register(account, '%s/v1/authenticate/?u=%s' % (gateway_url, user_id), user_id)
+                    runner.log.info('Registration success was %s', response.ok)
                     web3 = Web3(Web3.HTTPProvider('%s/v1/?u=%s' % (gateway_url, user_id)))
                     runner.addCleanupFunction(lambda: self.__print_cost(runner,
                                                                         '%s/v1/authenticate/?u=%s' % (gateway_url, user_id),
