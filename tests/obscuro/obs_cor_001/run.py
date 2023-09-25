98ed3f4e012bd9ae45eef00f9a74f7c3c498f822
Moray Grieve <moray.grieve@me.com>
2023-09-25 11:51:05 +0100

    Send smaller amount

--- tests/obscuro/obs_cor_001/run.py
+++ tests/obscuro/obs_cor_001/run.py
@@ -16,7 +16,7 @@ class PySysTest(ObscuroNetworkTest):
         # deploy the contract and send eth to it
         contract = ReceiveEther(self, web3_deploy)
         contract.deploy(network_deploy, account_deploy)
-        self.send(network_user, web3_user, account_user, contract, 0.5)
+        self.send(network_user, web3_user, account_user, contract, 0.001)
 
         # the user should not be able to read the balance of the contract
         raised = False
@@ -30,7 +30,7 @@ class PySysTest(ObscuroNetworkTest):
         # the deployer should still be able to read their balance
         balance = web3_deploy.eth.get_balance(contract.address)
         self.log.info('Contract balance is %.3f', web3_deploy.fromWei(balance, 'ether'))
-        self.assertTrue(balance == web3_deploy.toWei(0.5, 'ether'))
+        self.assertTrue(balance == web3_deploy.toWei(0.001, 'ether'))
 
     def send(self, network, web3, account, contract, amount):
         tx = {
