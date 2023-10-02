6ee24a5bd6159cd401cbd97cbbf29d964e4c1db5
Moray Grieve <moray.grieve@me.com>
2023-10-02 12:58:33 +0100

    Add test for msg bus sending (#171)

--- tests/obscuro/obs_cor_023/run.py
+++ tests/obscuro/obs_cor_023/run.py
@@ -1,3 +1,5 @@
+import time
+from pysys.constants import TIMEDOUT
 from obscuro.test.basetest import ObscuroNetworkTest
 from obscuro.test.utils.bridge import BridgeUser
 from obscuro.test.utils.properties import Properties
@@ -14,12 +16,20 @@ class PySysTest(ObscuroNetworkTest):
         l1_balance_before = accnt1.l1.web3.eth.get_balance(accnt1.l1.account.address)
         l2_balance_before = accnt1.l2.web3.eth.get_balance(accnt1.l2.account.address)
 
-        self.log.info('Send native and wait for the xchain msg on the L2')
-        tx_receipt, xchain_msg = accnt1.l1.send_native(accnt1.l2.account.address, transfer)
-        accnt1.l2.wait_for_message(xchain_msg)
+        self.log.info('Send to the message bus to perform a transfer')
+        tx_receipt, logs = accnt1.l1.send_to_msg_bus(transfer)
+        self.assertTrue(logs[0]['args']['sender'] == props.l1_message_bus_address(self.env))
+        self.assertTrue(logs[0]['args']['receiver'] == accnt1.l1.account.address)
+        self.assertTrue(logs[0]['args']['amount'] == transfer)
 
         l1_balance_after = accnt1.l1.web3.eth.get_balance(accnt1.l1.account.address)
-        l2_balance_after = accnt1.l2.web3.eth.get_balance(accnt1.l2.account.address)
+        start = time.time()
+        while True:
+            l2_balance_after = accnt1.l2.web3.eth.get_balance(accnt1.l2.account.address)
+            if l2_balance_after > l2_balance_before: break
+            if time.time() - start > 10:
+                self.addOutcome(TIMEDOUT)
+                break
 
         self.log.info('Account balances;')
         self.log.info('   L1 before: %d', l1_balance_before)
