06e73678350e272372cf1ace46b4de7f24c2f911
Moray Grieve <moray.grieve@me.com>
2023-10-11 13:58:46 +0100

    Update config

--- admin/print_balances/run.py
+++ admin/print_balances/run.py
@@ -11,40 +11,15 @@ class PySysTest(ObscuroNetworkTest):
         if self.is_obscuro():
             self.log.info("")
             network = self.get_l1_network_connection()
-            self.log.info("Checking funds for %s:", "l1_funded_account_pk", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
+            self.log.info("Checking L1 funds for %s:", "l1_funded_account_pk", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
             web3, account = network.connect(self, Properties().l1_funded_account_pk(self.env), check_funds=False, verbose=False)
-
-            genesis = web3.eth.get_block('earliest')
-            self.log.info(genesis)
-
             balance = web3.fromWei(web3.eth.get_balance(account.address), 'ether')
             self.log.info('Account %s balance %.12f ETH', account.address, balance)
-
-            self.log.info("")
-            address = '0x9f7b0CDB121Af3923A98771c326b1aAC03A0D717'
-            self.log.info("Checking funds for %s:", "WORKER_ADDR DEV", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
-            balance = web3.fromWei(web3.eth.get_balance(address), 'ether')
-            self.log.info('Account %s balance %.12f ETH', address, balance)
-            self.log.info("")
-
-            self.log.info("")
-            address = '0xB872Bf306929B59c29B0029B3cccd1200153A612'
-            self.log.info("Checking funds for %s:", "WORKER_ADDR UAT", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
-            balance = web3.fromWei(web3.eth.get_balance(address), 'ether')
-            self.log.info('Account %s balance %.12f ETH', address, balance)
-            self.log.info("")
-
-            self.log.info("")
-            address = '0x5555E184dDC7de1A1fD0FF237CcA77338cE7162D'
-            self.log.info("Checking funds for %s:", "WORKER_ADDR SEP", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
-            balance = web3.fromWei(web3.eth.get_balance(address), 'ether')
-            self.log.info('Account %s balance %.12f ETH', address, balance)
-            self.log.info("")
 
         network = self.get_network_connection()
         for fn in Properties().accounts():
             self.log.info("")
-            self.log.info("Checking funds for %s:", fn.__name__, extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
+            self.log.info("Checking L2 funds for %s:", fn.__name__, extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
             web3, account = network.connect(self, fn(), check_funds=False, verbose=False)
             balance = web3.fromWei(web3.eth.get_balance(account.address), 'ether')
             self.log.info('Account %s balance %.6f ETH', account.address, balance)
