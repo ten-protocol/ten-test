99be3a71f6e715ac793bc4962c812955bf07ce67
Moray Grieve <moray.grieve@me.com>
2023-10-11 12:50:29 +0100

    Get worker address

--- admin/print_balances/run.py
+++ admin/print_balances/run.py
@@ -14,7 +14,12 @@ class PySysTest(ObscuroNetworkTest):
             self.log.info("Checking funds for %s:", "l1_funded_account_pk", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
             web3, account = network.connect(self, Properties().l1_funded_account_pk(self.env), check_funds=False, verbose=False)
             balance = web3.fromWei(web3.eth.get_balance(account.address), 'ether')
-            self.log.info('Account %s balance %.6f ETH', account.address, balance)
+            self.log.info('Account %s balance %.12f ETH', account.address, balance)
+
+            self.log.info("")
+            self.log.info("Checking funds for %s:", "WORKER_ADDR", extra=BaseLogFormatter.tag(LOG_TRACEBACK, 0))
+            balance = web3.fromWei(web3.eth.get_balance('0x9f7b0CDB121Af3923A98771c326b1aAC03A0D717'), 'ether')
+            self.log.info('Account %s balance %.12f ETH', account.address, balance)
             self.log.info("")
 
         network = self.get_network_connection()
