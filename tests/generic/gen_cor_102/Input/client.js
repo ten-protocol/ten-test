d1d6e221a4b39ca4893859885e7238182ab6cdac
Moray Grieve <moray.grieve@me.com>
2023-11-08 15:56:18 +0000

    Interim update

--- tests/generic/gen_cor_102/Input/client.js
+++ tests/generic/gen_cor_102/Input/client.js
@@ -5,10 +5,10 @@ const commander = require('commander')
 require('console-stamp')(console, 'HH:MM:ss')
 
 async function sendTransaction() {
-  const unsignedTrx = await contract.populateTransaction.setItem('Key', 1, { gasLimit: 400000 } )
+  const tx = await contract.populateTransaction.setItem('Key', 1, { gasLimit: 400000 } )
   console.log('Transaction created')
 
-  const txResponse = await signer.sendTransaction(unsignedTrx)
+  const txResponse = await wallet.sendTransaction(tx)
   console.log(`Transaction sent: ${txResponse.hash}`)
 
   const txReceipt = await txResponse.wait();
@@ -29,8 +29,8 @@ var json = fs.readFileSync(options.contract_abi)
 var abi = JSON.parse(json)
 
 const provider = new ethers.providers.WebSocketProvider(options.network_ws)
-const signer = new ethers.Wallet(options.private_key, provider)
-const contract = new ethers.Contract(options.address, abi, signer)
+const wallet = new ethers.Wallet(options.private_key, provider)
+const contract = new ethers.Contract(options.address, abi, wallet)
 
 console.log('Starting transactions')
 sendTransaction().then(() => console.log('Completed transactions'));
