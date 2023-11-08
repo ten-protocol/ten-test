f99e6ac71456375518681b31542468dfd91cd724
Moray Grieve <moray.grieve@me.com>
2023-11-08 15:10:17 +0000

    Move to general tests area

---
+++ tests/generic/gen_cor_102/Input/client.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const ethers = require('ethers')
+const commander = require('commander')
+
+require('console-stamp')(console, 'HH:MM:ss')
+
+async function sendTransaction() {
+  const unsignedTrx = await contract.populateTransaction.setItem('Key', 1, { gasLimit: 400000 } )
+  console.log('Transaction created')
+
+  const txResponse = await signer.sendTransaction(unsignedTrx)
+  console.log(`Transaction sent: ${txResponse.hash}`)
+
+  const txReceipt = await txResponse.wait();
+  console.log(`Transaction status: ${txReceipt.status}`)
+}
+
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('--network_ws <value>', 'Web socket connection URL to the network')
+  .option('--address <value>', 'Contract address')
+  .option('--contract_abi <value>', 'Contract ABI file')
+  .option('--private_key <value>', 'The account private key')
+  .parse(process.argv)
+
+const options = commander.opts()
+var json = fs.readFileSync(options.contract_abi)
+var abi = JSON.parse(json)
+
+const provider = new ethers.providers.WebSocketProvider(options.network_ws)
+const signer = new ethers.Wallet(options.private_key, provider)
+const contract = new ethers.Contract(options.address, abi, signer)
+
+console.log('Starting transactions')
+sendTransaction().then(() => console.log('Completed transactions'));
+
