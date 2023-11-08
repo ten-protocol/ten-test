b9d5f7362c950c422ec8ed426b7b2c30fc395dc9
Moray Grieve <moray.grieve@me.com>
2023-11-08 16:02:10 +0000

    Two approaches

--- tests/generic/gen_cor_102/Input/client.js
+++ tests/generic/gen_cor_102/Input/client.js
@@ -4,8 +4,25 @@ const commander = require('commander')
 
 require('console-stamp')(console, 'HH:MM:ss')
 
-async function sendTransaction() {
+async function sendTransaction1() {
   const tx = await contract.populateTransaction.setItem('Key', 1, { gasLimit: 400000 } )
+  console.log('Transaction created')
+
+  const txResponse = await wallet.sendTransaction(tx)
+  console.log(`Transaction sent: ${txResponse.hash}`)
+
+  const txReceipt = await txResponse.wait();
+  console.log(`Transaction status: ${txReceipt.status}`)
+}
+
+async function sendTransaction2() {
+  const gasPrice = await provider.getGasPrice();
+  const estimatedGas = await contract.estimateGas.setItem('Key', 1);
+
+  const tx = await contract.setItem('Key', 1, {
+    gasLimit: estimatedGas,
+    gasPrice: gasPrice,
+   });
   console.log('Transaction created')
 
   const txResponse = await wallet.sendTransaction(tx)
@@ -33,5 +50,5 @@ const wallet = new ethers.Wallet(options.private_key, provider)
 const contract = new ethers.Contract(options.address, abi, wallet)
 
 console.log('Starting transactions')
-sendTransaction().then(() => console.log('Completed transactions'));
+sendTransaction1().then(() => console.log('Completed transactions'));
 
