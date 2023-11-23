3ac4339e5ffcfedc020bdb65afa539ee06c0028a
Moray Grieve <moray.grieve@me.com>
2023-11-23 10:30:05 +0000

    Convert from hex

--- tests/ten/ten_cor_011/run.py
+++ tests/ten/ten_cor_011/run.py
@@ -10,7 +10,7 @@ class PySysTest(TenNetworkTest):
         if len(txs) >= 1:
             batch = self.get_batch_for_transaction(txs[0])
             parent_hash = batch['Header']['parentHash']
-            number = batch['Header']['number']
+            number = int(batch['Header']['number'], 16)
             num_tx = len(batch['TxHashes'])
             self.log.info('Parent hash: %s, Number: %d, Ntx: %d', parent_hash, number, num_tx)
 
@@ -24,13 +24,13 @@ class PySysTest(TenNetworkTest):
                     break
                 else:
                     parent_hash = batch['Header']['parentHash']
-                    number = batch['Header']['number']
+                    number = int(batch['Header']['number'], 16)
                     num_tx = len(batch['TxHashes'])
                     self.log.info('Parent hash: %s, Number: %d, Ntx: %d', parent_hash, number, num_tx)
 
                 count = count - 1
 
-            # in case we didn't reach the genesis block give is a go
+            # in case we didn't reach the genesis block give it a go
             parent_hash = '0x0000000000000000000000000000000000000000000000000000000000000000'
             self.log.info('Calling to get batch for parent %s', parent_hash)
             batch = self.get_batch(parent_hash)
