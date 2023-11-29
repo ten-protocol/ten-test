11117cb348b31556e7bfc64bc574ae07080c49b0
Moray Grieve <moray.grieve@me.com>
2023-11-29 11:33:28 +0000

    Updated

---
+++ tests/ten/ten_cor_006/run.py
@@ -0,0 +1,10 @@
+from ten.test.basetest import TenNetworkTest
+
+class PySysTest(TenNetworkTest):
+
+    def execute(self):
+        response = self.get_config()
+        self.log.info(response)
+
+        response = self.get_health()
+        self.log.info(response)
