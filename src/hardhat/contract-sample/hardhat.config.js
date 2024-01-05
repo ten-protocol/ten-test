62941c9ce2803959dce8cb76450e189bd56caf22
Moray Grieve <moray.grieve@me.com>
2024-01-05 13:54:13 +0000

    Updated

--- src/hardhat/contract-sample/hardhat.config.js
+++ src/hardhat/contract-sample/hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { APP_DEV_PK, TEN_TOKEN } = process.env;
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity:  "0.8.7",
+  defaultNetwork: "ten",
+  networks: {
+    ten: {
+      deploy: [ "scripts/" ],
+      chainId: 443,
+      url: `https://127.0.0.1:3000/v1/${TEN_TOKEN}`,
+      gasPrice: 2000000000,
+      accounts: [ `0x${APP_DEV_PK}` ]
+    },
+    hardhat: {
+      deploy: [ "scripts/" ],
+      chainId: 1337,
+      accounts: [
+        { "privateKey": `0x${APP_DEV_PK}`,  "balance": "174165200000000000" }
+      ]
+    }
+  }
 };
