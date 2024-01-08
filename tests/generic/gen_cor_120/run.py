3aa1c768f7f8f5a2232cc53ea27383d94309cf8d
Moray Grieve <moray.grieve@me.com>
2024-01-08 15:30:15 +0000

    Test now working

--- tests/generic/gen_cor_120/run.py
+++ tests/generic/gen_cor_120/run.py
@@ -1,3 +1,4 @@
+import re, json
 import os, shutil, copy
 from ten.test.basetest import GenericNetworkTest
 from ten.test.utils.properties import Properties
@@ -14,11 +15,10 @@ class PySysTest(GenericNetworkTest):
 
         # copy over and initialise the project
         shutil.copytree(self.input, project)
-        self.run_npm(args=['install', 'hardhat', '--yes'],
-                     stdout='npm_init_hh.out', stderr='npm_init_hh_err', working_dir=project)
-        self.run_npm(args=['install', 'dotenv', '--yes'],
-                     stdout='npm_init_de.out', stderr='npm_init_de_err', working_dir=project)
+        self.run_npm(args=['install', 'hardhat', '--yes'], stdout='npm1.out', stderr='npm1_err', working_dir=project)
+        self.run_npm(args=['install', 'dotenv', '--yes'], stdout='npm2.out', stderr='npm2_err', working_dir=project)
 
+        # deploy and get the address from the hardhat output
         environ = copy.deepcopy(os.environ)
         environ['PK'] = Properties().account1pk()
         environ['HOST'] = network.HOST
@@ -27,3 +27,19 @@ class PySysTest(GenericNetworkTest):
         self.run_npx(args=['hardhat', 'run', '--network', 'ten', 'scripts/deploy.js'],
                      working_dir=project, environ=environ, stdout='npx_deploy.out', stderr='npx_deploy.err')
 
+        address = 'undefined'
+        regex = re.compile('TestMaths deployed at (?P<address>.*)$', re.M)
+        with open(os.path.join(self.output, 'npx_deploy.out'), 'r') as fp:
+            for line in fp.readlines():
+                result = regex.search(line)
+                if result is not None: address = result.group('address')
+        self.log.info('TestMaths contract deployed at address %s', address)
+
+        # construct an instance of the contract from the address and abi
+        with open(os.path.join(self.output,'project','artifacts','contracts','TestMath.sol','TestMath.json')) as f:
+            contract = web3.eth.contract(address=address, abi=json.load(f)['abi'])
+
+        # make a call and assert we get the correct returned result
+        ret = int(contract.functions.testSquareRoot(25).call())
+        self.log.info('Returned value is %d', ret)
+        self.assertTrue(ret == 5)
