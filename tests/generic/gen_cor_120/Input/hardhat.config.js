a9ea86f18e6470a0709908e8e8cbe7e9db8fc494
Moray Grieve <moray.grieve@me.com>
2024-01-08 13:24:56 +0000

    Placeholder test (#217)
    * Add in ten and ten local
    
    * Add test
    
    * New placeholder test

--- tests/generic/gen_cor_120/Input/hardhat.config.js
+++ tests/generic/gen_cor_120/Input/hardhat.config.js
@@ -1,25 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
 
-const { APP_DEV_PK, TEN_TOKEN } = process.env;
+const { ACCOUNT_PK, GW_PORT, GW_TOKEN } = process.env;
 
 module.exports = {
   solidity:  "0.8.7",
   defaultNetwork: "hardhat",
   networks: {
-    ten: {
-      deploy: [ "scripts/" ],
-      chainId: 443,
-      url: `https://testnet.obscu.ro/v1/${TEN_TOKEN}`,
-      gasPrice: 2000000000,
-      accounts: [ `0x${APP_DEV_PK}` ]
-    },
     ten_local: {
       deploy: [ "scripts/" ],
       chainId: 443,
-      url: `http://127.0.0.1:3000/v1/${TEN_TOKEN}`,
+      url: `http://127.0.0.1:${GW_PORT}/v1/${GW_TOKEN}`,
       gasPrice: 2000000000,
-      accounts: [ `0x${APP_DEV_PK}` ]
+      accounts: [ `0x${ACCOUNT_PK}` ]
     },
     hardhat: {
       deploy: [ "scripts/" ],
