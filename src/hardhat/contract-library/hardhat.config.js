40b0cce1809b73e947a0af0001e96acee9158e47
Moray Grieve <moray.grieve@me.com>
2024-01-08 12:05:31 +0000

    Add in ten and ten local

--- src/hardhat/contract-library/hardhat.config.js
+++ src/hardhat/contract-library/hardhat.config.js
@@ -10,6 +10,13 @@ module.exports = {
     ten: {
       deploy: [ "scripts/" ],
       chainId: 443,
+      url: `https://testnet.obscu.ro/v1/${TEN_TOKEN}`,
+      gasPrice: 2000000000,
+      accounts: [ `0x${APP_DEV_PK}` ]
+    },
+    ten_local: {
+      deploy: [ "scripts/" ],
+      chainId: 443,
       url: `http://127.0.0.1:3000/v1/${TEN_TOKEN}`,
       gasPrice: 2000000000,
       accounts: [ `0x${APP_DEV_PK}` ]
