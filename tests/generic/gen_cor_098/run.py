68e8a5521789b86ed2ddcc05216982b5e4d72d1f
Moray Grieve <moray.grieve@me.com>
2024-01-19 11:06:37 +0000

    Updted

--- tests/generic/gen_cor_098/run.py
+++ tests/generic/gen_cor_098/run.py
@@ -1,9 +1,11 @@
+from pysys.constants import PASSED, FAILED
 from collections import namedtuple
 from web3.exceptions import TimeExhausted
 from ten.test.basetest import GenericNetworkTest
 from ten.test.contracts.storage import Storage
 
-Item = namedtuple("Item", "gas value nonce")
+# expect 0=fail, 1=timeout, 2=success
+Item = namedtuple("Item", "gas value nonce expect")
 
 class TransactionFailed(Exception):
     pass
@@ -30,32 +32,36 @@ class PySysTest(GenericNetworkTest):
 
         # these are the transactions to work through and their expectation on being mined or not
         stack = Stack()
-        stack.append(Item(int(0.9 * estimate_gas), 1, None))
-        stack.append(Item(int(0.8 * estimate_gas), 2, None))
-        stack.append(Item(int(0.6 * estimate_gas), 3, None))
-        stack.append(Item(int(0.4 * estimate_gas), 4, None))
-        stack.append(Item(int(0.1 * estimate_gas), 5, None))
+        stack.append(Item(int(0.9 * estimate_gas), 1, None, 0))
+        stack.append(Item(int(0.8 * estimate_gas), 2, None, 0))
+        stack.append(Item(int(0.6 * estimate_gas), 3, None, 0))
+        stack.append(Item(int(0.4 * estimate_gas), 4, None, 1))
+        stack.append(Item(int(0.1 * estimate_gas), 5, None, 1))
 
         # process the stack of transactions
         last_value = 0
         while not stack.isEmpty():
             item = stack.pop()
             if last_value != item.value: self.log.info("")
-            if item.nonce is None: item = Item(item.gas, item.value, self.nonce(web3, account))
+            if item.nonce is None: item = Item(item.gas, item.value, self.nonce(web3, account), item.expect)
 
             self.log.info('Running for item: %s', item)
             try:
                 self.log.info('Submitting the transaction value %d, gas %d, nonce %d', item.value, item.gas, item.nonce)
                 self.submit(account, contract, web3, item)
+                if item.expect == 2: self.addOutcome(PASSED)
+                else: self.addOutcome(FAILED)
                 self.log.info('Transaction was mined successfully')
             except TimeExhausted as e:
                 self.log.error(e)
+                if item.expect == 1: self.addOutcome(PASSED)
+                else: self.addOutcome(FAILED)
                 self.log.info('Inserting transaction to use same nonce but increase gas')
-                stack.insert(Item(estimate_gas, item.value, item.nonce))
+                stack.insert(Item(estimate_gas, item.value, item.nonce, 2))
             except TransactionFailed as e:
                 self.log.error(e)
-                self.log.info('Inserting transaction to use next nonce but increase gas')
-                stack.insert(Item(estimate_gas, item.value, None))
+                if item.expect == 0: self.addOutcome(PASSED)
+                else: self.addOutcome(FAILED)
             last_value = item.value
 
     def nonce(self, web3, account):
