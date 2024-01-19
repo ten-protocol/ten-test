c5bca6463924e6a121a08bb0988c0ae683df2cd4
Moray Grieve <moray.grieve@me.com>
2024-01-19 10:01:48 +0000

    Interim update

--- tests/generic/gen_cor_098/run.py
+++ tests/generic/gen_cor_098/run.py
@@ -1,7 +1,20 @@
+from collections import namedtuple
 from web3.exceptions import TimeExhausted
 from ten.test.basetest import GenericNetworkTest
 from ten.test.contracts.storage import Storage
 
+Item = namedtuple("Item", "gas value nonce")
+
+class TransactionFailed(Exception):
+    pass
+
+class Stack:
+    def __init__(self): self.items = []
+    def isEmpty(self): return self.items == []
+    def append(self, item): self.items.append(item)
+    def insert(self, item): return self.items.insert(0, item)
+    def pop(self): return self.items.pop(0)
+    def size(self): return len(self.items)
 
 class PySysTest(GenericNetworkTest):
 
@@ -15,36 +28,59 @@ class PySysTest(GenericNetworkTest):
         estimate_gas = contract.contract.functions.store(1).estimate_gas()
         self.log.info("Estimate gas:    %d", estimate_gas)
 
-        # submit the transaction to the function call with decreasing gas supplied
-        # if the transaction times out, use the same nonce to replace it and supply the
-        # full gas amount
-        for i in [0.9, 0.8, 0.6, 0.4, 0.1]:
-            self.log.info("")
-            self.log.info(" **** Submitting with fractional lower gas of %f **** ", i)
-            use_gas = int(i * estimate_gas)
-            nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
-            self.log.info("Transact gas:    %d", use_gas)
-            try:
-                self.log.info('Submitting the transaction with lower gas than the estimate')
-                self.submit(account, contract, nonce, web3, use_gas)
-            except TimeExhausted as e:
-                self.log.info('Time exhausted so increasing the gas to the estimate')
-                self.submit(account, contract, nonce, web3, estimate_gas)
-            except ValueError as e:
-                self.log.info(e['message'])
+        # these are the transactions to work through and their expectation on being mined or not
+        stack = Stack()
+        stack.append(Item(int(0.9 * estimate_gas), 1, None))
+        stack.append(Item(int(0.8 * estimate_gas), 2, None))
+        stack.append(Item(int(0.6 * estimate_gas), 3, None))
+        stack.append(Item(int(0.4 * estimate_gas), 4, None))
+        stack.append(Item(int(0.1 * estimate_gas), 5, None))
 
-    def submit(self, account, contract, nonce, web3, gas):
+        # process the stack of transactions
+        last_value = 0
+        while not stack.isEmpty():
+            item = stack.pop()
+            if last_value != item.value: self.log.info("")
+            if item.nonce is None: item = Item(item.gas, item.value, self.nonce(web3, account))
+
+            self.log.info('Running for item: %s', item)
+            try:
+                self.log.info('Submitting the transaction value %d, gas %d, nonce %d', item.value, item.gas, item.nonce)
+                self.submit(account, contract, web3, item)
+                self.log.info('Transaction was mined successfully')
+            except TimeExhausted as e:
+                self.log.error(e)
+                self.log.info('Inserting transaction to use same nonce but increase gas')
+                stack.insert(Item(estimate_gas, item.value, item.nonce))
+            except TransactionFailed as e:
+                self.log.error(e)
+                self.log.info('Inserting transaction to use next nonce but increase gas')
+                stack.insert(Item(estimate_gas, item.value, None))
+            last_value = item.value
+
+    def nonce(self, web3, account):
+        return self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
+
+    def submit(self, account, contract, web3, item):
         build_tx = contract.contract.functions.store(1).build_transaction(
-            { 'nonce':nonce, 'gasPrice':web3.eth.gas_price, 'gas':gas, 'chainId':web3.eth.chain_id }
-        )
+            {
+                'nonce': item.nonce,
+                'gasPrice': web3.eth.gas_price,
+                'gas': item.gas,
+                'chainId': web3.eth.chain_id
+        })
         signed_tx = account.sign_transaction(build_tx)
         tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
         tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash.hex(), timeout=10)
         if tx_receipt.status == 1:
-            self.log.info('Transaction successful with block hash %s', tx_receipt.blockHash.hex())
-            self.nonce_db.update(account.address, self.env, nonce, 'CONFIRMED')
+            self.nonce_db.update(account.address, self.env, item.nonce, 'CONFIRMED')
         else:
             try:
                 web3.eth.call(build_tx, block_identifier=tx_receipt.blockNumber)
             except Exception as e:
-                self.log.error('Replay call: %s', e)
+                raise TransactionFailed(e)
+            raise TransactionFailed('Failure processing transaction')
+        return tx_receipt
+
+
+
