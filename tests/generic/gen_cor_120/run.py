9ce65792648d839043dec87c6dc1ee3394517e88
Moray Grieve <moray.grieve@me.com>
2024-01-29 12:19:22 +0000

    Modes

--- tests/generic/gen_cor_120/run.py
+++ tests/generic/gen_cor_120/run.py
@@ -47,4 +47,5 @@ class PySysTest(GenericNetworkTest):
         # make a call and assert we get the correct returned result
         ret = int(contract.functions.doIt(2).call())
         self.log.info('Returned value is %d', ret)
-        self.assertTrue(ret == 3)
+        self.assertTrue(ret == 4)
+        
