905e9c605a5a4a3f11d506dab3d7e489725a2860
Moray Grieve <moray.grieve@me.com>
2024-01-30 11:01:02 +0000

    Modify and simplify test

--- tests/ten/ten_per_001/run.py
+++ tests/ten/ten_per_001/run.py
@@ -1,76 +1,76 @@
-import secrets, random, os, shutil
-from web3 import Web3
+import secrets, os
 from datetime import datetime
 from collections import OrderedDict
 from web3.exceptions import TimeExhausted
-from ten.test.contracts.error import Error
 from ten.test.basetest import TenNetworkTest
 from ten.test.utils.gnuplot import GnuplotHelper
 
 
 class PySysTest(TenNetworkTest):
-    ITERATIONS = 512      # don't exceed bulk loading more than 1024 (single client used)
+    ITERATIONS = 1024      # don't exceed bulk loading more than 1024 (single client used)
 
     def execute(self):
-        self.execute_run()
-
-    def execute_run(self):
-        # connect to the network
-        network = self.get_network_connection()
-        web3_deploy, account_deploy = network.connect_account1(self)
-
-        # we need to perform a transaction on the account to ensure the nonce is greater than zero for the
-        # following bulk loading (a hack to avoid count=0 being considered a new deployment and clearing the db)
-        error = Error(self, web3_deploy)
-        error.deploy(network, account_deploy)
-
         # use an ephemeral account so we don't need to manage nonce through persistence
-        web3, account = network.connect(self, private_key=secrets.token_hex())
-        self.distribute_native(account, amount=0.001)
-        accounts = [Web3().eth.account.from_key(x).address for x in [secrets.token_hex()]*25]
+        self.log.info('')
+        self.log.info('Creating ephemeral account to distribute funds from')
+        network = self.get_network_connection()
+        web3_send, account_send = network.connect(self, private_key=secrets.token_hex(), check_funds=False)
+        self.distribute_native(account_send, amount=0.001)
+
+        # connect a bunch of recipient clients to receive funds
+        self.log.info('')
+        self.log.info('Creating ephemeral account to receive funds')
+        web3_receive, account_receive = network.connect(self, private_key=secrets.token_hex(), check_funds=False)
 
         # bulk load transactions to the accounts, and wait for the last
+        self.log.info('')
         self.log.info('Creating and signing %d transactions', self.ITERATIONS)
-        value = web3.to_wei(0.0000000001, 'ether')
-        gas_price = web3.eth.gas_price
+        value = web3_send.to_wei(0.0000000001, 'ether')
+        gas_price = web3_send.eth.gas_price
         chain_id = network.chain_id()
 
         txs = []
         for nonce in range(0, self.ITERATIONS):
-            tx = self.create_signed_tx(network, account, nonce, random.choice(accounts), value, gas_price, chain_id)
+            tx = self.create_signed_tx(network, account_send, nonce, account_receive.address, value, gas_price, chain_id)
             txs.append((tx, nonce))
 
         self.log.info('Bulk sending transactions to the network')
-        balance_before = web3.from_wei(web3.eth.get_balance(account.address), 'gwei')
+        balance_before = web3_send.from_wei(web3_send.eth.get_balance(account_send.address), 'gwei')
         tx_hashes = []
         for tx in txs:
-            tx_hashes.append((network.send_transaction(self, web3, tx[1], account, tx[0], False, verbose=False), tx[1]))
-        balance_after_send = web3.from_wei(web3.eth.get_balance(account.address), 'gwei')
+            tx_hashes.append((network.send_transaction(self, web3_send, tx[1], account_send, tx[0], False, verbose=False), tx[1]))
+        balance_after_send = web3_send.from_wei(web3_send.eth.get_balance(account_send.address), 'gwei')
 
         try:
             self.log.info('Waiting for last transaction')
-            web3.eth.wait_for_transaction_receipt(tx_hashes[-1][0], timeout=30)
+            web3_send.eth.wait_for_transaction_receipt(tx_hashes[-1][0], timeout=30)
 
         except TimeExhausted as e:
             count = 0
             for tx_hash, nonce in tx_hashes:
-                try: web3.eth.wait_for_transaction_receipt(tx_hash, timeout=30)
+                try: web3_send.eth.wait_for_transaction_receipt(tx_hash, timeout=30)
                 except Exception as e:
-                    self.log.info('Looks like transaction %d failed', count)
+                    self.log.info('Transaction %d failed', count)
                     break
                 count = count + 1
 
-        balance_after_wait = web3.from_wei(web3.eth.get_balance(account.address), 'gwei')
-        self.log.info('Balance before send: %d', balance_before)
-        self.log.info('Balance after send:  %d', balance_after_send)
-        self.log.info('Balance after wait: %d', balance_after_wait)
+        # print out balances for information
+        self.log.info('')
+        self.log.info('Printing out balance information')
+        balance_after_wait = web3_send.from_wei(web3_send.eth.get_balance(account_send.address), 'gwei')
+        self.log.info('Sender balance before send: %d gwei', balance_before)
+        self.log.info('Sender balance after send:  %d gwei', balance_after_send)
+        self.log.info('Sender balance after wait: %d gwei', balance_after_wait)
+        balance_receiver = web3_receive.from_wei(web3_send.eth.get_balance(account_receive.address), 'gwei')
+        self.log.info('Receiver balance after wait: %d gwei', balance_receiver)
 
         # bin the data into timestamp intervals and log out to file
-        self.log.info('Constructing binned data from the transaction receipts')
+        self.log.info('')
+        self.log.info('Constructing binned data from the transaction receipts and graphing')
         bins = OrderedDict()
         for tx_hash, nonce in tx_hashes:
-            block_number_deploy = web3.eth.get_transaction(tx_hash).blockNumber
-            timestamp = int(web3.eth.get_block(block_number_deploy).timestamp)
+            block_number_deploy = web3_send.eth.get_transaction(tx_hash).blockNumber
+            timestamp = int(web3_send.eth.get_block(block_number_deploy).timestamp)
             bins[timestamp] = 1 if timestamp not in bins else bins[timestamp] + 1
 
         times = list(bins)
@@ -98,14 +98,3 @@ class PySysTest(TenNetworkTest):
               'chainId': chain_id
               }
         return network.sign_transaction(self, tx, nonce, account, False)
-
-    def execute_graph(self):
-        """Test method to develop graph creation. """
-        branch = GnuplotHelper.buildInfo().branch
-        duration = 114
-        average = 43.860
-        date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-        shutil.copy(os.path.join(self.input, 'data.bin'), os.path.join(self.output, 'data.bin'))
-        GnuplotHelper.graph(self, os.path.join(self.input, 'gnuplot.in'),
-                            branch, date,
-                            str(self.mode), str(self.ITERATIONS), str(duration), '%.3f'%average)
