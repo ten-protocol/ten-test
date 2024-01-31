787ca0b646e417ed0bd719bc63dff00ff26a3807
Moray Grieve <moray.grieve@me.com>
2024-01-31 10:26:53 +0000

    Fixing performance tests

--- tests/ten/ten_per_002/Input/client.py
+++ tests/ten/ten_per_002/Input/client.py
@@ -5,29 +5,28 @@ import logging, random, argparse, sys
 logging.basicConfig(format='%(asctime)s %(message)s', stream=sys.stdout, level=logging.INFO)
 
 
-def create_signed_tx(account, nonce, address, value, gas_price, chain_id):
+def create_signed_tx(account, nonce, address, value, gas_price, gas_limit, chain_id):
     """Create a signed transaction to transfer funds to an address. """
     tx = {'nonce': nonce,
           'to': address,
           'value': value,
-          'gas': 4 * 720000,
+          'gas': gas_limit,
           'gasPrice': gas_price,
           'chainId': chain_id
           }
     return account.sign_transaction(tx)
 
 
-def run(name, chainId, web3, account, num_accounts, num_iterations):
+def run(name, chainId, web3, account, num_accounts, num_iterations, amount, gas_limit):
     """Run a loop of bulk loading transactions into the mempool, draining, and collating results. """
-    accounts = [Web3().eth.account.from_key(x).address for x in [secrets.token_hex()]*num_accounts]
+    accounts = [Web3().eth.account.from_key(x).address for x in [secrets.token_hex() for y in range(0, num_accounts)]]
 
     logging.info('Creating and signing %d transactions', num_iterations)
-    value = web3.to_wei(0.0000000001, 'ether')
     gas_price = web3.eth.gas_price
 
     txs = []
     for i in range(0, num_iterations):
-        tx = create_signed_tx(account, i, random.choice(accounts), value, gas_price, chainId)
+        tx = create_signed_tx(account, i, random.choice(accounts), amount, gas_price, gas_limit, chainId)
         txs.append((tx, i))
 
     logging.info('Bulk sending transactions to the network')
@@ -52,6 +51,7 @@ def run(name, chainId, web3, account, num_accounts, num_iterations):
 
 
 if __name__ == "__main__":
+    print('HERE')
     parser = argparse.ArgumentParser(prog='event_listener')
     parser.add_argument('-u', '--network_http', help='Connection URL')
     parser.add_argument('-c', '--chainId', help='The network chain Id')
@@ -59,6 +59,8 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--num_accounts', help='Number of accounts to send funds to')
     parser.add_argument('-i', '--num_iterations', help='Number of iterations')
     parser.add_argument('-n', '--client_name', help='The logical name of the client')
+    parser.add_argument('-x', '--amount', help='The amount to send in wei')
+    parser.add_argument('-y', '--gas_limit', help='The gas limit')
     args = parser.parse_args()
 
     web3 = Web3(Web3.HTTPProvider(args.network_http))
@@ -66,4 +68,5 @@ if __name__ == "__main__":
     name = args.client_name
     logging.info('Starting client %s', name)
 
-    run(name, int(args.chainId), web3, account, int(args.num_accounts), int(args.num_iterations))
+    run(name, int(args.chainId), web3, account, int(args.num_accounts), int(args.num_iterations), int(args.amount), int(args.gas_limit))
+
