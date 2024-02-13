078193b43707c0712500def52f2f0798a4674dd9
Moray Grieve <moray.grieve@me.com>
2024-02-13 09:53:31 +0000

    Updating for new test as WIP

---
+++ tests/ten/ten_cor_050/Input/hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const { PK, HOST, PORT, TOKEN, API_KEY } = process.env;
+
+module.exports = {
+  solidity:  "0.8.7",
+  defaultNetwork: "ganache",
+  networks: {
+    ten: {
+      deploy: [ "scripts/" ],
+      chainId: 443,
+      url: `${HOST}:${PORT}/v1/${TOKEN}`,
+      gasPrice: 2000000000,
+      accounts: [ `0x${PK}` ]
+    },
+    ganache: {
+      deploy: [ "scripts/" ],
+      chainId: 1337,
+      url: `${HOST}:${PORT}`,
+      accounts: [ `0x${PK}` ]
+    }
+  }
+};
