f01e32870a0203d872eb8accb948de7a54f07111
Moray Grieve <moray.grieve@me.com>
2024-03-11 20:43:22 +0000

    HH test

---
+++ tests/ten/ten_cor_052/Input/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("ten-hardhat-plugin")
+
+const { PK, HOST, PORT } = process.env;
+
+module.exports = {
+  solidity:  "0.8.19",
+  defaultNetwork: "ten",
+  networks: {
+    ten: {
+      deploy: [ "scripts/" ],
+      chainId: 443,
+      url: `${HOST}:${PORT}/v1/`,
+      useGateway: true,
+      accounts: [ `0x${PK}` ]
+    }
+  }
+};
