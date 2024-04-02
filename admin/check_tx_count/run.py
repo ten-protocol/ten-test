4ec6692d81de76eeae13871ec9281b9f38aa9cb5
Moray Grieve <moray.grieve@me.com>
2024-04-02 16:06:22 +0100

    Add in storage of the tx count

--- admin/check_tx_count/run.py
+++ admin/check_tx_count/run.py
@@ -7,15 +7,17 @@ from ten.test.utils.properties import Properties
 class PySysTest(TenNetworkTest):
 
     def execute(self):
+        current_time = int(time.time())
+
         network = self.get_l1_network_connection(self.env)
         url = network.connection_url()
         web3 = Web3(Web3.HTTPProvider(url))
 
         sequencer_address = Properties().sequencer_address(key='ten.sepolia')
+        sequencer_count = web3.eth.get_transaction_count(sequencer_address)
 
-        for i in range(0,12):
-            sequencer_tx_count = web3.eth.get_transaction_count(sequencer_address)
-            self.log.info('Sequencer count is %d', sequencer_tx_count)
-            time.sleep(10)
+        self.log.info('Sequencer tx count %d', sequencer_count)
+        self.counts_db.insert_count('Sequencer', sequencer_address, self.env, current_time, sequencer_count)
+
 
 
