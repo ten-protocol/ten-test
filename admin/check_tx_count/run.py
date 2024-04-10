2f192d3fc3bde3e119b2d3316b271637a412053e
Moray Grieve <moray.grieve@me.com>
2024-04-10 08:57:08 +0100

    Fix tx count e2e health check  (#269)
    * Dont run perf and rob on deploy
    
    * Handle job being queued
    
    * Readme link and docker versions

--- admin/check_tx_count/run.py
+++ admin/check_tx_count/run.py
@@ -8,18 +8,32 @@ class PySysTest(TenNetworkTest):
 
     def execute(self):
         current_time = int(time.time())
-
         network = self.get_l1_network_connection(self.env)
         url = network.connection_url()
         web3 = Web3(Web3.HTTPProvider(url))
 
+        # recorded last then get last two hours of recorded tx counts - note that we expect a regular interval of
+        # recordings which might not be the case if the job is queued or stopped at any point
         sequencer_address = Properties().sequencer_address(key='ten.sepolia')
         sequencer_count = web3.eth.get_transaction_count(sequencer_address)
-
         self.log.info('Sequencer tx count %d', sequencer_count)
         self.counts_db.insert_count('Sequencer', sequencer_address, self.env, current_time, sequencer_count)
+        entries = self.counts_db.get_last_hour('Sequencer', self.env, current_time - 7200)
 
-        entries = self.counts_db.get_last_hour('Sequencer', self.env, current_time - 3900)
-        self.log.info('Last hour counts;')
-        for entry in entries: self.log.info('  %s %s', entry[0], entry[1])
-        self.assertTrue(int(entries[0][1]) > int(entries[-1][1]), assertMessage='Check increase in the tx count seen')
+        # make sure we have at least 2 entries recorded within the last two hours
+        if len(entries) >= 2:
+
+            # make sure of those recorded there is at least a one hour difference between the first and last
+            if (entries[0][0] - entries[-1][0]) >= 3600:
+                self.log.info('Retrieved counts;')
+                for entry in entries: self.log.info('  %s %s', entry[0], entry[1])
+
+                # assert on there being a difference in the tx count for this period
+                self.assertTrue(int(entries[0][1]) > int(entries[-1][1]), assertMessage='Check increase in the tx count seen')
+
+            else:
+                self.log.warn('Less than 1 hour recorded between retrieved entries ... skipping')
+
+        else:
+            self.log.warn('Less than 2 entries recorded over the last 2 hour period ... skipping')
+
