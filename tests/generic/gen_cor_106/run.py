5c736199be337bf758f319ee1d1a1bceb28c4a56
Moray Grieve <moray.grieve@me.com>
2024-04-30 10:40:03 +0100

    Debugging on UAT fails for newHeads (#274)
    * Put some debugging in
    
    * Put some debugging in
    
    * Put some debugging in
    
    * Put some debugging in

--- tests/generic/gen_cor_106/run.py
+++ tests/generic/gen_cor_106/run.py
@@ -7,6 +7,8 @@ class PySysTest(GenericNetworkTest):
     def execute(self):
         # connect to network
         network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+        self.log.info('Initial block number before starting task is %d', web3.eth.get_block_number())
 
         # run a background script to filter and collect events
         stdout = os.path.join(self.output, 'block_notifier.out')
@@ -16,10 +18,9 @@ class PySysTest(GenericNetworkTest):
         args.extend(['--network_ws', network.connection_url(web_socket=True)])
         self.run_javascript(script, stdout, stderr, args)
         self.waitForGrep(file=stdout, expr='Starting task ...', timeout=10)
-
-        # wait two block times
-        self.wait(float(self.block_time) * 2)
+        self.log.info('Initial block number after starting task is %d', web3.eth.get_block_number())
 
         # wait and validate we only see one block
-        self.waitForGrep(file='block_notifier.out', expr='Block =', condition='==1')
-
+        self.waitForGrep(file='block_notifier.out', expr='Block =', abortOnError=False, timeout=120)
+        self.assertLineCount(file='block_notifier.out', expr='Block =', condition='==1')
+        self.log.info('Final block number after running test is %d', web3.eth.get_block_number())
