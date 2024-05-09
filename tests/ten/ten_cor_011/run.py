69d3ffaa3252ee22c62e7f4fb003360967d78788
Moray Grieve <moray.grieve@me.com>
2024-05-09 13:22:47 +0100

    Just check last two

--- tests/ten/ten_cor_011/run.py
+++ tests/ten/ten_cor_011/run.py
@@ -1,30 +1,29 @@
+import math
 from ten.test.basetest import TenNetworkTest
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        page_sze = 100
+        page_sze = 3
 
         # get some public transaction data
         tx_count = self.tenscan_get_total_transactions()         # independent check on num tx transactions
-        num_pages = int((tx_count / page_sze) + 1)               # expected number of pages to call
-        residual = int(tx_count % page_sze)                      # the residual amount in the last page
-        start_page = (num_pages-5) if (num_pages > 5) else 0     # start requesting pages from this value
-        pages = range(start_page, num_pages)                     # the pages we are going to request
+        num_pages = math.ceil((tx_count / page_sze))             # expected number of pages to call
+        last = tx_count - (num_pages-1)*page_sze                 # the last page amount in the last page
+        pages = list(range(0, num_pages))[-2:]
         self.log.info('Total transaction count:      %d', tx_count)
+        self.log.info('Page size being used:         %d', page_sze)
         self.log.info('Total number of pages:        %d', num_pages)
-        self.log.info('Residual number in last page: %d', residual)
-        self.log.info('The pages requested:          %d', pages)
+        self.log.info('Last two pages requested:     %s', pages)
+        self.log.info('Last two page sizes:          %s, %s', page_sze, last)
 
         # get the pages
         total = 0
-        tx_data = []
-        pages = range(start_page, num_pages)
         for page in pages:
             tx_data = self.scan_get_public_transaction_data(page, page_sze)
             total = total + len(tx_data['TransactionsData'])
-            self.log.info('Processed page %d, total is %d', page, total)
+            self.log.info('Processed page %d, number returned %d', page, len(tx_data['TransactionsData']))
 
         # assert we get the expected amount over the last pages
-        self.assertTrue(len(tx_data) == (len(pages)-1)*page_sze + residual)
+        self.assertTrue(total == page_sze + last)
