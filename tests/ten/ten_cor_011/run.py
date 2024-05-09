5a3d9439dbc1799f60436750431b3dfeba637141
Moray Grieve <moray.grieve@me.com>
2024-05-09 11:27:43 +0100

    test just the last 5 pages

--- tests/ten/ten_cor_011/run.py
+++ tests/ten/ten_cor_011/run.py
@@ -7,19 +7,24 @@ class PySysTest(TenNetworkTest):
         page_sze = 100
 
         # get some public transaction data
-        tx_count = self.tenscan_get_total_transactions()
-        num_pages = int((tx_count / page_sze) + 1)
-        residual = int(tx_count % page_sze)
-        self.log.info('Total transaction count: %d', tx_count)
-        self.log.info('Total number of pages: %d', num_pages)
-        self.log.info('last residual page: %d', residual)
+        tx_count = self.tenscan_get_total_transactions()         # independent check on num tx transactions
+        num_pages = int((tx_count / page_sze) + 1)               # expected number of pages to call
+        residual = int(tx_count % page_sze)                      # the residual amount in the last page
+        start_page = (num_pages-5) if (num_pages > 5) else 0     # start requesting pages from this value
+        pages = range(start_page, num_pages)                     # the pages we are going to request
+        self.log.info('Total transaction count:      %d', tx_count)
+        self.log.info('Total number of pages:        %d', num_pages)
+        self.log.info('Residual number in last page: %d', residual)
+        self.log.info('The pages requested:          %d', pages)
 
+        # get the pages
         total = 0
         tx_data = []
-        for page in range(0, num_pages):
+        pages = range(start_page, num_pages)
+        for page in pages:
             tx_data = self.scan_get_public_transaction_data(page, page_sze)
             total = total + len(tx_data['TransactionsData'])
             self.log.info('Processed page %d, total is %d', page, total)
 
-        self.assertTrue(total == tx_count)
-        self.assertTrue(len(tx_data) == residual)
+        # assert we get the expected amount over the last pages
+        self.assertTrue(len(tx_data) == (len(pages)-1)*page_sze + residual)
