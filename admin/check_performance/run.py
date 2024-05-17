0be987be3427b9796931ed1c82bc9443d3a0f5fb
Moray Grieve <moray.grieve@me.com>
2024-05-17 14:05:54 +0100

    Circles

--- admin/check_performance/run.py
+++ admin/check_performance/run.py
@@ -13,7 +13,7 @@ class PySysTest(TenNetworkTest):
         for test in tests:
             with open(os.path.join(self.output, '%s.log' % test), 'w') as fp:
                 for entry in reversed(self.results_db.get_results(test=test, environment=self.env)):
-                    fp.write('%s %s\n' % (entry[0], entry[1]))
+                    fp.write('%s %s\n' % (self.epoch_to_fractional_doy(entry[0]), entry[1]))
         self.graph()
 
     def graph(self):
@@ -21,3 +21,9 @@ class PySysTest(TenNetworkTest):
         date = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         GnuplotHelper.graph(self, os.path.join(self.input, 'gnuplot.in'), branch, date, str(self.mode))
 
+    def epoch_to_fractional_doy(self, epoch_time):
+        date = datetime.utcfromtimestamp(epoch_time)
+        start_of_year = datetime(date.year, 1, 1)
+        delta = date - start_of_year
+        fractional_doy = delta.days + (delta.seconds + delta.microseconds / 1e6) / 86400
+        return fractional_doy
