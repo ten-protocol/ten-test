4e79a34226b82ea25a8acc049ce732cdaee5c358
Moray Grieve <moray.grieve@me.com>
2024-05-17 13:16:18 +0100

    Updated

--- admin/check_performance/run.py
+++ admin/check_performance/run.py
@@ -1,14 +1,23 @@
-import time, os
+import os
+from datetime import datetime
 from ten.test.basetest import TenNetworkTest
+from ten.test.utils.gnuplot import GnuplotHelper
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_04','ten_per_005',
+        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_004','ten_per_005',
                  'ten_per_006','ten_per_007','ten_per_008','ten_per_009']
 
         for test in tests:
             with open(os.path.join(self.output, '%s.log' % test), 'w') as fp:
                 for entry in reversed(self.results_db.get_results(test=test, environment='ten.uat')):
                     fp.write('%s %s\n' % (entry[0], entry[1]))
+        self.graph()
+
+    def graph(self):
+        branch = GnuplotHelper.buildInfo().branch
+        date = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        GnuplotHelper.graph(self, os.path.join(self.input, 'gnuplot.in'), branch, date, str(self.mode))
+
