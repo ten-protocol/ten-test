ad92c3523170bee11a3a1d52d714c98a46372350
Moray Grieve <moray.grieve@me.com>
2024-06-07 15:28:58 +0100

    Interim update

---
+++ tests/ten/ten_cor_024/Input/src/merkle.mjs
@@ -0,0 +1,24 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import { Command } from 'commander';
+import fs from 'fs';
+
+const program = new Command();
+
+program
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('--input <value>', 'Input file containing the leafs')
+  .parse(process.argv)
+
+  const options = program.opts()
+
+  fs.readFile(options.input, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const rows = data.split(/\r?\n/).filter(row => row.trim() !== '');
+        const result = rows.map(row => row.split(','));
+        const tree = StandardMerkleTree.of(result, ["string", "bytes32"]);
+        console.log('Merkle Root:', tree.root);
+    });
--- tests/ten/ten_cor_024/run.py
+++ tests/ten/ten_cor_024/run.py
@@ -1,10 +1,9 @@
-import base64, ast
+import base64, ast, os, shutil
 from web3 import Web3
 from eth_abi.abi import encode
 from ten.test.basetest import TenNetworkTest
 from ten.test.utils.bridge import BridgeUser
 from ten.test.utils.properties import Properties
-from ten.test.utils.merkle import MerkleTree
 
 
 class PySysTest(TenNetworkTest):
@@ -13,6 +12,7 @@ class PySysTest(TenNetworkTest):
         props = Properties()
         transfer_to = 1000000
         transfer_back = 4000000
+        project = os.path.join(self.output, 'project')
 
         accnt = BridgeUser(self, props.account1pk(), props.account1pk(), 'accnt1')
 
@@ -20,28 +20,35 @@ class PySysTest(TenNetworkTest):
         tx_receipt, _, xchain_msg = accnt.l1.send_native(accnt.l2.account.address, transfer_to)
         accnt.l2.wait_for_message(xchain_msg)
 
-        for i in range(0,3):
-            self.log.info('')
-            self.log.info('Send native from L2 to L1')
-            balance = accnt.l2.web3.eth.get_balance(accnt.l2.account.address)
-            self.log.info('  balance:         %d', balance)
+        self.log.info('')
+        self.log.info('Send native from L2 to L1')
+        balance = accnt.l2.web3.eth.get_balance(accnt.l2.account.address)
+        self.log.info('  balance:         %d', balance)
 
-            # perform the transfer
-            tx_receipt, value_transfer, _ = accnt.l2.send_native(accnt.l1.account.address, transfer_back)
-            block = accnt.l2.web3.eth.get_block(tx_receipt.blockNumber)
-            decoded = ast.literal_eval(base64.b64decode(block.crossChainTree).decode('utf-8'))
-            self.log.info('  value_transfer:   %s', list(value_transfer.values()))
-            self.log.info('  cross_chain:      %s', decoded)
+        # perform the transfer and log out the cross chain tree leafs (v=value transfer)
+        tx_receipt, value_transfer, _ = accnt.l2.send_native(accnt.l1.account.address, transfer_back)
+        block = accnt.l2.web3.eth.get_block(tx_receipt.blockNumber)
+        decoded = ast.literal_eval(base64.b64decode(block.crossChainTree).decode('utf-8'))
+        self.log.info('  value_transfer:   %s', list(value_transfer.values()))
+        self.log.info('  cross_chain:      %s', decoded)
+        self.log.info('  merkle_root:      %s', block.crossChainTreeHash)
+        with open(os.path.join(self.output, 'cross_chain.log'), 'w') as fp:
+            for entry in decoded: fp.write('%s,%s\n' % (entry[0], entry[1]))
 
-            # construct the hash of the value transfer and compare with the xchain tree
-            abi_types = ['address', 'address', 'uint256', 'uint64']
-            values = [value_transfer['sender'], value_transfer['receiver'],
-                      value_transfer['amount'], value_transfer['sequence']]
-            encoded_data = encode(abi_types, values)
-            hash_result = Web3.keccak(encoded_data).hex()
-            self.assertTrue(hash_result == decoded[0][1])
+        # construct the hash of the value transfer and compare with the xchain tree
+        abi_types = ['address', 'address', 'uint256', 'uint64']
+        values = [value_transfer['sender'], value_transfer['receiver'],
+                  value_transfer['amount'], value_transfer['sequence']]
+        encoded_data = encode(abi_types, values)
+        hash_result = Web3.keccak(encoded_data).hex()
+        self.assertTrue(hash_result == decoded[0][1])
 
-            merkle_tree = MerkleTree(["v"+x[1] for x  in decoded])
-            root = merkle_tree.getRootHash()
-            self.log.info('  merkle_root:      %s', root)
-            # assert the root is correct here once added into the block header
+        # pass the cross chain tree leafs to javascript so we can compute the tree and validate
+        # we have the same based on the root hash
+        shutil.copytree(self.input, project)
+        self.run_npm(args=['install', '--yes'], stdout='npm.out', stderr='npm.err', working_dir=project)
+        stdout = os.path.join(self.output, 'merkle.out')
+        stderr = os.path.join(self.output, 'merkle.err')
+        script = os.path.join(project, 'src/merkle.mjs')
+        args = ['--input', os.path.join(self.output, 'cross_chain.log')]
+        self.run_javascript(script, stdout, stderr, args)
