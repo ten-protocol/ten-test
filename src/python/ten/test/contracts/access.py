f1a7d1d367769dc977d1597fc314d963c8b4245d
Moray Grieve <moray.grieve@me.com>
2024-06-13 10:58:23 +0100

    Updated test

--- src/python/ten/test/contracts/access.py
+++ src/python/ten/test/contracts/access.py
@@ -4,9 +4,9 @@ from ten.test.contracts.default import DefaultContract
 
 class ContractA(DefaultContract):
     SOURCE = os.path.join(PROJECT.root, 'src', 'solidity', 'contracts', 'access', 'ContractA.sol')
-    CONTRACT = 'Error'
+    CONTRACT = 'ContractA'
 
 
 class ContractB(DefaultContract):
     SOURCE = os.path.join(PROJECT.root, 'src', 'solidity', 'contracts', 'access', 'ContractB.sol')
-    CONTRACT = 'Error'
+    CONTRACT = 'ContractB'
--- src/solidity/contracts/access/ContractA.sol
+++ src/solidity/contracts/access/ContractA.sol
@@ -7,4 +7,8 @@ contract ContractA {
     function setValue(uint256 _value) public {
         value = _value;
     }
+
+    function getValue() public view returns (uint256){
+        return value;
+    }
 }
