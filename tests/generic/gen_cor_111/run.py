946a8f08c47dde1ec5da65e32bd58c0074c30b51
Moray Grieve <moray.grieve@me.com>
2024-06-13 12:18:49 +0100

    Updated test

--- tests/generic/gen_cor_111/run.py
+++ tests/generic/gen_cor_111/run.py
@@ -19,13 +19,16 @@ class PySysTest(GenericNetworkTest):
         network.transact(self, web3, contractB.contract.functions.setContractA(contractA.address), account, contractB.GAS_LIMIT)
         tx_receipt = network.transact(self, web3, contractB.contract.functions.setContractAValue(100),
                                        account, contractB.GAS_LIMIT)
+        gas_without = tx_receipt.gasUsed
         self.log.info('Call shows value %d', contractA.contract.functions.getValue().call())
-        self.log.info('Without access list gas: %d' % tx_receipt.gasUsed)
+        self.log.info('Without access list gas: %d' % gas_without)
 
         # Create an access list and repeat
         access_list = [ { 'address': contractA.address, 'storageKeys': []},
                         { 'address': contractB.address, 'storageKeys': []} ]
         tx_receipt = network.transact(self, web3, contractB.contract.functions.setContractAValue(200),
                                       account, contractB.GAS_LIMIT, access_list=access_list)
+        gas_with = tx_receipt.gasUsed
         self.log.info('Call shows value %d', contractA.contract.functions.getValue().call())
-        self.log.info('With access list gas: %d' % tx_receipt.gasUsed)
+        self.log.info('With access list gas: %d' % gas_with)
+        self.assertTrue(gas_with < gas_without, assertMessage='Gas with access list should be less')
