1b5e9ebd7f92b7f62d99f23ca666c190b5e87782
Moray Grieve <moray.grieve@me.com>
2024-06-25 15:35:27 +0100

    Tweak uat load

--- tests/ten/ten_rob_005/run.py
+++ tests/ten/ten_rob_005/run.py
@@ -6,7 +6,7 @@ from ten.test.contracts.emitter import EventEmitter
 
 class PySysTest(TenNetworkTest):
     CLIENTS = 10         # number of transactors and subscribers
-    TRANSACTIONS = 1000  # number of txs the transactors will perform
+    TRANSACTIONS = 2500  # number of txs the transactors will perform
 
     def execute(self):
         # connect to network on the primary gateway and deploy contract
