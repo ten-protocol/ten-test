1682787cadd8c02bc35b558625a5be239dbdd7dd
Moray Grieve <moray.grieve@me.com>
2024-07-02 10:27:07 +0100

    Flush to file

--- tests/generic/gen_cor_073/run.py
+++ tests/generic/gen_cor_073/run.py
@@ -17,13 +17,16 @@ class PySysTest(GenericNetworkTest):
         # run a background script to filter and collect events
         stdout = os.path.join(self.output, 'poller.out')
         stderr = os.path.join(self.output, 'poller.err')
+        log_file = os.path.join(self.output, 'poller.log')
         script = os.path.join(self.input, 'poller.js')
         args = []
         args.extend(['--network_ws', network.connection_url(web_socket=True)])
         args.extend(['--address', '%s' % storage.address])
         args.extend(['--contract_abi', '%s' % storage.abi_path])
+        args.extend(['--contract_abi', '%s' % storage.abi_path])
+        args.extend(['--log_file', '%s' % log_file])
         self.run_javascript(script, stdout, stderr, args)
-        self.waitForGrep(file=stdout, expr='Starting task ...', timeout=10)
+        self.waitForGrep(file=log_file, expr='Starting task ...', timeout=10)
 
         # perform some transactions with a sleep in between
         for i in range(0,5):
@@ -31,8 +34,8 @@ class PySysTest(GenericNetworkTest):
         self.wait(float(self.block_time) * 1.1)
 
         # wait and validate
-        self.waitForGrep(file=stdout, expr='Stored value = 4', timeout=20)
-        self.assertOrderedGrep(file=stdout, exprList=['Stored value = %d' % x for x in range(0, 5)])
+        self.waitForGrep(file=log_file, expr='Stored value = 4', timeout=20)
+        self.assertOrderedGrep(file=log_file, exprList=['Stored value = %d' % x for x in range(0, 5)])
 
         # validate correct count
-        self.assertLineCount(file=stdout, expr='Stored value', condition='== 5')
+        self.assertLineCount(file=log_file, expr='Stored value', condition='== 5')
