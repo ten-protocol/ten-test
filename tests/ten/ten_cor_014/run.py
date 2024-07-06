caed71bc56271be294c7a28190ce156f2129455a
Moray Grieve <moray.grieve@me.com>
2024-07-06 08:52:18 +0100

    Add back in test for personal txs (#311)

--- tests/ten/ten_cor_014/run.py
+++ tests/ten/ten_cor_014/run.py
@@ -16,10 +16,13 @@ class PySysTest(TenNetworkTest):
         pk = secrets.token_hex(32)
         web3_usr, account_usr = network.connect(self, private_key=pk, check_funds=False)
         self.distribute_native(account_usr, network.ETH_ALLOC_EPHEMERAL)
-        network.transact(self, web3_usr, storage.contract.functions.store(1), account_usr, storage.GAS_LIMIT)
-        network.transact(self, web3_usr, storage.contract.functions.store(2), account_usr, storage.GAS_LIMIT)
+        tx_receipt1 = network.transact(self, web3_usr, storage.contract.functions.store(1), account_usr, storage.GAS_LIMIT)
+        tx_receipt2 = network.transact(self, web3_usr, storage.contract.functions.store(2), account_usr, storage.GAS_LIMIT)
 
         # list the personal transactions for the user
-        txs = self.scan_list_personal_transactions(address = account_usr.address, offset=0, size=5)
+        txs = self.scan_list_personal_transactions(url=network.connection_url(), address = account_usr.address, offset=0, size=5)
+        tx_hashs = [x['blockHash'] for x in txs['Receipts']]
         self.assertTrue(txs != None, abortOnError=True, assertMessage='Returned list is None')
         self.assertTrue(len(txs) == 2)
+        self.assertTrue(tx_receipt1.blockHash.hex() in tx_hashs)
+        self.assertTrue(tx_receipt2.blockHash.hex() in tx_hashs)
