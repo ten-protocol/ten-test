88c545afc9f9a10df02f001eded4a245068b5ee0
Moray Grieve <moray.grieve@me.com>
2024-07-08 11:05:44 +0100

    Just build on target (#312)

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -29,18 +29,6 @@ cd $src_path/tools/walletextension/main
 echo Building for target platform
 go build -o ${script_path}/artifacts/wallet_extension/wallet_extension .
 
-echo Building GOOS=darwin GOARCH=amd64
-env GOOS=darwin GOARCH=amd64 go build -o ${script_path}/artifacts/wallet_extension/wallet_extension_macos_amd64 .
-
-echo Building GOOS=darwin GOARCH=arm64
-env GOOS=darwin GOARCH=arm64 go build -o ${script_path}/artifacts/wallet_extension/wallet_extension_macos_arm64 .
-
-echo Building GOOS=windows GOARCH=amd64
-env GOOS=windows GOARCH=amd64 go build -o ${script_path}/artifacts/wallet_extension/wallet_extension_win_amd64.exe .
-
-echo Building GOOS=linux GOARCH=amd64
-env GOOS=linux GOARCH=amd64 go build -o ${script_path}/artifacts/wallet_extension/wallet_extension_linux_amd64
-
 # run the abigen to create the contract ABIs
 cd $src_path/contracts
 npm install
