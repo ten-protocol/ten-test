57f21ea3b48a1551b9df3ea72d2c2f8bcbe4a575
Moray Grieve <moray.grieve@me.com>
2024-07-10 10:52:38 +0100

    Complete test

--- tests/ten/ten_cor_015/run.py
+++ tests/ten/ten_cor_015/run.py
@@ -24,18 +24,20 @@ class PySysTest(TenNetworkTest):
         contract_2 = Relevancy.clone(web3_2, account_2, contract_1)
 
         # subscribe for events from the contract through gateway account 1 is using
-        self.subscribe(network_1, contract_1.address, contract_1.abi_path)
-
-        # account 2 transacts, passing an indexed account address of the unseen account, and then transacts again
-        # using the account address of the seen account (account 1)
-        network_2.transact(self, web3_2, contract_2.contract.functions.indexedAddressAndNumber(unseen), account_2, contract_2.GAS_LIMIT)
-        network_2.transact(self, web3_2, contract_2.contract.functions.indexedAddressAndNumber(account_1.address), account_2, contract_2.GAS_LIMIT)
+        self.subscribe(network_1, contract_1.address, contract_1.abi_path, 'subscriber1')
+        self.subscribe(network_2, contract_2.address, contract_2.abi_path, 'subscriber2')
 
         # account 1 transacts, passing an indexed account address of the unseen account, and then transacts again
         # using the account address of the seen account (account 2)
         network_1.transact(self, web3_1, contract_1.contract.functions.indexedAddressAndNumber(unseen), account_1, contract_2.GAS_LIMIT)
         network_1.transact(self, web3_1, contract_1.contract.functions.indexedAddressAndNumber(account_2.address), account_1, contract_1.GAS_LIMIT)
 
-    def subscribe(self, network, address, abi_path):
-        subscriber = AllEventsLogSubscriber(self, network, address, abi_path, stdout='subscriber.out', stderr='subscriber.err')
+        self.waitForGrep(file='subscriber1.out', expr='Received event: IndexedAddressAndNumber', timeout=20)
+        self.assertLineCount(file='subscriber1.out', expr='Received event: IndexedAddressAndNumber', condition='==1')
+
+        self.waitForGrep(file='subscriber2.out', expr='Received event: IndexedAddressAndNumber', condition='==2', timeout=20)
+        self.assertLineCount(file='subscriber2.out', expr='Received event: IndexedAddressAndNumber', condition='==2')
+
+    def subscribe(self, network, address, abi_path, name):
+        subscriber = AllEventsLogSubscriber(self, network, address, abi_path, stdout='%s.out'%name, stderr='%s.err'%name)
         subscriber.run()
