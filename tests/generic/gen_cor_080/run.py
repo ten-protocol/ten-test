ec73d111eae4af797af24a024f40bc470ebbcbbd
Moray Grieve <moray.grieve@me.com>
2024-07-15 10:26:48 +0100

    Make sure block number increases

--- tests/generic/gen_cor_080/run.py
+++ tests/generic/gen_cor_080/run.py
@@ -1,3 +1,4 @@
+import time
 from ten.test.basetest import GenericNetworkTest
 from ten.test.contracts.storage import Storage
 from ten.test.helpers.log_subscriber import FilterLogSubscriber
@@ -14,10 +15,19 @@ class PySysTest(GenericNetworkTest):
         storage = Storage(self, web3, 100)
         storage.deploy(network, account)
 
-        network.transact(self, web3, storage.contract.functions.store(0), account, storage.GAS_LIMIT)
+        # do a transaction and wait for the block number to increase
+        tx = network.transact(self, web3, storage.contract.functions.store(0), account, storage.GAS_LIMIT)
+        block_number = web3.eth.get_block_number()
+        start = time.time()
+        while block_number <= tx.blockNumber:
+            if time.time() - start > (3*float(self.block_time)): raise TimeoutError('Timed out waiting for block number to increase')
+            self.log.info('Waiting for the block number to increase (last %d, current %d)' %(tx.blockNumber, block_number))
+            time.sleep(float(self.block_time))
+            block_number = web3.eth.get_block_number()
+
+        # do some more transactions and then kick off the event subscriber
         tx = network.transact(self, web3, storage.contract.functions.store(1), account, storage.GAS_LIMIT)
         network.transact(self, web3, storage.contract.functions.store(2), account, storage.GAS_LIMIT)
-        self.wait(float(self.block_time) * 2)
 
         # run the javascript event log subscriber in the background
         subscriber = FilterLogSubscriber(self, network)
