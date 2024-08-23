ff83c7db7aab4871969966fc5331a46059024124
Moray Grieve <moray.grieve@me.com>
2024-08-23 15:54:40 +0100

    Addition

--- tests/ten/ten_cor_027/run.py
+++ tests/ten/ten_cor_027/run.py
@@ -9,6 +9,7 @@ class PySysTest(TenNetworkTest):
     def execute(self):
         props = Properties()
         transfer = 1010
+        transfer_back = 1000
 
         # the account of the user for the bridges
         accnt1 = BridgeUser(self, props.account1pk(), props.account1pk(), 'accnt1')
@@ -22,15 +23,25 @@ class PySysTest(TenNetworkTest):
 
         # send native from the L1 to the L2
         self.log.info('Send native and wait for the xchain msg on the L2')
-        tx_receipt, _, xchain_msg = accnt1.l1.send_native(accnt1.l2.account.address, transfer)
+        _, _, xchain_msg = accnt1.l1.send_native(accnt1.l2.account.address, transfer)
         accnt1.l2.wait_for_message(xchain_msg)
 
-        # we should see the log
+        # send native from the L2 to the L1
+        self.log.info('Send native from L2 to L1')
+        accnt1.l2.send_native(accnt1.l1.account.address, transfer_back)
+
+        # we should see the logs from both of the value transfer events
         expr_list = []
         expr_list.append('Log transfer sender = %s' % accnt1.l1.bridge.address)
         expr_list.append('Log transfer receiver = %s' % accnt1.l1.account.address)
         expr_list.append('Log transfer amount = %d' % transfer)
         self.assertOrderedGrep(file='query_l1.log', exprList=expr_list)
+
+        expr_list = []
+        expr_list.append('Log transfer sender = %s' % accnt1.l2.bridge.address)
+        expr_list.append('Log transfer receiver = %s' % accnt1.l1.account.address)
+        expr_list.append('Log transfer amount = %d' % transfer_back)
+        self.assertOrderedGrep(file='query_l2.log', exprList=expr_list)
 
     def run_client(self, layer, id):
         # run the javascript event log subscriber in the background
@@ -42,6 +53,8 @@ class PySysTest(TenNetworkTest):
         args.extend(['--network_ws', layer.network.connection_url(web_socket=True)])
         args.extend(['--bus_address', '%s' % layer.bus.address])
         args.extend(['--bus_abi', '%s' % layer.bus.abi_path])
+        args.extend(['--sender_address', '%s' % layer.bridge.address])
+        args.extend(['--receiver_address', '%s' % layer.account.address])
         args.extend(['--log_file', '%s' % logout])
         self.run_javascript(script, stdout, stderr, args)
         self.waitForGrep(file=logout, expr='Starting task ...', timeout=10)
