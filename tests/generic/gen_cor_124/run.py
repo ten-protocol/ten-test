1997becafe0359cb980255cf48526821d6287456
Moray Grieve <moray.grieve@me.com>
2024-09-02 11:54:11 +0100

    Interim update

--- tests/generic/gen_cor_124/run.py
+++ tests/generic/gen_cor_124/run.py
@@ -11,12 +11,14 @@ class PySysTest(GenericNetworkTest):
 
     def execute(self):
         project = os.path.join(self.output, 'project')
-        private_key = secrets.token_hex(32)
+        private_key_1 = secrets.token_hex(32)
+        private_key_2 = secrets.token_hex(32)
 
         # connect to the network, allocate the normal ephemeral amount
         network = self.get_network_connection(name='local')
-        self.distribute_native(Web3().eth.account.from_key(private_key), network.ETH_ALLOC)
-        web3, account = network.connect(self, private_key=private_key, check_funds=False)
+        self.distribute_native(Web3().eth.account.from_key(private_key_1), network.ETH_ALLOC)
+        web3_1, account_1 = network.connect(self, private_key=private_key_1, check_funds=False)
+        web3_2, account_2 = network.connect(self, private_key=private_key_2, check_funds=False)
 
         # copy over and initialise the project
         shutil.copytree(self.input, project)
@@ -24,7 +26,7 @@ class PySysTest(GenericNetworkTest):
 
         # deploy and get the address from the hardhat output
         environ = copy.deepcopy(os.environ)
-        environ['PK'] = private_key
+        environ['PK'] = private_key_1
         environ['HOST'] = network.HOST
         environ['PORT'] = str(network.PORT)
         environ['TOKEN'] = network.ID if self.is_ten() else ''
@@ -42,27 +44,32 @@ class PySysTest(GenericNetworkTest):
 
         # construct an instance of the contract from the address and abi
         with open(os.path.join(self.output, 'project', 'artifacts', 'contracts', 'StoreV1.sol', 'StoreV1.json')) as f:
-            contract = web3.eth.contract(address=address, abi=json.load(f)['abi'])
+            contract = web3_1.eth.contract(address=address, abi=json.load(f)['abi'])
 
         # store a value in the contract and retrieve it
-        network.transact(self, web3, contract.functions.store(200), account, 3_000_000, persist_nonce=False)
+        network.transact(self, web3_1, contract.functions.store(200), account_1, 3_000_000, persist_nonce=False)
         ret = int(contract.functions.retrieve().call())
         self.log.info('Returned value is %d', ret)
         self.assertTrue(ret == 200)
 
         # upgrade the contract
+        environ = copy.deepcopy(os.environ)
+        environ['PK'] = private_key_1
+        environ['HOST'] = network.HOST
+        environ['PORT'] = str(network.PORT)
+        environ['TOKEN'] = network.ID if self.is_ten() else ''
         environ['ADDRESS'] = address
         self.run_npx(args=['hardhat', 'run', '--network', self.get_network(), 'scripts/upgrade.js'],
                      working_dir=project, environ=environ, stdout='npx_upgrade.out', stderr='npx_upgrade.err')
 
         # make a call to v2 and assert we get the correct returned result, then repeat
         with open(os.path.join(self.output, 'project', 'artifacts', 'contracts', 'StoreV2.sol', 'StoreV2.json')) as f:
-            contract = web3.eth.contract(address=address, abi=json.load(f)['abi'])
+            contract = web3_1.eth.contract(address=address, abi=json.load(f)['abi'])
         ret = int(contract.functions.retrieve().call())
         self.log.info('Returned value is %d', ret)
         self.assertTrue(ret == 400)
 
-        network.transact(self, web3, contract.functions.store(400), account, 3_000_000, persist_nonce=False)
+        network.transact(self, web3_1, contract.functions.store(400), account_1, 3_000_000, persist_nonce=False)
         ret = int(contract.functions.retrieve().call())
         self.log.info('Returned value is %d', ret)
         self.assertTrue(ret == 800)
