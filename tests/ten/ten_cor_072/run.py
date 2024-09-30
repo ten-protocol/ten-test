a8029b4b2e4181b79d3881b3668e6db429072afb
Moray Grieve <moray.grieve@me.com>
2024-09-30 20:13:52 +0800

    Addition

--- tests/ten/ten_cor_072/run.py
+++ tests/ten/ten_cor_072/run.py
@@ -1,6 +1,6 @@
 import os
 from ten.test.basetest import TenNetworkTest
-from ten.test.contracts.game import FieldEveryoneGuessGame
+from ten.test.contracts.game import FieldTopic1GuessGame
 
 
 class PySysTest(TenNetworkTest):
@@ -10,31 +10,43 @@ class PySysTest(TenNetworkTest):
         network_dev = self.get_network_connection()
         web3_dev, account_dev = network_dev.connect_account2(self)
         block_number = web3_dev.eth.get_block_number()
-        game = FieldEveryoneGuessGame(self, web3_dev)
+        game = FieldTopic1GuessGame(self, web3_dev)
         game.deploy(network_dev, account_dev)
 
         # connect a user to the network to play the game and make some guesses
         network_usr = self.get_network_connection()
         web3_usr, account_usr = network_usr.connect_account1(self)
-        game_usr = FieldEveryoneGuessGame.clone(web3_usr, account_usr, game)
+        game_usr = FieldTopic1GuessGame.clone(web3_usr, account_usr, game)
         target = game_usr.contract.functions.guess
         for i in range(1,5): network_dev.transact(self, web3_usr, target(i), account_usr, game_usr.GAS_LIMIT)
 
-        # run a javascript by the dev to get past events
-        stdout = os.path.join(self.output, 'poller.out')
-        stderr = os.path.join(self.output, 'poller.err')
-        logout = os.path.join(self.output, 'poller.log')
-        script = os.path.join(self.input, 'poller.js')
-        args = []
-        args.extend(['--network_ws', network_dev.connection_url(web_socket=True)])
-        args.extend(['--address', '%s' % game.address])
-        args.extend(['--contract_abi', '%s' % game.abi_path])
-        args.extend(['--contract_abi', '%s' % game.abi_path])
-        args.extend(['--log_file', '%s' % logout])
-        args.extend(['--from_block', '%s' % block_number])
-        self.run_javascript(script, stdout, stderr, args)
-        self.waitForGrep(file=logout, expr='Completed task', timeout=30)
+        # dev account can see the Guessed event but not the Attempts event
+        logout = self.get_logs(network_dev, game, block_number, 'dev')
+        self.assertLineCount(file=logout, expr='Guessed event:', condition='==0')
+        self.assertLineCount(file=logout, expr='Attempts event:', condition='==0')
 
+        # usr account can see the Guessed event and the Attempts event
+        logout = self.get_logs(network_usr, game_usr, block_number, 'usr')
         self.assertLineCount(file=logout, expr='Guessed event:', condition='==4')
         self.assertOrderedGrep(file=logout, exprList=['guessedNumber = %d' % d for d in range(1,5)])
-        self.assertLineCount(file=logout, expr='Attempts event:', condition='==0')
+        self.assertLineCount(file=logout, expr='Attempts event:', condition='==4')
+        self.assertOrderedGrep(file=logout, exprList=['attempts = %d' % d for d in range(1,5)])
+
+
+    def get_logs(self, network, contract, block_numer, name):
+        # run a javascript by the dev to get past events
+        self.log.info('Gett past events for %s'%name)
+        stdout = os.path.join(self.output, 'poller_%s.out'%name)
+        stderr = os.path.join(self.output, 'poller_%s.err'%name)
+        logout = os.path.join(self.output, 'poller_%s.log'%name)
+        script = os.path.join(self.input, 'poller.js')
+        args = []
+        args.extend(['--network_ws', network.connection_url(web_socket=True)])
+        args.extend(['--address', '%s' % contract.address])
+        args.extend(['--contract_abi', '%s' % contract.abi_path])
+        args.extend(['--contract_abi', '%s' % contract.abi_path])
+        args.extend(['--log_file', '%s' % logout])
+        args.extend(['--from_block', '%s' % block_numer])
+        self.run_javascript(script, stdout, stderr, args)
+        self.waitForGrep(file=logout, expr='Completed task', timeout=30)
+        return logout
