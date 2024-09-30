f2ebcde1a553687e479184c96011adb3f60f7c41
Moray Grieve <moray.grieve@me.com>
2024-09-30 12:00:47 +0800

    Use relevancy contract

--- src/solidity/contracts/relevancy/Topic2CanView.sol
+++ src/solidity/contracts/relevancy/Topic2CanView.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./GuessGame.sol";
+import "./Relevancy.sol";
 import "./ContractTransparencyConfig.sol";
 
-contract Topic2CanViewGuessGame is GuessGame, ContractTransparencyConfig  {
+contract Topic2CanView is Relevancy, ContractTransparencyConfig  {
 
-    // set the isPublic property of the Guessed event to true
     function visibilityRules() external pure override returns (VisibilityConfig memory) {
         EventLogConfig[]  memory configs = new EventLogConfig[](1);
         configs[0] = EventLogConfig(hex"bc11465ed8ccb85c11046e78bf3d70287e3c77bf1c9542a3e250511bfb3c1b04", false, false, true, false, false);
