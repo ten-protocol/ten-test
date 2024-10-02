2cf52a646f1a571b144237abc8e77d954efeac15
Moray Grieve <moray.grieve@me.com>
2024-10-02 19:07:11 +0800

    Another tx receipt test

--- tests/ten/ten_cor_080/run.py
+++ tests/ten/ten_cor_080/run.py
@@ -16,11 +16,19 @@ class PySysTest(TenNetworkTest):
         game_1 = TransparentGuessGame(self, web3_1)
         game_1.deploy(network_1, account_1)
 
-        # player 2 transacts with the contract
+        # player 2 transacts with the contract and parses the logs from the tx receipt
         game_2 = TransparentGuessGame.clone(web3_2, account_2, game_1)
         tx_rcpt = network_2.transact(self, web3_2, game_2.contract.functions.guess(2), account_2, game_2.GAS_LIMIT)
+
+        receipt = web3_2.eth.get_transaction_receipt(tx_rcpt.transactionHash)
+        logs = game_2.contract.events.Guessed().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(logs[0]['args']['guessedNumber'] == 2, assertMessage='Logs should show the guessed number as 2')
+        logs = game_2.contract.events.Attempts().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(logs[0]['args']['attempts'] == 1, assertMessage='Logs should show the number attempts as 1')
 
         # player 1 tries to get the tx receipt and see the logs
         receipt = web3_1.eth.get_transaction_receipt(tx_rcpt.transactionHash)
         logs = game_1.contract.events.Guessed().process_receipt(receipt, EventLogErrorFlags.Discard)
         self.assertTrue(logs[0]['args']['guessedNumber'] == 2, assertMessage='Logs should show the guessed number as 2')
+        logs = game_1.contract.events.Attempts().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(logs[0]['args']['attempts'] == 1, assertMessage='Logs should show the number attempts as 1')
--- tests/ten/ten_cor_081/run.py
+++ tests/ten/ten_cor_081/run.py
@@ -1,24 +1,34 @@
 from ten.test.basetest import TenNetworkTest
-from ten.test.contracts.game import PublicEventGuessGame
+from ten.test.contracts.game import FieldEveryoneGuessGame
 from web3._utils.events import EventLogErrorFlags
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        # connect the dev to the network to deploy the game
-        network_dev = self.get_network_connection()
-        web3_dev, account_dev = network_dev.connect_account2(self)
-        game = PublicEventGuessGame(self, web3_dev)
-        game.deploy(network_dev, account_dev)
+        # connect the players to the network
+        network_1 = self.get_network_connection()
+        network_2 = self.get_network_connection()
+        web3_1, account_1 = network_1.connect_account1(self)
+        web3_2, account_2 = network_2.connect_account2(self)
 
-        # connect a user to the network to play the game and make some guesses
-        network_usr = self.get_network_connection()
-        web3_usr, account_usr = network_usr.connect_account1(self)
-        game_usr = PublicEventGuessGame.clone(web3_usr, account_usr, game)
-        tx_rcpt = network_dev.transact(self, web3_usr, game_usr.contract.functions.guess(2), account_usr,
-                                       game_usr.GAS_LIMIT)
+        # player 1 deploys the contract
+        game_1 = FieldEveryoneGuessGame(self, web3_1)
+        game_1.deploy(network_1, account_1)
 
-        receipt = web3_dev.eth.get_transaction_receipt(tx_rcpt.transactionHash)
-        logs = game.contract.events.Guessed().process_receipt(receipt, EventLogErrorFlags.Discard)
+        # player 2 transacts with the contract and parses the logs from the tx receipt
+        game_2 = FieldEveryoneGuessGame.clone(web3_2, account_2, game_1)
+        tx_rcpt = network_2.transact(self, web3_2, game_2.contract.functions.guess(2), account_2, game_2.GAS_LIMIT)
+
+        receipt = web3_2.eth.get_transaction_receipt(tx_rcpt.transactionHash)
+        logs = game_2.contract.events.Guessed().process_receipt(receipt, EventLogErrorFlags.Discard)
         self.assertTrue(logs[0]['args']['guessedNumber'] == 2, assertMessage='Logs should show the guessed number as 2')
+        logs = game_2.contract.events.Attempts().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(logs[0]['args']['attempts'] == 1, assertMessage='Logs should show the number attempts as 1')
+
+        # player 1 tries to get the tx receipt and see the logs
+        receipt = web3_1.eth.get_transaction_receipt(tx_rcpt.transactionHash)
+        logs = game_1.contract.events.Guessed().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(logs[0]['args']['guessedNumber'] == 2, assertMessage='Logs should show the guessed number as 2')
+        logs = game_1.contract.events.Attempts().process_receipt(receipt, EventLogErrorFlags.Discard)
+        self.assertTrue(len(logs) == 0, assertMessage='Should not be able to retrieve attempts')
