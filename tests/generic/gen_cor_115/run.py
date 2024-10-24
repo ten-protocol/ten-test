8054229cd95fb9a392d0cf2539d89f830bcb0bed
Moray Grieve <moray.grieve@me.com>
2024-10-24 11:08:56 +0100

    Interim updates

--- tests/generic/gen_cor_115/run.py
+++ tests/generic/gen_cor_115/run.py
@@ -7,37 +7,40 @@ from ten.test.contracts.emitter import EventEmitter, EventEmitterCaller
 class PySysTest(GenericNetworkTest):
 
     def execute(self):
-        # connect to network
+        # connect two accounts to the network
         network_1 = self.get_network_connection(name='user1')
         network_2 = self.get_network_connection(name='user2')
         web3_1, account_1 = network_1.connect_account1(self)
         web3_2, account_2 = network_2.connect_account2(self)
 
-        # deploy the contracts
-        emitter = EventEmitter(self, web3_1)
-        emitter.deploy(network_1, account_1)
-        emitter_caller = EventEmitterCaller(self, web3_1, emitter.address)
-        emitter_caller.deploy(network_1, account_1)
+        # deploy the contracts as account 1, get a reference to the emitter caller for both accounts
+        emitter_1 = EventEmitter(self, web3_1)
+        emitter_1.deploy(network_1, account_1)
+        emitter_1_caller = EventEmitterCaller(self, web3_1, emitter_1.address)
+        emitter_1_caller.deploy(network_1, account_1)
+        emitter_2_caller = EventEmitterCaller.clone(web3_2, account_2, emitter_1_caller)
 
-        # transact in a loop
-        for id in range(0, 5):
-            # callEmitAddressEvent will emit the CallerAddressEvent with the msg.sender address in it, and through the
-            # contract call will also emit AddressEvent with the address field supplied
-            tx_receipt_1 = network_1.transact(self, web3_1,
-                                          emitter_caller.contract.functions.callEmitAddressEvent(1, account_2.address),
-                                          account_1, emitter_caller.GAS_LIMIT)
+        # make a transaction against the caller contract to emit two events as below;
+        #   emit CallerAddressEvent with id, msg.sender (account 1 address) - relevant only to account 1
+        #   emit AddressEvent       with id, address    (account 2 address) - relevant only to account 2
+        tx_receipt_1 = network_1.transact(self, web3_1,
+                                          emitter_1_caller.contract.functions.callEmitAddressEvent(1, account_2.address),
+                                          account_1, emitter_1_caller.GAS_LIMIT)
+        
+        event_1 = emitter_1_caller.contract.events.CallerAddressEvent().process_receipt(tx_receipt_1, errors=DISCARD)[0]
+        self.log.info('  tx_receipt_1.logs:                   %s' % len(tx_receipt_1['logs']))
+        self.log.info('  callerAddressEvent.id:               %s' % event_1['args']['id'])
+        self.log.info('  callerAddressEvent.address:          %s' % event_1['args']['_address'])
 
-            try:
+        try:
+            if not self.is_ten():
                 tx_receipt_2 = web3_2.eth.get_transaction_receipt(tx_receipt_1.transactionHash)
+                event_2 = emitter_2_caller.contract.events.CallerAddressEvent().process_receipt(tx_receipt_2, errors=DISCARD)[0]
+                self.log.info('  callerAddressEvent.address:  %s' % event_2['args']['_address'])
 
-                event_1 = emitter_caller.contract.events.CallerAddressEvent().process_receipt(tx_receipt_1, errors=DISCARD)[0]
-                event_2 = emitter_caller.contract.events.CallerAddressEvent().process_receipt(tx_receipt_2, errors=DISCARD)[0]
-
-                self.log.info('  simpleEvent.address:        %s' % event_1['args']['_address'])
-                self.log.info('  callerSimpleEvent.address:  %s' % event_2['args']['_address'])
-            except Exception as e:
-                self.log.info('Exception: %s' % e['message'])
-                self.addOutcome(FAILED, outcomeReason='We should be able to get the two events from the tx receipt', abortOnError=True)
+        except Exception as e:
+            self.log.info('Exception: %s' % e['message'])
+            self.addOutcome(FAILED, outcomeReason='We should be able to get the two events from the tx receipt', abortOnError=True)
 
 
 
