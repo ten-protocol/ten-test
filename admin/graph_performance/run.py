87db29a5b6b53461b7848e7bd73465bac5d0bc49
Moray Grieve <moray.grieve@me.com>
2024-10-29 15:43:02 +0000

    Update performance plotting

--- admin/graph_performance/run.py
+++ admin/graph_performance/run.py
@@ -7,14 +7,18 @@ from ten.test.utils.gnuplot import GnuplotHelper
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_004','ten_per_005',
-                 'ten_per_006','ten_per_007','ten_per_008','ten_per_009']
+        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_004','ten_per_005','ten_per_006',
+                 'ten_per_010','ten_per_011','ten_per_012','ten_per_020','ten_per_021']
 
         for test in tests:
             with open(os.path.join(self.output, '%s.log' % test), 'w') as fp:
                 for entry in reversed(self.results_db.get_results(test=test, environment=self.env)):
                     fp.write('%s %s\n' % (self.epoch_to_fractional_doy(entry[0]), entry[1]))
-        self.graph()
+        try:
+            self.graph()
+        except Exception as e:
+            self.log.warn('Exception raised when trying to graph results')
+            self.log.warn(e)
 
     def graph(self):
         branch = GnuplotHelper.buildInfo().branch
