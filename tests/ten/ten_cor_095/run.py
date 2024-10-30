4dc3ae70c981f515d1cc2ec5671d08aebbc347de
Moray Grieve <moray.grieve@me.com>
2024-10-30 10:36:44 +0000

    Updated

--- tests/ten/ten_cor_095/run.py
+++ tests/ten/ten_cor_095/run.py
@@ -26,20 +26,20 @@ class PySysTest(TenNetworkTest):
         self.log.info('  tx_receipt_2.blockNumber:         %s' % tx_receipt_2.blockNumber)
 
         # call from first block to latest ... should have both events
-        response_1 = self.get_debug_event_log_relevancy( url=network.connection_url(),
+        response = self.get_debug_event_log_relevancy( url=network.connection_url(),
             address=storage.address, signature=web3.keccak(text='Stored(uint256)').hex(),
             fromBlock=hex(tx_receipt_1.blockNumber), toBlock='latest')
 
         # dump for reference
-        self.dump(response_1[0], 'response_1_event_1.log')
-        self.dump(response_1[1], 'response_1_event_2.log')
+        self.dump(response[0], 'response_1_event_1.log')
+        self.dump(response[1], 'response_1_event_2.log')
 
         # contract has no explicit configuration so should be default
-        self.assertTrue(len(response_1) == 2)
-        self.assertTrue(response_1[0]['transactionHash'] == tx_receipt_1.transactionHash.hex())
-        self.assertTrue(response_1[1]['transactionHash'] == tx_receipt_2.transactionHash.hex())
-        self.assertTrue(response_1[0]['defaultContract'] == True)
-        self.assertTrue(response_1[1]['defaultContract'] == True)
+        self.assertTrue(len(response) == 2)
+        self.assertTrue(response[0]['transactionHash'] == tx_receipt_1.transactionHash.hex())
+        self.assertTrue(response[1]['transactionHash'] == tx_receipt_2.transactionHash.hex())
+        self.assertTrue(response[0]['defaultContract'] == True)
+        self.assertTrue(response[1]['defaultContract'] == True)
 
     def dump(self, obj, filename):
         with open(os.path.join(self.output, filename), 'w') as file:
