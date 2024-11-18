6da0c932a5e55735bb1e2d5413e27ce27cdd7968
Moray Grieve <moray.grieve@me.com>
2024-11-18 20:51:38 +0000

    New endpoint (#379)
    * New endpoint
    
    * Updated
    
    * Remove client doing the checksum conversion
    
    * Add to the robustness test to use session keys

--- tests/ten/ten_cor_105/run.py
+++ tests/ten/ten_cor_105/run.py
@@ -1,11 +1,38 @@
-import pysys
-from pysys.constants import *
-from pysys.basetest import BaseTest
+import re
+from pysys.constants import PASSED, FAILED
+from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.storage import Storage
 
-class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
-	def validate(self):
-		pass
-	
+class PySysTest(TenNetworkTest):
+
+    def execute(self):
+        # connect to the network via the primary gateway and deploy the storage contract
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+        storage = Storage(self, web3, 100)
+        storage.deploy(network, account)
+
+        # get a session key for this connection and fund it
+        sk = self.get_session_key(network.connection_url())
+        address = web3.to_checksum_address(sk)
+        tx = {'to': address, 'value': web3.to_wei(0.01, 'ether'), 'gasPrice': web3.eth.gas_price}
+        tx['gas'] = web3.eth.estimate_gas(tx)
+        network.tx(self, web3, tx, account)
+        self.activate_session_key(network.connection_url())
+
+        # transact using the session key
+        network.transact_unsigned(self, web3, storage.contract.functions.store(2), address, storage.GAS_LIMIT)
+        self.assertTrue(storage.contract.functions.retrieve().call() ==2)
+
+        # connect against another user id
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+        try:
+            network.transact_unsigned(self, web3, storage.contract.functions.store(2), address, storage.GAS_LIMIT)
+            self.addOutcome(FAILED, outcomeReason='Exception should be raised')
+        except Exception as e:
+            self.log.info('Exception type: %s', type(e).__name__)
+            self.log.info('Exception args: %s', e.args)
+            regex = re.compile('illegal access', re.M)
+            self.assertTrue(regex.search(e.args[0]['message']) is not None)
--- tests/ten/ten_rob_002/run.py
+++ tests/ten/ten_rob_002/run.py
@@ -9,8 +9,9 @@ from ten.test.contracts.error import Error
 
 class PySysTest(TenNetworkTest):
     NUM_FUNDS = 5
+    NUM_STORAGE = 2
+    NUM_STORAGE_SK = 2
     NUM_GUESSERS = 4
-    NUM_STORAGE = 4
     NUM_ERROR = 2
     DURATION = 60
 
@@ -28,13 +29,13 @@ class PySysTest(TenNetworkTest):
         funders = [secrets.token_hex() for _ in range(0, self.NUM_FUNDS)]
         funders_connection = self.get_network_connection()
 
-        guesser = Guesser(self, web3, 0, 100)
-        guesser.deploy(network, account)
-        guesser_connection = self.get_network_connection()
-
         storage = Storage(self, web3, 100)
         storage.deploy(network, account)
         storage_connection = self.get_network_connection()
+
+        guesser = Guesser(self, web3, 0, 100)
+        guesser.deploy(network, account)
+        guesser_connection = self.get_network_connection()
 
         error = Error(self, web3)
         error.deploy(network, account)
@@ -45,11 +46,14 @@ class PySysTest(TenNetworkTest):
             recipients = [Web3().eth.account.from_key(x).address for x in funders if x != funders[i]]
             self.funds_client(network, funders[i], recipients, i, funders_connection)
 
-        for i in range(0, self.NUM_GUESSERS):
-            self.guesser_client(guesser.address, guesser.abi_path, i, guesser_connection)
-
         for i in range(0, self.NUM_STORAGE):
             self.storage_client(storage.address, storage.abi_path, i, storage_connection)
+
+        for i in range(0, self.NUM_STORAGE_SK):
+            self.storage_client_sk(storage.address, storage.abi_path, i)
+
+        for i in range(0, self.NUM_GUESSERS):
+            self.guesser_client(guesser.address, guesser.abi_path, i, guesser_connection)
 
         for i in range(0, self.NUM_ERROR):
             self.error_client(error.address, error.abi_path, i, error_connection)
@@ -68,7 +72,7 @@ class PySysTest(TenNetworkTest):
 
     def funds_client(self, network, pk, recipients, num, funders_connection):
         web3, account = funders_connection.connect(self, private_key=pk, check_funds=False)
-        self.distribute_native(account, network.ETH_ALLOC_EPHEMERAL)
+        self.distribute_native(account, 5*network.ETH_ALLOC_EPHEMERAL)
 
         stdout = os.path.join(self.output, 'funds_%d.out' % num)
         stderr = os.path.join(self.output, 'funds_%d.err' % num)
@@ -77,14 +81,35 @@ class PySysTest(TenNetworkTest):
         args.extend(['--network_http', '%s' % funders_connection.connection_url()])
         args.extend(['--pk_to_register', '%s' % pk])
         args.extend(['--recipients', ','.join([str(i) for i in recipients])])
-        self.run_python(script, stdout, stderr, args)
+        self.clients.append(self.run_python(script, stdout, stderr, args))
+        self.waitForGrep(file=stdout, expr='Client running', timeout=10)
+
+    def storage_client(self, address, abi_path, num, network):
+        self._client(address, abi_path, 'storage_client', num, network)
+
+    def storage_client_sk(self, address, abi_path, num):
+        network = self.get_network_connection()
+        web3, account = network.connect(self, private_key=secrets.token_hex(32), check_funds=False)
+        self.distribute_native(account, 5*network.ETH_ALLOC_EPHEMERAL)
+        sk = self.get_session_key(network.connection_url())
+        tx = {'to': sk, 'value': web3.to_wei(0.9*5*network.ETH_ALLOC_EPHEMERAL, 'ether'), 'gasPrice': web3.eth.gas_price}
+        tx['gas'] = web3.eth.estimate_gas(tx)
+        network.tx(self, web3, tx, account)
+        self.activate_session_key(network.connection_url())
+
+        stdout = os.path.join(self.output, 'storage_client_sk_%d.out' % num)
+        stderr = os.path.join(self.output, 'storage_client_sk_%d.err' % num)
+        script = os.path.join(self.input, 'storage_client_sk.py')
+        args = []
+        args.extend(['--network_http', '%s' % network.connection_url()])
+        args.extend(['--address', '%s' % address])
+        args.extend(['--contract_abi', '%s' % abi_path])
+        args.extend(['--session_key', '%s' % sk])
+        self.clients.append(self.run_python(script, stdout, stderr, args))
         self.waitForGrep(file=stdout, expr='Client running', timeout=10)
 
     def guesser_client(self, address, abi_path, num, network):
         self._client(address, abi_path, 'guesser_client', num, network, False)
-
-    def storage_client(self, address, abi_path, num, network):
-        self._client(address, abi_path, 'storage_client', num, network)
 
     def error_client(self, address, abi_path, num, network):
         self._client(address, abi_path, 'error_client', num, network, False)
@@ -93,7 +118,7 @@ class PySysTest(TenNetworkTest):
         pk = secrets.token_hex(32)
         web3, account = network.connect(self, private_key=pk, check_funds=False)
         if fund:
-            self.distribute_native(account, network.ETH_ALLOC_EPHEMERAL)
+            self.distribute_native(account, 5*network.ETH_ALLOC_EPHEMERAL)
             self.client_connections.append((web3, account, network))
 
         stdout = os.path.join(self.output, '%s_%d.out' % (name, num))
