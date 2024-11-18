8430c69cc40651eae9e13a350d193e1317be4520
Moray Grieve <moray.grieve@me.com>
2024-11-18 11:43:32 +0000

    Remove client doing the checksum conversion

--- tests/ten/ten_rob_002/Input/storage_client_sk.py
+++ tests/ten/ten_rob_002/Input/storage_client_sk.py
@@ -5,18 +5,18 @@ import argparse, json, sys, time
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', stream=sys.stdout, level=logging.INFO)
 
 
-def store_value(value, web3, account, contract):
+def store_value(value, web3, address, contract):
     params = {
-        'nonce': web3.eth.get_transaction_count(account.address),
-        'gasPrice': web3.eth.gas_price,
-        'chainId': web3.eth.chain_id
+        'from': address,
+        'nonce': web3.eth.get_transaction_count(address),
+        'chainId': web3.eth.chain_id,
+        'gasPrice': web3.eth.gas_price
     }
     gas_estimate = contract.functions.store(value).estimate_gas(params)
     params['gas'] = int(1.1*gas_estimate)
 
     build_tx = contract.functions.store(value).buildTransaction(params)
-    signed_tx = account.sign_transaction(build_tx)
-    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_hash = web3.eth.send_transaction(build_tx)
     tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
     if tx_receipt.status != 1:
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     parser.add_argument('-u', '--network_http', help='Connection URL')
     parser.add_argument('-a', '--address', help='Address of the contract')
     parser.add_argument('-b', '--contract_abi', help='Abi of the contract')
-    parser.add_argument('-p', '--pk_to_register', help='Private key of account')
+    parser.add_argument('-p', '--session_key', help='Session key to use')
     args = parser.parse_args()
 
     web3 = Web3(Web3.HTTPProvider(args.network_http))
@@ -38,10 +38,8 @@ if __name__ == "__main__":
         contract = web3.eth.contract(address=args.address, abi=json.load(f))
 
     logging.info('Client running')
-    account = web3.eth.account.from_key(args.pk_to_register)
-
     while True:
-        store_value(random.randint(0,100), web3, account, contract)
+        store_value(random.randint(0,100), web3, args.session_key, contract)
         time.sleep(0.1)
 
 
