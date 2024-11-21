8cbb335bf8f1dd540c674f4c86ebfdfa6cc7134c
Moray Grieve <moray.grieve@me.com>
2024-11-21 11:34:16 +0000

    New test on contract.events.Event

---
+++ tests/ten/ten_cor_112/Input/subscriber.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const Web3 = require('web3')
+const commander = require('commander')
+
+function log(data) {
+    let timestamp = new Date().toISOString();
+    const entry = `${timestamp} ${data}\n`;
+    fs.appendFileSync(options.log_file, entry, { flag: 'a' });
+}
+
+function task() {
+  log('Starting task ...')
+  contract.events.Stored({fromBlock:'latest'},
+    function(error, result) {
+      if (error) {
+        log(`Error returned is ${error}`)
+      } else {
+        log(result);
+        log(`Stored value = ${result.returnValues['value']}`);
+      }
+    }
+  )
+}
+
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('--network_ws <value>', 'Web socket connection URL to the network')
+  .option('--address <value>', 'Web socket connection URL to the network')
+  .option('--contract_abi <value>', 'Web socket connection URL to the network')
+  .option('--log_file <value>', 'The output file to write to')
+  .parse(process.argv)
+
+const options = commander.opts()
+const web3 = new Web3(`${options.network_ws}`)
+
+var json = fs.readFileSync(`${options.contract_abi}`)
+var abi = JSON.parse(json)
+const contract = new web3.eth.Contract(abi, `${options.address}`)
+task()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
