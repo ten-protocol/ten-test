ec45fd74f5d9fa70256159b998b33d39df1bb4c3
Moray Grieve <moray.grieve@me.com>
2024-11-22 13:29:04 +0000

    Add the require test

--- tests/generic/gen_cor_051/run.py
+++ tests/generic/gen_cor_051/run.py
@@ -1,4 +1,5 @@
 import re
+from pysys.constants import PASSED, FAILED
 from ten.test.basetest import GenericNetworkTest
 from ten.test.contracts.error import Error
 
@@ -13,18 +14,21 @@ class PySysTest(GenericNetworkTest):
         error = Error(self, web3)
         error.deploy(network, account)
 
-        # force a require (note we manage the transaction here ourselves rather than using
-        # network.transact, as we want to get the failure reason and validate)
+        # transact successfully
+        nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
+        self.submit(account, error.contract.functions.set_key("new"), web3, nonce)
+
+        # force a require
         nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
         self.submit(account, error.contract.functions.set_key(""), web3, nonce)
 
-    def submit(self, account, target, web3, nonce):
+    def submit(self, account, target, web3, nonce, expect_success=True):
         build_tx = target.build_transaction({
-                'nonce': nonce,
-                'gasPrice': web3.eth.gas_price,
-                'gas': 10*21000,                    # hard code the gas as an estimate will fail on the revert
-                'chainId': web3.eth.chain_id
-            })
+            'nonce': nonce,
+            'gasPrice': web3.eth.gas_price,
+            'gas': 10*21000,                    # hard code the gas as an estimate will fail
+            'chainId': web3.eth.chain_id
+        })
         signed_tx = account.sign_transaction(build_tx)
         self.nonce_db.update(account.address, self.env, nonce, 'SIGNED')
 
@@ -35,14 +39,15 @@ class PySysTest(GenericNetworkTest):
         if tx_receipt.status == 1:
             self.log.info('Transaction successful')
             self.nonce_db.update(account.address, self.env, nonce, 'CONFIRMED')
+            self.addOutcome(PASSED if expect_success else FAILED, 'Transaction should pass')
         else:
             self.log.info('Transaction failed')
             self.nonce_db.update(account.address, self.env, nonce, 'FAILED')
             try:
                 web3.eth.call(build_tx, block_identifier=tx_receipt.blockNumber)
             except Exception as e:
+                self.addOutcome(FAILED if expect_success else PASSED, 'Transaction should fail')
                 self.log.error('Replay call: %s', e)
                 regex = re.compile('.*New key cannot be empty', re.M)
                 self.assertTrue(regex.search(e.args[0]) is not None)
         return tx_receipt
-
--- tests/ten/ten_cor_114/run.py
+++ tests/ten/ten_cor_114/run.py
@@ -1,4 +1,5 @@
 import re
+from pysys.constants import PASSED, FAILED
 from ten.test.basetest import GenericNetworkTest
 from ten.test.utils.properties import Properties
 from ten.test.contracts.error import ErrorTwoPhase
@@ -14,18 +15,21 @@ class PySysTest(GenericNetworkTest):
         error = ErrorTwoPhase(self, web3, Properties().L2PublicCallbacks)
         error.deploy(network, account)
 
-        # force a require (note we manage the transaction here ourselves rather than using
-        # network.transact, as we want to get the failure reason and validate)
+        # transact successfully
+        nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
+        self.submit(account, error.contract.functions.set_key_with_require("new"), web3, nonce)
+
+        # force a require
         nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
         self.submit(account, error.contract.functions.set_key_with_require(""), web3, nonce)
 
-    def submit(self, account, target, web3, nonce):
+    def submit(self, account, target, web3, nonce, expect_success=True):
         build_tx = target.build_transaction({
             'nonce': nonce,
             'gasPrice': web3.eth.gas_price,
-            'gas': 10*21000,                    # hard code the gas as an estimate will fail on the revert
-            'value': web3.to_wei(0.01, 'ether'),
-            'chainId': web3.eth.chain_id
+            'gas': 10*21000,                    # hard code the gas as an estimate will fail
+            'chainId': web3.eth.chain_id,
+            'value': web3.to_wei(0.01, 'ether') # we need to provide funds for the tx to go through
         })
         signed_tx = account.sign_transaction(build_tx)
         self.nonce_db.update(account.address, self.env, nonce, 'SIGNED')
@@ -37,20 +41,15 @@ class PySysTest(GenericNetworkTest):
         if tx_receipt.status == 1:
             self.log.info('Transaction successful')
             self.nonce_db.update(account.address, self.env, nonce, 'CONFIRMED')
+            self.addOutcome(PASSED if expect_success else FAILED, 'Transaction should pass')
         else:
             self.log.info('Transaction failed')
             self.nonce_db.update(account.address, self.env, nonce, 'FAILED')
             try:
                 web3.eth.call(build_tx, block_identifier=tx_receipt.blockNumber)
             except Exception as e:
+                self.addOutcome(FAILED if expect_success else PASSED, 'Transaction should fail')
                 self.log.error('Replay call: %s', e)
                 regex = re.compile('.*New key cannot be empty', re.M)
                 self.assertTrue(regex.search(e.args[0]) is not None)
         return tx_receipt
-
-    def transact(self, target, web3, network, account):
-        params = {'gasPrice': web3.eth.gas_price, 'value': web3.to_wei(0.01, 'ether')}
-        gas_estimate = target.estimate_gas(params)
-        params['gas'] = int(1.1 * gas_estimate)
-        build_tx = target.build_transaction(params)
-        network.tx(self, web3, build_tx, account)
