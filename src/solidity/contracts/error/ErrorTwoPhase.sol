c5dd8354915637a040fc5c73da6ab02ac9e7bb06
Moray Grieve <moray.grieve@me.com>
2024-11-22 12:18:01 +0000

    Require in callback

--- src/solidity/contracts/error/ErrorTwoPhase.sol
+++ src/solidity/contracts/error/ErrorTwoPhase.sol
@@ -6,14 +6,45 @@ interface IPublicCallbacks {
     function reattemptCallback(uint256 callbackId) external;
 }
 
-contract Error {
+contract ErrorTwoPhase {
     IPublicCallbacks public callbacks;
+
+    string public key = "foo";
 
     constructor(address _callbacks) {
         callbacks = IPublicCallbacks(_callbacks);
     }
 
-    function force_require() public view {
+    function set(string memory _key) public {
+        key = _key;
+    }
+
+    // set_key_with_require
+    function set_key_with_require(string memory newKey) public payable {
+        bytes memory callbackData = abi.encodeWithSelector(this.handle_set_key_with_require.selector, newKey);
+        callbacks.register{value: msg.value}(callbackData);
+    }
+
+    function handle_set_key_with_require(string memory newKey) external {
+        require(bytes(newKey).length != 0, "New key cannot be empty");
+        key = newKey;
+    }
+
+    // set_key_with_revert
+    function set_key_with_revert(string memory newKey) public payable {
+        bytes memory callbackData = abi.encodeWithSelector(this.handle_set_key_with_revert.selector, newKey);
+        callbacks.register{value: msg.value}(callbackData);
+    }
+
+    function handle_set_key_with_revert(string memory newKey) external {
+        if (bytes(newKey).length == 0){
+            revert("New key cannot be empty");
+        }
+        key = newKey;
+    }
+
+    // force_require
+    function force_require() public payable {
         bytes memory callbackData = abi.encodeWithSelector(this.handle_force_require.selector);
         callbacks.register{value: msg.value}(callbackData);
     }
@@ -22,12 +53,23 @@ contract Error {
         require(false, "Forced require");
     }
 
-    function force_revert() public view {
+    // force_revert
+    function force_revert() public payable {
         bytes memory callbackData = abi.encodeWithSelector(this.handle_force_revert.selector);
         callbacks.register{value: msg.value}(callbackData);
     }
 
-    function handle_force_revert() public view {
+    function handle_force_revert() external {
         revert("Forced revert");
+    }
+
+    // force_assert
+    function force_assert() public payable {
+        bytes memory callbackData = abi.encodeWithSelector(this.handle_force_assert.selector);
+        callbacks.register{value: msg.value}(callbackData);
+    }
+
+    function handle_force_assert() external {
+        assert(false);
     }
 }
--- tests/ten/ten_cor_114/run.py
+++ tests/ten/ten_cor_114/run.py
@@ -1,46 +1,56 @@
 import re
 from ten.test.basetest import GenericNetworkTest
-from ten.test.contracts.error import Error
+from ten.test.utils.properties import Properties
+from ten.test.contracts.error import ErrorTwoPhase
 
 
 class PySysTest(GenericNetworkTest):
-    PROXY = False
 
     def execute(self):
-        # connect to the network
+        # connect to the network and deploy the contract
         network = self.get_network_connection()
         web3, account = network.connect_account1(self, web_socket=True)
 
-        # deploy the contract
-        error = Error(self, web3)
+        error = ErrorTwoPhase(self, web3, Properties().L2PublicCallbacks)
         error.deploy(network, account)
 
-        # force a require
-        try:
-            self.log.info('Forcing a require on contract function')
-            error.contract.functions.force_require().call()
-        except Exception as e:
-            self.log.info('Exception type: %s', type(e).__name__)
-            self.log.info('Exception args: %s', e.args)
-            regex = re.compile('execution reverted:.*Forced require', re.M)
-            self.assertTrue(regex.search(e.args[0]) is not None)
+        # force a require (note we manage the transaction here ourselves rather than using
+        # network.transact, as we want to get the failure reason and validate)
+        nonce = self.nonce_db.get_next_nonce(self, web3, account.address, self.env)
+        self.submit(account, error.contract.functions.set_key_with_require(""), web3, nonce)
 
-        # force a revert
-        try:
-            self.log.info('Forcing a revert on contract function')
-            error.contract.functions.force_revert().call()
-        except Exception as e:
-            self.log.info('Exception type: %s', type(e).__name__)
-            self.log.info('Exception args: %s', e.args)
-            regex = re.compile('execution reverted:.*Forced revert', re.M)
-            self.assertTrue(regex.search(e.args[0]) is not None)
+    def submit(self, account, target, web3, nonce):
+        build_tx = target.build_transaction({
+            'nonce': nonce,
+            'gasPrice': web3.eth.gas_price,
+            'gas': 10*21000,                    # hard code the gas as an estimate will fail on the revert
+            'value': web3.to_wei(0.01, 'ether'),
+            'chainId': web3.eth.chain_id
+        })
+        signed_tx = account.sign_transaction(build_tx)
+        self.nonce_db.update(account.address, self.env, nonce, 'SIGNED')
 
-        # force assert (note this fails on ganache)
-        try:
-            self.log.info('Forcing an assert on contract function')
-            error.contract.functions.force_assert().call()
-        except Exception as e:
-            self.log.info('Exception type: %s', type(e).__name__)
-            self.log.info('Exception args: %s', e.args)
-            regex = re.compile('Assert evaluates to false', re.M)
-            self.assertTrue(regex.search(e.args[0]) is not None)
+        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        self.nonce_db.update(account.address, self.env, nonce, 'SENT')
+
+        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash.hex(), timeout=30)
+        if tx_receipt.status == 1:
+            self.log.info('Transaction successful')
+            self.nonce_db.update(account.address, self.env, nonce, 'CONFIRMED')
+        else:
+            self.log.info('Transaction failed')
+            self.nonce_db.update(account.address, self.env, nonce, 'FAILED')
+            try:
+                web3.eth.call(build_tx, block_identifier=tx_receipt.blockNumber)
+            except Exception as e:
+                self.log.error('Replay call: %s', e)
+                regex = re.compile('.*New key cannot be empty', re.M)
+                self.assertTrue(regex.search(e.args[0]) is not None)
+        return tx_receipt
+
+    def transact(self, target, web3, network, account):
+        params = {'gasPrice': web3.eth.gas_price, 'value': web3.to_wei(0.01, 'ether')}
+        gas_estimate = target.estimate_gas(params)
+        params['gas'] = int(1.1 * gas_estimate)
+        build_tx = target.build_transaction(params)
+        network.tx(self, web3, build_tx, account)
