db071afc8987ec535292140412de6778b2ff1cf5
Moray Grieve <moray.grieve@me.com>
2024-12-12 11:45:03 +0000

    Validate refund balance

--- tests/ten/ten_cor_119/run.py
+++ tests/ten/ten_cor_119/run.py
@@ -20,20 +20,23 @@ class PySysTest(TenNetworkTest):
         self.wait(float(self.block_time) * 1.1)
         value = storage.contract.functions.retrieve().call()
         self.log.info('Retrieved value: %d' % value)
+        self.assertTrue(storage.contract.functions.retrieve().call() == 200, assertMessage='Value should be 200')
 
         balance_after = web3.eth.get_balance(account.address)
         self.log.info('Balance before:  %d' % balance_before)
         self.log.info('Balance after:   %d' % balance_before)
         self.log.info('Balance diff:    %d' % (balance_before-balance_after))
+        self.log.info('Expected diff:   %d' % ((used*price) + web3.to_wei(0.001, 'ether')))
         self.log.info('Cost estimate:   %d' % (estimate*price))
         self.log.info('Cost used:       %d' % (used*price))
 
         refund_balance = storage.contract.functions.refundBalance().call()
         self.log.info('Refund balance:  %d' % refund_balance)
+        self.assertTrue(refund_balance != 0, assertMessage='Refund should not be zero')
 
     def transact(self, storage, web3, network, account, num):
         target = storage.contract.functions.store(num)
-        params = {'gasPrice': web3.eth.gas_price, 'value': web3.to_wei(0.01, 'ether')}
+        params = {'gasPrice': web3.eth.gas_price, 'value': web3.to_wei(0.001, 'ether')}
         gas_estimate = target.estimate_gas(params)
         params['gas'] = int(1.1 * gas_estimate)
         build_tx = target.build_transaction(params)
