fa40674b1bb777a65d9595afeb79e1946bae88c5
Moray Grieve <moray.grieve@me.com>
2024-12-17 12:27:11 +0000

    Add large calldata test (#391)
    * Call data size test
    
    * Addition of large call data test

--- src/python/ten/test/contracts/calldata.py
+++ src/python/ten/test/contracts/calldata.py
@@ -2,6 +2,11 @@ from pysys.constants import *
 from ten.test.contracts.default import DefaultContract
 
 
+class CallData(DefaultContract):
+    SOURCE = os.path.join(PROJECT.root, 'src', 'solidity', 'contracts', 'calldata', 'CallData.sol')
+    CONTRACT = 'CallData'
+
+
 class CallDataTwoPhase(DefaultContract):
     SOURCE = os.path.join(PROJECT.root, 'src', 'solidity', 'contracts', 'calldata', 'CallDataTwoPhase.sol')
     CONTRACT = 'CallDataTwoPhase'
