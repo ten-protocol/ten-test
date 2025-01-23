600b0010c4f80f14534836f30d532e6c7eb6fae0
Moray Grieve <moray.grieve@me.com>
2025-01-23 15:09:28 +0000

    Update for 007 (#408)

--- admin/graph_performance/run.py
+++ admin/graph_performance/run.py
@@ -8,13 +8,15 @@ from ten.test.utils.gnuplot import GnuplotHelper
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_004','ten_per_005','ten_per_006',
+        tests = ['ten_per_001','ten_per_002','ten_per_003','ten_per_004','ten_per_005','ten_per_006','ten_per_007',
                  'ten_per_010','ten_per_011','ten_per_012','ten_per_020','ten_per_021']
 
         for test in tests:
             with open(os.path.join(self.output, '%s.log' % test), 'w') as fp:
                 entries = self.results_db.get_results(test=test, environment=self.env)
-                weights = [1 - (i / (len(entries) - 1)) * (1 - 0.1) for i in range(len(entries))]
+                if len(entries) > 1:
+                    weights = [1 - (i / (len(entries) - 1)) * (1 - 0.1) for i in range(len(entries))]
+                else: weights = [1]*len(entries)
                 count = 0
                 for entry in reversed(entries):
                     weight = weights[count]
