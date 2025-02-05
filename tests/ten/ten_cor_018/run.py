f0b45100794f75c5665cb0403661f6bc16ef8480
Moray Grieve <moray.grieve@me.com>
2025-02-05 09:16:53 +0000

    Adding tx cost persistence

---
+++ tests/ten/ten_cor_018/run.py
@@ -0,0 +1,18 @@
+from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.storage import Storage
+
+
+class PySysTest(TenNetworkTest):
+
+    def execute(self):
+        # connect to network
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+
+        # deploy the contract
+        storage = Storage(self, web3, 100)
+        storage.deploy(network, account)
+
+        # get the receipt sort the fields
+        tx_receipt = network.transact(self, web3, storage.contract.functions.store(1), account, storage.GAS_LIMIT)
+
