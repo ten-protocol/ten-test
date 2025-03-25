385d6947dde49b5ede0f3b0f823f0d27b3a6bbbc
Moray Grieve <moray.grieve@me.com>
2025-03-25 13:49:20 +0000

    Merkle validate (#437)
    * Updated to validate on the merkle tree
    
    * Updated for mtree
    
    * Completed

--- tests/ten/ten_cor_024/run.py
+++ tests/ten/ten_cor_024/run.py
@@ -9,7 +9,7 @@ class PySysTest(TenNetworkTest):
     def execute(self):
         props = Properties()
         transfer = 4000000000000000
-        proof_timeout = 90 if self.is_local_ten() else 2400
+        proof_timeout = 90
 
         # create the bridge user and subscribe for value transfer events
         accnt = BridgeUser(self, props.account1pk(), props.account1pk(), 'accnt1')
@@ -27,33 +27,42 @@ class PySysTest(TenNetworkTest):
 
         # get the log msg from the merkle tree helper
         mh = MerkleTreeHelper.create(self)
-        mh.dump_tree(accnt.l2.web3, tx_receipt, 'xchain_tree.log')
+        block, decoded = mh.dump_tree(accnt.l2.web3, tx_receipt, 'xchain_tree.log')
         msg, msg_hash = mh.process_transfer(value_transfer)
         self.log.info('  value_transfer:        %s', msg)
         self.log.info('  value_transfer_hash:   %s', msg_hash)
+        self.log.info('  decoded_cross_chain:   %s', decoded)
+        self.log.info('  block_merkle_root:     %s', block.crossChainTreeHash)
+        self.assertTrue(msg_hash in [x[1] for x in decoded], assertMessage='Value transfer should be in the xchain tree')
 
-        # get the root and proof of inclusion from the node
-        self.log.info('Request proof and root from the node')
-        root, proof = accnt.l2.wait_for_proof('v', msg_hash, proof_timeout)
-        self.log.info('  returned root:         %s', root)
-        self.log.info('  returned proof:        %s', [p.hex() for p in proof])
+        mh_root, mh_proof = mh.get_proof('xchain_tree.log', 'v,%s' % msg_hash)
+        self.log.info('  calculated root:       %s', mh_root)
+        self.assertTrue(block.crossChainTreeHash == mh_root, assertMessage='Calculated merkle root should be same as the block header')
 
-        # release the funds from the L1 and check the balances
-        self.log.info('Relay the message on the L1 to release them')
-        tx_receipt = accnt.l1.release_funds(msg, proof, root)
+        # if a local testnet wait for the xchain message on the L1 and release the funds
+        if self.is_local_ten():
+            # get the root and proof of inclusion from the node
+            self.log.info('Request proof and root from the node')
+            root, proof = accnt.l2.wait_for_proof('v', msg_hash, proof_timeout)
+            self.log.info('  returned root:         %s', root)
+            self.log.info('  returned proof:        %s', [p.hex() for p in proof])
 
-        l1_cost = int(tx_receipt.gasUsed) * int(tx_receipt.effectiveGasPrice)
-        l1_after = accnt.l1.web3.eth.get_balance(accnt.l1.account.address)
-        l2_after = accnt.l2.web3.eth.get_balance(accnt.l2.account.address)
-        self.log.info('  l1_balance before:     %s', l1_before)
-        self.log.info('  l2_balance before:     %s', l2_before)
-        self.log.info('  l1_balance after:      %s', l1_after)
-        self.log.info('  l2_balance after:      %s', l2_after)
-        self.log.info('  l1_cost:               %s', l1_cost)
-        self.log.info('  l2_cost:               %s', l2_cost)
-        self.log.info('  l1_delta (inc):        %s', l1_after-l1_before)
-        self.log.info('  l1_delta (with cost):  %s', l1_after-l1_before+l1_cost)
-        self.log.info('  l2_delta (dec):        %s', l2_before-l2_after)
-        self.log.info('  l2_delta (with cost):  %s', l2_before-l2_after-l2_cost)
-        self.assertTrue(l1_after-l1_before+l1_cost == transfer,
-                        assertMessage='L1 balance should increase by transfer amount plus gas for releasing')
+            # release the funds from the L1 and check the balances
+            self.log.info('Relay the message on the L1 to release them')
+            tx_receipt = accnt.l1.release_funds(msg, proof, root)
+
+            l1_cost = int(tx_receipt.gasUsed) * int(tx_receipt.effectiveGasPrice)
+            l1_after = accnt.l1.web3.eth.get_balance(accnt.l1.account.address)
+            l2_after = accnt.l2.web3.eth.get_balance(accnt.l2.account.address)
+            self.log.info('  l1_balance before:     %s', l1_before)
+            self.log.info('  l2_balance before:     %s', l2_before)
+            self.log.info('  l1_balance after:      %s', l1_after)
+            self.log.info('  l2_balance after:      %s', l2_after)
+            self.log.info('  l1_cost:               %s', l1_cost)
+            self.log.info('  l2_cost:               %s', l2_cost)
+            self.log.info('  l1_delta (inc):        %s', l1_after-l1_before)
+            self.log.info('  l1_delta (with cost):  %s', l1_after-l1_before+l1_cost)
+            self.log.info('  l2_delta (dec):        %s', l2_before-l2_after)
+            self.log.info('  l2_delta (with cost):  %s', l2_before-l2_after-l2_cost)
+            self.assertTrue(l1_after-l1_before+l1_cost == transfer,
+                            assertMessage='L1 balance should increase by transfer amount plus gas for releasing')
