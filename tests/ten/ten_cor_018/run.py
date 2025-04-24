fe61f65ad28e5c7a47b7b21afe8a907f23106859
Moray Grieve <moray.grieve@me.com>
2025-04-24 10:54:22 +0100

    Push add test for invalid chain ids

--- tests/ten/ten_cor_018/run.py
+++ tests/ten/ten_cor_018/run.py
@@ -1,37 +1,46 @@
-import traceback
 from pysys.constants import PASSED, FAILED
 from ten.test.basetest import TenNetworkTest
-from ten.test.contracts.storage import Storage
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        # connect to the network (use an ephemeral account)
-        network = self.get_network_connection()
-        web3, account = network.connect(self, private_key=self.get_ephemeral_pk(), check_funds=False)
-        self.distribute_native(account, network.ETH_ALLOC_EPHEMERAL)
+        # connect to the l1 and l2 networks (we will only transact on the l2)
+        l1 = self.get_l1_network_connection()
+        l2 = self.get_network_connection(name='local')
+        _, account_to = l2.connect_account1(self, check_funds=False)
 
-        # deploy the contract
-        storage = Storage(self, web3, 100)
-        storage.deploy(network, account)
-        target = storage.contract.functions.store(100)
-        params = {'from':account.address, 'chainId': web3.eth.chain_id, 'gasPrice': web3.eth.gas_price}
-        estimate = target.estimate_gas(params)
+        pk = self.get_ephemeral_pk()
+        web3_l1, account_l1 = l1.connect(self, private_key=pk, check_funds=False)
+        web3_l2, account_l2 = l2.connect(self, private_key=pk, check_funds=False)
+        self.distribute_native(account_l2, l2.ETH_ALLOC_EPHEMERAL)
 
-        # transact (the first should be rejected so we just check later ones go through)
-        self.transact(storage, web3, account, 101, estimate, chainId=web3.eth.chain_id, expect_pass=True)
-        self.transact(storage, web3, account, 102, estimate, chainId=200, expect_pass=False)
+        # transact using the tx signed from the l2 details
+        tx, signed_tx = self.get_signed_transaction(web3_l2, account_l2, account_to.address, 10, l2.chain_id())
+        self.send_signed_transaction(web3_l2, tx, signed_tx, expect_pass=True)
 
-    def transact(self, storage, web3, account, value, estimate, chainId, expect_pass=True):
-        target = storage.contract.functions.store(value)
+        # transact using the tx signed from the l2 details but with a bogus chain ID
+        tx, signed_tx = self.get_signed_transaction(web3_l2, account_l2, account_to.address, 10, 123456)
+        self.send_signed_transaction(web3_l2, tx, signed_tx, expect_pass=False)
+        self.assertGrep(file='wallet_local_logs',
+                        expr='invalid sender: invalid chain id for signer: have %d want %d' % (123456, l2.chain_id()))
+
+        # transact using the tx signed from the l1 details and signed on the l1
+        tx, signed_tx = self.get_signed_transaction(web3_l1, account_l1, account_to.address, 10, l1.chain_id())
+        self.send_signed_transaction(web3_l2, tx, signed_tx, expect_pass=False)
+        self.assertGrep(file='wallet_local_logs',
+                        expr='invalid sender: invalid chain id for signer: have %d want %d' % (l1.chain_id(), l2.chain_id()))
+
+    def get_signed_transaction(self, web3, account, address, amount, chain_id):
+        self.log.info('Creating signed transaction')
         nonce = web3.eth.get_transaction_count(account.address)
-        params = {'from':account.address, 'nonce': nonce, 'chainId': chainId,
-                  'gasPrice': web3.eth.gas_price, 'gas':estimate}
-        build_tx = target.build_transaction(params)
+        tx = {'nonce': nonce, 'from': account.address, 'to': address, 'value': amount,
+              'chainId': chain_id, 'gasPrice': web3.eth.gas_price, 'gas': 42000}
+        return tx, account.sign_transaction(tx)
 
+    def send_signed_transaction(self, web3, build_tx, signed_tx, expect_pass=True):
+        self.log.info('Sending signed transaction')
         try:
-            signed_tx = account.sign_transaction(build_tx)
             tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
             tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
             if tx_receipt.status == 1:
@@ -42,8 +51,7 @@ class PySysTest(TenNetworkTest):
                 self.log.warn('Replaying the transaction did not throw an error')
                 if expect_pass: self.addOutcome(FAILED, outcomeReason='Expected tx to succeed')
         except Exception as e:
-            self.log.error('Transaction failed')
-            self.log.error('Stack trace:' + traceback.format_exc())
+            self.log.warn('Transaction failed: %s' % e)
             if expect_pass:
                 self.addOutcome(FAILED, outcomeReason='Expected tx to succeed')
             else:
