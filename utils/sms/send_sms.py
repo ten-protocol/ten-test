b679fc9070c4ac830964dbd10f8f35ebf4b9ea6e
Moray Grieve <moray.grieve@me.com>
2025-05-05 16:08:43 +0100

    Updated

--- utils/sms/send_sms.py
+++ utils/sms/send_sms.py
@@ -1,13 +1,21 @@
+import argparse
 from twilio.rest import Client
 
-account_sid = ''
-auth_token = ''
-client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    body="This is the ship that made the Kessel Run in fourteen parsecs?",
-    from_="",
-    to="",
-)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='websocket proxy.')
+    parser.add_argument('--account', help='The twilio account SID', required=True)
+    parser.add_argument('--token', help='The account authentication token', required=True)
+    parser.add_argument('--fr', help='The phone number to send from', required=True)
+    parser.add_argument('--to', help='The phone number to send to', required=True)
+    args = parser.parse_args()
 
-print(message.body)
+    client = Client(args.account, args.token)
+
+    message = client.messages.create(
+        body = "This is the ship that made the Kessel Run in fourteen parsecs?",
+        from_ = args.fr,
+        to = args.to,
+    )
+
+    print(message.body)
