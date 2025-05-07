8a5894d3ec9a66bccba6dad1bb56af3e28e90b06
Moray Grieve <moray.grieve@me.com>
2025-05-07 12:22:48 +0100

    Add discord test

--- utils/sms/discord.py
+++ utils/sms/discord.py
@@ -11,13 +11,14 @@ embed = {
     "fields": [
         {"name": "Team status", "value": "Panicking", "inline": True},
         {"name": "On-call support", "value": "<@814873419207409685>", "inline": True},
-        {"name": "Workflow", "value": "[health_check_sepolia](https://github.com/ten-protocol/ten-test/actions/workflows/health_check_sepolia.yml)", "inline": False},
+        {"name": "Workflow", "value": "[health_check_uat](https://github.com/ten-protocol/ten-test/actions/workflows/health_check_uat.yml)", "inline": False},
 
     ],
     "footer": {"text": "E2E Monitoring"},
 }
 
 data = {
+    "content":  "Please investigate <@814873419207409685>",
     "username": "E2E Health Checks",
     "embeds": [embed]
 }
