58959e9aa08732047812da648bdfead7c7a9186c
Moray Grieve <moray.grieve@me.com>
2025-05-16 13:27:25 +0100

    Add bridging (#489)

--- admin/bridge_funds/run.py
+++ admin/bridge_funds/run.py
@@ -1,22 +1,23 @@
 from ten.test.basetest import TenNetworkTest
-from ten.test.utils.bridge import BridgeUser
 from ten.test.utils.properties import Properties
+from ten.test.utils.bridge import BridgeUser
 
 
 class PySysTest(TenNetworkTest):
+    PK = None
+    AMOUNT = 1
 
     def execute(self):
         props = Properties()
-        transfer = 1000
-
-        accnt1 = BridgeUser(self, props.account1pk(), props.account1pk(), 'accnt1')
+        pk = self.PK if self.PK is not None else props.l1_funded_account_pk(self.env)
+        accnt1 = BridgeUser(self, pk, pk, 'accnt1', check_funds=False)
+        amount = accnt1.l1.web3.to_wei(self.AMOUNT, 'ether')
 
         l1_balance_before = accnt1.l1.web3.eth.get_balance(accnt1.l1.account.address)
         l2_balance_before = accnt1.l2.web3.eth.get_balance(accnt1.l2.account.address)
 
-        # send native from the L1 to the L2, wait for the xchain msg on the l2
         self.log.info('Send native and wait for the xchain message on the L2')
-        tx_receipt, xchain_msg = accnt1.l1.send_native(accnt1.l2.account.address, transfer)
+        tx_receipt, xchain_msg = accnt1.l1.send_native(accnt1.l2.account.address, amount)
         accnt1.l2.wait_for_message(xchain_msg)
 
         l1_balance_after = accnt1.l1.web3.eth.get_balance(accnt1.l1.account.address)
@@ -30,5 +31,5 @@ class PySysTest(TenNetworkTest):
         self.log.info('   L2 after:  %d', l2_balance_after)
         self.log.info('   L2 incr:   %d', (l2_balance_after-l2_balance_before))
 
-        self.assertTrue(l1_balance_before-l1_balance_after > transfer)
-        self.assertTrue(l2_balance_after-l2_balance_before == transfer)
+        self.assertTrue(l1_balance_before-l1_balance_after > amount)
+        self.assertTrue(l2_balance_after-l2_balance_before == amount)
