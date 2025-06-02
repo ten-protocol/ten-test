0f4b8ceba18df2e5187e946f56b033f670c6e90a
Moray Grieve <moray.grieve@me.com>
2025-06-02 15:51:23 +0100

    Updated

--- admin/upgrade_contracts/run.py
+++ admin/upgrade_contracts/run.py
@@ -1,7 +1,24 @@
 from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.guesser import Guesser
+from ten.test.helpers.upgrade import UpgradeL1ContractsHelper
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        pass
+        # create the helper and run the upgrade
+        helper = UpgradeL1ContractsHelper(self)
+        helper.run()
+
+        # perform some simple transactions
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+
+        guesser = Guesser(self, web3)
+        guesser.deploy(network, account)
+
+        # guess the number
+        self.log.info('Starting guessing game')
+        self.assertTrue(guesser.guess(0, 100) == 12)
+
+
