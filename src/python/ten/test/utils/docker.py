d9baa89af6cedbbc70a45cb5471a8b7dbccc8532
Moray Grieve <moray.grieve@me.com>
2025-06-20 13:04:16 +0100

    Refactor the node restart tests

--- src/python/ten/test/utils/docker.py
+++ src/python/ten/test/utils/docker.py
@@ -1,17 +1,18 @@
 import os
-from pysys.constants import FOREGROUND
+from pysys.constants import FOREGROUND, BACKGROUND
 from ten.test.utils.properties import Properties
 
 
 class DockerHelper:
 
     @classmethod
-    def container_id(cls, test, name):
+    def container_id(cls, test, container, name=None):
         """Get a docker container id by name. """
-        stdout = os.path.join(test.output, 'docker_cntid_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_cntid_%s'%name+'.err')
+        basename = 'docker_id_%s' % container if name is None else 'docker_id_%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
 
-        arguments = ['ps', '-aqf', 'name=%s'%name]
+        arguments = ['ps', '-aqf', 'name=%s' % container]
         test.startProcess(command=Properties().docker_binary(), displayName='docker-ps', workingDir=test.output,
                           arguments=arguments, stdout=stdout, stderr=stderr, state=FOREGROUND,
                           ignoreExitStatus=True, quiet=True)
@@ -19,16 +20,18 @@ class DockerHelper:
         with open(stdout, 'r') as fp: return fp.readline().strip()
 
     @classmethod
-    def container_stop(cls, test, name, time=10):
+    def container_stop(cls, test, container, time=10, name=None):
         """Stop a docker container by name.
 
         Sends a SIGTERM to gracefully stop the container. If the container does not stop within the time period
         supplied, a SIGKILL is then sent. To immediately send a SIGKILL, set time to zero.
         """
-        stdout = os.path.join(test.output, 'docker_stop_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_stop_%s'%name+'.err')
-        id = cls.container_id(test, name)
-        test.log.info('Stopping container %s with id %s, time %d' % (name, id, time))
+        basename = 'docker_stop_%s' % container if name is None else 'docker_stop_%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
+
+        id = cls.container_id(test, container)
+        test.log.info('Stopping container %s with id %s, time %d' % (container, id, time))
 
         arguments = ['stop', '-t=%d' % time, id]
         test.startProcess(command=Properties().docker_binary(), displayName='docker-stop', workingDir=test.output,
@@ -36,12 +39,14 @@ class DockerHelper:
                           ignoreExitStatus=True, quiet=True)
 
     @classmethod
-    def container_start(cls, test, name):
+    def container_start(cls, test, container, name=None):
         """Start a docker container by name. """
-        stdout = os.path.join(test.output, 'docker_start_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_start_%s'%name+'.err')
-        id = cls.container_id(test, name)
-        test.log.info('Starting container %s with id %s' % (name, id))
+        basename = 'docker_start_%s' % container if name is None else 'docker_start_%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
+
+        id = cls.container_id(test, container)
+        test.log.info('Starting container %s with id %s' % (container, id))
 
         arguments = ['start', id]
         test.startProcess(command=Properties().docker_binary(), displayName='docker-start', workingDir=test.output,
@@ -49,38 +54,46 @@ class DockerHelper:
                           ignoreExitStatus=True, quiet=True)
 
     @classmethod
-    def container_rm(cls, test, name):
+    def container_rm(cls, test, container, name=None):
         """Remove a docker container by name. """
-        stdout = os.path.join(test.output, 'docker_rm_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_rm_%s'%name+'.err')
-        id = cls.container_id(test, name)
+        basename = 'docker_rm_%s' % container if name is None else 'docker_rm_%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
+
+        id = cls.container_id(test, container)
         if id == '':
             test.log.info('No container exists')
             return
-        test.log.info('Removing container %s with id %s' % (name, id))
+        test.log.info('Removing container %s with id %s' % (container, id))
 
         arguments = ['container', 'rm', id]
-        test.startProcess(command=Properties().docker_binary(), displayName='docker-start', workingDir=test.output,
+        test.startProcess(command=Properties().docker_binary(), displayName='docker-rm', workingDir=test.output,
                           arguments=arguments, stdout=stdout, stderr=stderr, state=FOREGROUND,
                           ignoreExitStatus=True, quiet=True)
+
     @classmethod
-    def container_logs(cls, test, name):
+    def container_logs(cls, test, container, state=FOREGROUND, name=None, since=None):
         """Get the docker logs to stdout and stderr by name. """
-        stdout = os.path.join(test.output, 'docker_logs_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_logs_%s'%name+'.err')
-        id = cls.container_id(test, name)
+        basename = 'docker_logs_%s' % container if name is None else '%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
+
+        id = cls.container_id(test, container)
 
         arguments = ['logs', id]
-        test.startProcess(command=Properties().docker_binary(), displayName='docker-start', workingDir=test.output,
-                          arguments=arguments, stdout=stdout, stderr=stderr, state=FOREGROUND,
+        if since is not None: arguments.extend(['--since', '%s' % since])
+        test.startProcess(command=Properties().docker_binary(), displayName='docker-logs', workingDir=test.output,
+                          arguments=arguments, stdout=stdout, stderr=stderr, state=state,
                           ignoreExitStatus=True, quiet=True)
         return stdout, stderr
 
     @classmethod
-    def container_running(cls, test, name):
-        stdout = os.path.join(test.output, 'docker_inspect_%s'%name+'.out')
-        stderr = os.path.join(test.output, 'docker_inspect_%s'%name+'.err')
-        arguments = ['inspect', '-f', '\'{{.State.Running}}\'', name]
+    def container_running(cls, test, container, name=None):
+        basename = 'docker_inspect_%s' % container if name is None else 'docker_inspect_%s_%s' % (container, name)
+        stdout = os.path.join(test.output, '%s.out' % basename)
+        stderr = os.path.join(test.output, '%s.err' % basename)
+
+        arguments = ['inspect', '-f', '\'{{.State.Running}}\'', container]
         test.startProcess(command=Properties().docker_binary(), displayName='docker-inspect', workingDir=test.output,
                           arguments=arguments, stdout=stdout, stderr=stderr, state=FOREGROUND,
                           ignoreExitStatus=True, quiet=True)
