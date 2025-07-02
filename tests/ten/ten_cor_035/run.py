628b4e3335013f768eadd19cc1b679360249bb3d
Moray Grieve <moray.grieve@me.com>
2025-07-02 13:05:51 +0100

    Complete test run

--- tests/ten/ten_cor_035/run.py
+++ tests/ten/ten_cor_035/run.py
@@ -20,24 +20,33 @@ class PySysTest(TenNetworkTest):
         hashes = []
         for i in range(1,5):
             tx_receipt = network.transact(self, web3, game.contract.functions.guess(i), account, game.GAS_LIMIT)
-            hashes.append(tx_receipt.transactionHash)
+            hashes.append(tx_receipt.transactionHash.hex())
 
         # an ephemeral user connects and requests the public transactions (last 10, 2 pages max 5 in each)
         self.log.info('')
         self.log.info('Ephemeral user gets last two pages of personal transactions')
         web_usr, account_usr = network.connect(self, private_key=self.get_ephemeral_pk(), check_funds=True)
 
-        # personal transaction count for the ephemeral user
+        # personal transactions for the ephemeral user with show public true
+        self.log.info('')
         personal_txs_count = self.scan_list_personal_transactions(url=network.connection_url(), address=account_usr.address,
                                                                   show_public=True, offset=0, size=1)['Total']
-        self.log.info('Personal transaction count is %d' % personal_txs_count)
+        self.log.info('Personal transaction count (with public)is %d' % personal_txs_count)
 
-        # personal transactions for the ephemeral user
         personal_txs = self.scan_list_personal_transactions(url=network.connection_url(), address=account_usr.address,
                                                             show_public=True, offset=0, size=personal_txs_count)['Receipts']
-        self.log.info('List of personal transactions (block and tx hash) are;')
-        for tx in personal_txs: self.log.info('  %s %s' % (tx['blockHash'], tx['transactionHash']))
         personal_txs_hashes = [x['transactionHash'] for x in personal_txs]
-
         for h in hashes:
             self.assertTrue(h in personal_txs_hashes, assertMessage='%s is seen in list' % h)
+
+        # personal transactions for the ephemeral user with show public false
+        self.log.info('')
+        personal_txs_count = self.scan_list_personal_transactions(url=network.connection_url(), address=account_usr.address,
+                                                                      show_public=False, offset=0, size=1)['Total']
+        self.log.info('Personal transaction count (no public) is %d' % personal_txs_count)
+
+        personal_txs = self.scan_list_personal_transactions(url=network.connection_url(), address=account_usr.address,
+                                                            show_public=False, offset=0, size=personal_txs_count)['Receipts']
+        personal_txs_hashes = [x['transactionHash'] for x in personal_txs]
+        for h in hashes:
+            self.assertTrue(h not in personal_txs_hashes, assertMessage='%s is not seen in list' % h)
