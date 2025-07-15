88e2b0dd934a92036ee9a53d27a84785abab2cb3
Moray Grieve <moray.grieve@me.com>
2025-07-15 11:55:27 +0100

    Zentest balances (#524)
    * Updated the zen balance check
    
    * Updated the zen balance check

--- src/python/ten/test/contracts/system.py
+++ src/python/ten/test/contracts/system.py
@@ -23,5 +23,17 @@ class TransactionPostProcessor:
         self.contract = self.web3.eth.contract(address=self.address, abi=self.abi)
 
 
+class ZenTestnet:
+    GAS_LIMIT = 3_000_000
+
+    def __init__(self, test, web3, address):
+        self.test = test
+        self.web3 = web3
+        self.address = address
+        self.abi_path = os.path.join(PROJECT.root, 'artifacts', 'contracts', 'testing', 'ZenTestnet.sol',
+                                     'ZenTestnet.json')
+        with open(self.abi_path, 'r') as fp:
+            self.abi = json.load(fp)
+        self.contract = self.web3.eth.contract(address=self.address, abi=self.abi)
 
 
--- tests/ten/ten_cor_001/run.py
+++ tests/ten/ten_cor_001/run.py
@@ -1,24 +1,36 @@
 from ten.test.basetest import TenNetworkTest
-from ten.test.contracts.system import TransactionPostProcessor
+from ten.test.contracts.system import TransactionPostProcessor, ZenTestnet
 from ten.test.contracts.storage import Storage
 
 
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        # get the network, deploy the storage contract
+        # get the network
         network = self.get_network_connection()
         web3_deploy, account_deploy = network.connect_account1(self)
-        storage = Storage(self, web3_deploy, 0)
-        storage.deploy(network, account_deploy)
 
-        # the reference to the transaction post processor
+        # the reference to the transaction post processor and zen testnet contract
         processor = TransactionPostProcessor(self, web3_deploy)
         zen_address = processor.contract.functions.onBlockEndListeners(0).call()
-        self.log.info('The zen address is %s' % zen_address)
+        zentest = ZenTestnet(self, web3_deploy, zen_address)
+        zen_initial = zentest.contract.functions.balanceOf(account_deploy.address).call()
+        self.log.info('The zen address is %s, and deploying balance %d' % (zen_address, zen_initial))
+
+        # deploy the storage contract and check out zen goes up by one
+        storage = Storage(self, web3_deploy, 0)
+        storage.deploy(network, account_deploy)
+        self.assert_zen(zentest.contract, account_deploy.address, zen_initial+1)
 
         # connect as an ephemeral test user and transact against the storage contract
         pk = self.get_ephemeral_pk()
-        web_usr1, account_usr1 = network.connect(self, private_key=pk, check_funds=True)
-        network.transact(self, web_usr1, storage.contract.functions.store(1), account_usr1, storage.GAS_LIMIT)
-        network.transact(self, web_usr1, storage.contract.functions.store(2), account_usr1, storage.GAS_LIMIT)
+        web3, account = network.connect(self, private_key=pk, check_funds=True)
+        self.assert_zen(zentest.contract, account.address, 0)
+        network.transact(self, web3, storage.contract.functions.store(1), account, storage.GAS_LIMIT)
+        self.assert_zen(zentest.contract, account.address, 1)
+        network.transact(self, web3, storage.contract.functions.store(2), account, storage.GAS_LIMIT)
+        self.assert_zen(zentest.contract, account.address, 2)
+
+    def assert_zen(self, contract, address, expected):
+        self.assertTrue(contract.functions.balanceOf(address).call() == expected,
+                        assertMessage='Expected ZEN balance is %d'%expected)
