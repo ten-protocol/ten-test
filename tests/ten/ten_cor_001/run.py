d87ca91ece2a2949fa419bf870aaa91d71846f9d
Moray Grieve <moray.grieve@me.com>
2025-07-15 11:54:23 +0100

    Updated the zen balance check

--- tests/ten/ten_cor_001/run.py
+++ tests/ten/ten_cor_001/run.py
@@ -6,17 +6,21 @@ from ten.test.contracts.storage import Storage
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        # get the network, deploy the storage contract
+        # get the network
         network = self.get_network_connection()
         web3_deploy, account_deploy = network.connect_account1(self)
-        storage = Storage(self, web3_deploy, 0)
-        storage.deploy(network, account_deploy)
 
-        # the reference to the transaction post processor
+        # the reference to the transaction post processor and zen testnet contract
         processor = TransactionPostProcessor(self, web3_deploy)
         zen_address = processor.contract.functions.onBlockEndListeners(0).call()
         zentest = ZenTestnet(self, web3_deploy, zen_address)
-        self.log.info('The zen address is %s' % zen_address)
+        zen_initial = zentest.contract.functions.balanceOf(account_deploy.address).call()
+        self.log.info('The zen address is %s, and deploying balance %d' % (zen_address, zen_initial))
+
+        # deploy the storage contract and check out zen goes up by one
+        storage = Storage(self, web3_deploy, 0)
+        storage.deploy(network, account_deploy)
+        self.assert_zen(zentest.contract, account_deploy.address, zen_initial+1)
 
         # connect as an ephemeral test user and transact against the storage contract
         pk = self.get_ephemeral_pk()
