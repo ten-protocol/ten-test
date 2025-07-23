42f5864263f4c7a40401b11a1da454f9d756bdbd
Moray Grieve <moray.grieve@me.com>
2025-07-23 13:10:45 +0100

    Moved

--- tests/ten/ten_cor_156/run.py
+++ tests/ten/ten_cor_156/run.py
@@ -1,11 +1,32 @@
-import pysys
-from pysys.constants import *
-from pysys.basetest import BaseTest
+from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.storage import Storage
 
-class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
-	def validate(self):
-		pass
-	
+class PySysTest(TenNetworkTest):
+
+    def execute(self):
+        # connect players to the network
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+
+        # user deploys contract and performs a transactions against it
+        self.log.info('')
+        self.log.info('User deploys contract and submits transactions against it')
+        storage = Storage(self, web3, 0)
+        storage.deploy(network, account)
+
+        tx_receipt = network.transact(self, web3, storage.contract.functions.store(78), account, storage.GAS_LIMIT)
+        tx_block_hash = tx_receipt['blockHash'].hex()
+        self.log.info(tx_receipt)
+        self.log.info('Transaction made with reported block hash as %s', tx_block_hash)
+
+        # this actually just returns the header of the latest batch
+        block = self.scan_get_latest_batch()
+        block_number1 = int(block['number'], 16)
+
+        self.wait(2 * float(self.block_time))
+        block = self.scan_get_latest_batch()
+        block_number2 = int(block['number'], 16)
+
+        self.log.info('First block %d, second block %d' % (block_number1, block_number2))
+        self.assertTrue(block_number2 > block_number1, assertMessage='Latest block number should increase')
