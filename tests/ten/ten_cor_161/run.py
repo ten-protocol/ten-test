9e87021fe9cb265e31e216a3460131c6a0bfcbbd
Moray Grieve <moray.grieve@me.com>
2025-07-29 15:28:32 +0100

    Updated

--- tests/ten/ten_cor_161/run.py
+++ tests/ten/ten_cor_161/run.py
@@ -1,11 +1,16 @@
-import pysys
-from pysys.constants import *
-from pysys.basetest import BaseTest
+from collections import Counter
+from pysys.constants import FAILED
+from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.storage import Storage
 
-class PySysTest(BaseTest):
-	def execute(self):
-		pass
 
-	def validate(self):
-		pass
-	
+class PySysTest(TenNetworkTest):
+
+    def execute(self):
+        # this actually just returns the header of the latest rollup
+        rollup_header = self.scan_get_latest_rollup_header()
+
+        batches = self.scan_get_rollup_batches(hash=rollup_header['hash'], offset=0, size=10)
+        self.assertTrue(len(batches['BatchesData']) <= 10, assertMessage='Batches should be less than or equal the page')
+
+        for batch in batches['BatchesData']: self.log.info(batch)
