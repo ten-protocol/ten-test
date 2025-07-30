d4a1c849c3bf78d8f18a7ad451155dea630be268
Moray Grieve <moray.grieve@me.com>
2025-07-30 10:37:05 +0100

    Added test

--- tests/ten/ten_cor_161/run.py
+++ tests/ten/ten_cor_161/run.py
@@ -7,10 +7,33 @@ from ten.test.contracts.storage import Storage
 class PySysTest(TenNetworkTest):
 
     def execute(self):
-        # this actually just returns the header of the latest rollup
+        # get the latest rollup and from that the first and last batch sequence numbers
         rollup_header = self.scan_get_latest_rollup_header()
+        first = int(rollup_header['FirstBatchSeqNo'])
+        last = int(rollup_header['LastBatchSeqNo'])
+        total = last - first
+        self.log.info('First and last batch seq nos are %d %d, total %d', first, last, total)
 
-        batches = self.scan_get_rollup_batches(hash=rollup_header['hash'], offset=0, size=10)
-        self.assertTrue(len(batches['BatchesData']) <= 10, assertMessage='Batches should be less than or equal the page')
+        batches = []
+        for page in self.split_into_segments(total, 10):
+            self.log.info(' Getting page with offset and size %d %d', page[0], page[1])
+            batches.extend(self.scan_get_rollup_batches(hash=rollup_header['hash'], offset=page[0], size=page[1])['BatchesData'])
 
-        for batch in batches['BatchesData']: self.log.info(batch)
+        self.assertTrue(len(batches) <= total, assertMessage='Batches should be less than or equal to the page size')
+        batch_nos = [int(x['header']['number'], 16) for x in batches]
+        self.log.info('Batch numbers are %s', batch_nos)
+        self.assertTrue(batch_nos[0] == last, assertMessage='Last batch should be in the return set')
+        self.assertTrue(batch_nos[-1] == first, assertMessage='First batch should be in the return set')
+
+    def split_into_segments(self, number, increment):
+        result = []
+        start = 0
+        while number > 0:
+            if number >= increment:
+                result.append((start, increment))
+                number -= increment
+                start += increment
+            else:
+                result.append((start, number))
+                break
+        return result
