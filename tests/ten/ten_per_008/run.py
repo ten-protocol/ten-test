49d54b42d425b060ec38903b9f3586e32426715f
Moray Grieve <moray.grieve@me.com>
2025-08-12 12:10:55 +0100

    Add test

---
+++ tests/ten/ten_per_008/run.py
@@ -0,0 +1,82 @@
+import os, random, string
+from pysys.constants import PASSED, FOREGROUND
+from ten.test.basetest import TenNetworkTest
+from ten.test.contracts.emitter import EventEmitter, TransparentEventEmitter
+
+
+class PySysTest(TenNetworkTest):
+    NUM_CLIENTS = 10  # number of transactors and subscribers
+    NUM_TRANSACTIONS = 250  # number of txs the transactors will perform
+
+    def execute(self):
+        # connect to network on the primary gateway and deploy contract
+        network = self.get_network_connection()
+        web3, account = network.connect_account1(self)
+
+        emitter = EventEmitter(self, web3, 100)
+        emitter.deploy(network, account)
+        transparent_emitter = TransparentEventEmitter(self, web3, 100)
+        transparent_emitter.deploy(network, account)
+        debugger_url = network.connection_url()
+
+        # estimate how much gas each transactor will need
+        rstr = self.rand_str()
+        chain_id = network.chain_id()
+        gas_price = web3.eth.gas_price
+        params = {'from': account.address, 'chainId': chain_id, 'gasPrice': gas_price}
+        limits = [emitter.contract.functions.emitSimpleEvent(1, rstr).estimate_gas(params)]
+        limits.append(emitter.contract.functions.emitArrayEvent(1, [1, 2], [rstr, rstr]).estimate_gas(params))
+        limits.append(emitter.contract.functions.emitStructEvent(1, rstr).estimate_gas(params))
+        limits.append(emitter.contract.functions.emitMappingEvent(1, [account.address], [200]).estimate_gas(params))
+        gas_limit = max(limits)
+        funds_needed = 1.1 * self.NUM_TRANSACTIONS * (gas_price * gas_limit)
+
+        # set up the transactors and run the subscribers
+        clients = []
+        for id in range(0, self.NUM_CLIENTS):
+            pk, account, network = self.setup_transactor(funds_needed)
+            clients.append((id, pk, account, network))
+            self.run_subscriber(network, emitter if random.randint(0, 1) else transparent_emitter, account, id)
+
+        # run the transactors serially in the foreground
+        for id, pk, account, network in clients:
+            self.run_transactor(id, transparent_emitter if id == 1 else emitter, pk, network, gas_limit)
+            self.ratio_failures(file=os.path.join(self.output, 'transactor%d.out' % id))
+
+    def setup_transactor(self, funds_needed):
+        pk = self.get_ephemeral_pk()
+        network = self.get_network_connection()
+        web3, account = network.connect(self, private_key=pk, check_funds=False)
+        self.distribute_native(account, web3.from_wei(funds_needed, 'ether'))
+        return pk, account, network
+
+    def run_transactor(self, id, emitter, pk, network, gas_limit):
+        stdout = os.path.join(self.output, 'transactor%s.out' % id)
+        stderr = os.path.join(self.output, 'transactor%s.err' % id)
+        script = os.path.join(self.input, 'transactor.py')
+        args = []
+        args.extend(['--network_http', network.connection_url()])
+        args.extend(['--chainId', '%s' % network.chain_id()])
+        args.extend(['--pk', pk])
+        args.extend(['--contract_address', '%s' % emitter.address])
+        args.extend(['--contract_abi', '%s' % emitter.abi_path])
+        args.extend(['--transactions', '%d' % self.NUM_TRANSACTIONS])
+        args.extend(['--id', '%d' % id])
+        args.extend(['--gas_limit', '%d' % gas_limit])
+        self.run_python(script, stdout, stderr, args, state=FOREGROUND, timeout=300)
+
+    def run_subscriber(self, network, emitter, account, id_filter):
+        stdout = os.path.join(self.output, 'subscriber%d.out' % id_filter)
+        stderr = os.path.join(self.output, 'subscriber%d.err' % id_filter)
+        script = os.path.join(self.input, 'subscriber.js')
+        args = []
+        args.extend(['--network_ws', network.connection_url(web_socket=True)])
+        args.extend(['--contract_address', '%s' % emitter.address])
+        args.extend(['--contract_abi', '%s' % emitter.abi_path])
+        args.extend(['--id_filter', str(id_filter)])
+        args.extend(['--address_filter', account.address])
+        self.run_javascript(script, stdout, stderr, args)
+        self.waitForGrep(file=stdout, expr='Listening for filtered events...', timeout=10)
+
+    def rand_str(self):
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
